48352f5f52fa90d455ff12d79e644c3f20f6ce61
Eli Bendersky <eliben@gmail.com>
2014-09-13 05:46:53 -0700

    list rotation and WxPytris

---
+++ 2008/wxpytris/wxpytris.py
@@ -0,0 +1,19 @@
+import sys
+import wx
+
+sys.path.insert(0, 'lib.zip')
+from lib.TetrisGame import TetrisGame
+
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    frame = TetrisGame(None)
+    frame.Show(True)
+    app.MainLoop()
+
+
+
+
+
+
+
