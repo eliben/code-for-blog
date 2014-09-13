2baa10cbc2cf39c69f644c68f6ee0a8cae5dfd26
Eli Bendersky <eliben@gmail.com>
2014-09-13 05:43:48 -0700

    memmgr

---
+++ 2008/memmgr/README.txt
@@ -0,0 +1,30 @@
+Memory manager: provides memory from a fixed pool that is
+allocated statically at link-time.
+
+Useful for embedded systems, where dynamic allocation is not 
+always supported. Even when it is, it's sometimes safer to 
+pre-allocate the memory pool and not use the heap.
+
+Written by Eli Bendersky (eliben@gmail.com). Inspired by the 
+storage allocator in "The C Programming Language, 2nd Edition"
+and various allocators found online.
+
+-- License -- 
+
+This code is in the public domain.
+
+-- Package contents -- 
+
+memmgr.h
+   The header file to be included in your application. It contains
+   the API of the memory manager and can be customized by
+   defining pre-processor flags. Read the comment at its top for
+   explanation.
+  
+memmgr.c
+   The implementation of the memory manager.
+  
+main.c
+   A sample main application that uses the memory manager, with
+   some tests.
+
---
+++ 2008/memmgr/main.c
@@ -0,0 +1,88 @@
+//----------------------------------------------------------------
+// Statically-allocated memory manager
+//
+// by Eli Bendersky (eliben@gmail.com)
+//  
+// This code is in the public domain.
+//----------------------------------------------------------------
+#include <stdio.h>
+#include <assert.h>
+#include "memmgr.h"
+
+
+// A rudimentary test of the memory manager.
+// Runs assuming default flags in memmgr.h:
+//
+// #define POOL_SIZE 8 * 1024
+// #define MIN_POOL_ALLOC_QUANTAS 16
+//
+// And a 32-bit machine (sizeof(unsigned long) == 4)
+//
+void test_memmgr()
+{
+    byte *p[30] = {0};
+    int i;
+
+    // Each header uses 8 bytes, so this allocates
+    // 3 * (2048 + 8) = 6168 bytes, leaving us
+    // with 8192 - 6168 = 2024
+    //
+    for (i = 0; i < 3; ++i)
+    {
+        p[i] = memmgr_alloc(2048);
+        assert(p[i]);
+    }
+
+    // Allocate all the remaining memory
+    //
+    p[4] = memmgr_alloc(2016);
+    assert(p[4]);
+
+    // Nothing left...
+    //
+    p[5] = memmgr_alloc(1);
+    assert(p[5] == 0);
+
+    // Release the second block. This frees 2048 + 8 bytes.
+    //
+    memmgr_free(p[1]);
+    p[1] = 0;
+
+    // Now we can allocate several smaller chunks from the 
+    // free list. There, they can be smaller than the 
+    // minimal allocation size.
+    // Allocations of 100 require 14 quantas (13 for the 
+    // requested space, 1 for the header). So it allocates
+    // 112 bytes. We have 18 allocations to make:
+    //
+    for (i = 10; i < 28; ++i)
+    {
+        p[i] = memmgr_alloc(100);
+        assert(p[i]);
+    }
+
+    // Not enough for another one...
+    //
+    p[28] = memmgr_alloc(100);
+    assert(p[28] == 0);
+
+    // Now free everything
+    //
+    for (i = 0; i < 30; ++i)
+    {
+        if (p[i])
+            memmgr_free(p[i]);
+    }
+
+    memmgr_print_stats();
+}
+
+
+int main()
+{
+    test_memmgr();
+
+    getchar();
+    return 0;
+}
+
