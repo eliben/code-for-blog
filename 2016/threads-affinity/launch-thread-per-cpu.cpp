0c10861a99fbbaad26e144c9e14f5bb3c2082715
Eli Bendersky <eliben@gmail.com>
2015-12-28 06:27:42 -0800

    launch-thread-per-cpu sample

---
+++ 2016/threads-affinity/launch-thread-per-cpu.cpp
@@ -0,0 +1,40 @@
+// Sample of launching C++11 threads and checking how many CPUs the host system
+// has.
+//
+// Eli Bendersky [http://eli.thegreenplace.net]
+// This code is in the public domain.
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+int main(int argc, const char** argv) {
+  unsigned num_cpus = std::thread::hardware_concurrency();
+  std::cout << "Launching " << num_cpus << " threads\n";
+
+  // std::cout is not guaranteed to be thread safe, so we protect it with a
+  // mutex.
+  std::mutex iomutex;
+  std::vector<std::thread> threads(num_cpus);
+  for (unsigned i = 0; i < num_cpus; ++i) {
+    threads[i] = std::thread([&iomutex, i] {
+      {
+        // Use a lexical scope and lock_guard to safely lock the mutex only for
+        // the duration of std::cout usage.
+        std::lock_guard<std::mutex> iolock(iomutex);
+        std::cout << "Thread #" << i << " is running\n";
+      }
+
+      // Simulate important work done by the tread by sleeping for a bit...
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    });
+  }
+
+  std::for_each(threads.begin(), threads.end(),
+                std::mem_fn(&std::thread::join));
+
+  return 0;
+}
