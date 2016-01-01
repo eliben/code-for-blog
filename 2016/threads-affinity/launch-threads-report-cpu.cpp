9781bd7a2ca64b28f720efdce9693bfb0e027f7b
Eli Bendersky <eliben@gmail.com>
2016-01-01 09:19:10 -0800

    CPU reporting sample

---
+++ 2016/threads-affinity/launch-threads-report-cpu.cpp
@@ -0,0 +1,39 @@
+// Sample of launching C++11 threads that report what CPU they run on.
+//
+// Eli Bendersky [http://eli.thegreenplace.net]
+// This code is in the public domain.
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <sched.h>
+#include <thread>
+#include <vector>
+
+int main(int argc, const char** argv) {
+  constexpr unsigned num_threads = 4;
+  // std::cout is not guaranteed to be thread safe, so we protect it with a
+  // mutex.
+  std::mutex iomutex;
+  std::vector<std::thread> threads(num_threads);
+  for (unsigned i = 0; i < num_threads; ++i) {
+    threads[i] = std::thread([&iomutex, i] {
+      while (1) {
+        {
+          // Use a lexical scope and lock_guard to safely lock the mutex only
+          // for the duration of std::cout usage.
+          std::lock_guard<std::mutex> iolock(iomutex);
+          std::cout << "Thread #" << i << ": on CPU " << sched_getcpu() << "\n";
+        }
+
+        // Simulate important work done by the tread by sleeping for a bit...
+        std::this_thread::sleep_for(std::chrono::milliseconds(900));
+      }
+    });
+  }
+
+  std::for_each(threads.begin(), threads.end(),
+                std::mem_fn(&std::thread::join));
+
+  return 0;
+}
