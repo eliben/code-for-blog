1398b39b8c9d250863b6125431abd24f1dcfc746
Eli Bendersky <eliben@gmail.com>
2016-01-24 15:24:37 -0800

    Add files for persistent history post

---
+++ 2016/persistent-history/add-persistent-history.sh
@@ -0,0 +1,28 @@
+# Add this code to your .bashrc to save persistent history
+#
+# See http://eli.thegreenplace.net/2013/06/11/keeping-persistent-history-in-bash
+# for details.
+#
+# Eli Bendersky (http://eli.thegreenplace.net)
+# This code is in the public domain
+
+log_bash_persistent_history()
+{
+  local rc=$?
+  [[ $(history 1) =~ ^\ *[0-9]+\ +([^\ ]+\ [^\ ]+)\ +(.*)$ ]]
+  local date_part="${BASH_REMATCH[1]}"
+  local command_part="${BASH_REMATCH[2]}"
+  if [ "$command_part" != "$PERSISTENT_HISTORY_LAST" ]
+  then
+    echo $date_part "|" "$command_part" >> ~/.persistent_history
+    export PERSISTENT_HISTORY_LAST="$command_part"
+  fi
+}
+
+# Stuff to do on PROMPT_COMMAND
+run_on_prompt_command()
+{
+    log_bash_persistent_history
+}
+
+PROMPT_COMMAND="run_on_prompt_command"
