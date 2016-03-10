aa94dc9882857b438152fafd53763f842949ad01
Eli Bendersky <eliben@gmail.com>
2016-03-10 06:35:47 -0800

    Initial cut at a gRPC sample server+client
    Similar to my asio/protobuf sample in https://github.com/eliben/code-for-blog/tree/master/2011/asio_protobuf_sample

---
+++ 2016/grpc-stringdb/db_server.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+
+#include <grpc++/grpc++.h>
+#include "stringdb.grpc.pb.h"
+
+class StringDbImpl final : public stringdb::StringDb::Service {
+public:
+  grpc::Status GetValue(grpc::ServerContext* context,
+                        const stringdb::GetValueRequest* request,
+                        stringdb::GetValueReply* reply) override {
+    auto it = string_db_.find(request->key());
+    if (it == string_db_.end()) {
+      reply->set_value("");
+    } else {
+      reply->set_value(it->second);
+    }
+    return grpc::Status::OK;
+  }
+
+  grpc::Status SetValue(grpc::ServerContext* context,
+                        const stringdb::SetValueRequest* request,
+                        stringdb::SetValueReply* reply) override {
+    string_db_[request->key()] = request->value();
+    reply->set_value(request->value());
+    return grpc::Status::OK;
+  }
+
+  grpc::Status CountValue(grpc::ServerContext* context,
+                        const stringdb::CountValueRequest* request,
+                        stringdb::CountValueReply* reply) override {
+    auto it = string_db_.find(request->key());
+    if (it == string_db_.end()) {
+      reply->set_count(-1);
+    } else {
+      reply->set_count(it->second.size());
+    }
+    return grpc::Status::OK;
+  }
+
+private:
+  std::map<std::string, std::string> string_db_;
+};
+
+void RunServer() {
+  // This parts is taken from the "hello world" gRPC sample.
+  std::string server_address("0.0.0.0:4050");
+  StringDbImpl service;
+
+  grpc::ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+
+  return 0;
+}
