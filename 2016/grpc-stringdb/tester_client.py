357d66fafcceb9b4a76b2529caa8e5105b95be16
Eli Bendersky <eliben@gmail.com>
2016-03-10 06:35:47 -0800

    Initial cut at a gRPC sample server+client
    Similar to my asio/protobuf sample in https://github.com/eliben/code-for-blog/tree/master/2011/asio_protobuf_sample

---
+++ 2016/grpc-stringdb/tester_client.py
@@ -0,0 +1,36 @@
+from grpc.beta import implementations
+
+import stringdb_pb2
+
+PORT = 4050
+TIMEOUT_SECONDS = 3
+
+
+def set_value(stub, key, value):
+    request = stringdb_pb2.SetValueRequest(key=key, value=value)
+    response = stub.SetValue(request, TIMEOUT_SECONDS)
+    return response.value
+
+def get_value(stub, key):
+    request = stringdb_pb2.GetValueRequest(key=key)
+    response = stub.GetValue(request, TIMEOUT_SECONDS)
+    return response.value
+
+def count_value(stub, key):
+    request = stringdb_pb2.CountValueRequest(key=key)
+    response = stub.CountValue(request, TIMEOUT_SECONDS)
+    return response.count
+
+
+def main():
+  channel = implementations.insecure_channel('localhost', PORT)
+  stub = stringdb_pb2.beta_create_StringDb_stub(channel)
+
+  set_value(stub, 'foo', 'bar')
+  set_value(stub, 'baz', 'anaconda is here')
+  print get_value(stub, 'foo')
+  print count_value(stub, 'baz')
+
+
+if __name__ == '__main__':
+  main()
