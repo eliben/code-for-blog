91994264509139569bd1bf43db6f9b16b22a365a
Eli Bendersky <eliben@gmail.com>
2016-03-29 06:32:35 -0700

    Starting samples for multiple dispatch

---
+++ 2016/multiple-dispatch/c++/doesntwork.cpp
@@ -0,0 +1,66 @@
+// Eli Bendersky [http://eli.thegreenplace.net]
+// This code is in the public domain.
+#include <iostream>
+#include <memory>
+#include <string>
+
+class Shape {
+public:
+  virtual std::string name() const { return "Shape"; }
+};
+
+class Rectangle : public Shape {
+public:
+  virtual std::string name() const { return "Rectangle"; }
+};
+
+class Ellipse : public Shape {
+public:
+  virtual std::string name() const { return "Ellipse"; }
+};
+
+class Triangle : public Shape {
+public:
+  virtual std::string name() const { return "Triangle"; }
+};
+
+// Overloaded Intersect methods.
+void Intersect(const Rectangle* r, const Ellipse* e) {
+  std::cout << "Rectangle x Ellipse [names r=" << r->name()
+            << ", e=" << e->name() << "]\n";
+}
+
+void Intersect(const Rectangle* r1, const Rectangle* r2) {
+  std::cout << "Rectangle x Rectangle [names r1=" << r1->name()
+            << ", r2=" << r2->name() << "]\n";
+}
+
+// ...
+
+void Intersect(const Shape* s1, const Shape* s2) {
+  std::cout << "Shape x Shape [names s1=" << s1->name() << ", s2=" << s2->name()
+            << "]\n";
+}
+
+int main(int argc, const char** argv) {
+  Rectangle r1, r2;
+  Ellipse e;
+  Triangle t;
+
+  std::cout << "Static type dispatch\n";
+  Intersect(&r1, &e);
+  Intersect(&r1, &r2);
+  Intersect(&r1, &t);
+
+  std::unique_ptr<Shape> pr1(new Rectangle);
+  std::unique_ptr<Shape> pr2(new Rectangle);
+  std::unique_ptr<Shape> pe(new Ellipse);
+  std::unique_ptr<Shape> pt(new Triangle);
+
+  std::cout << "Dynamic type dispatch\n";
+  Intersect(pr1.get(), pe.get());
+  Intersect(pr1.get(), pr2.get());
+  Intersect(pr1.get(), pt.get());
+  
+  return 0;
+}
