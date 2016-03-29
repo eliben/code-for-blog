780aa41262ea691f2dbbc0d02d00346b4951086f
Eli Bendersky <eliben@gmail.com>
2016-03-29 14:22:49 -0700

    a bit of pythonization

--- 2016/multiple-dispatch/python/multi.py
+++ 2016/multiple-dispatch/python/multi.py
@@ -1,24 +1,30 @@
+# Based on Guido's sample in https://www.artima.com/weblogs/viewpost.jsp?thread=101605
 # Tested with Python 3.4
 #
 # Eli Bendersky [http://eli.thegreenplace.net]
 # This code is in the public domain.
 
 class _MultiMethod:
+    """Maps tuples of argument types to function to call for these types."""
     def __init__(self, name):
         self.name = name
         self.typemap = {}
+
     def __call__(self, *args):
         types = tuple(arg.__class__ for arg in args)
-        function = self.typemap.get(types)
-        if function is None:
-            raise TypeError("no match")
+        try:
+            return self.typemap[types](*args)
+        except KeyError:
+            raise TypeError('no match %s for types %s' % (self.name, types))
         return function(*args)
+
     def register_function_for_types(self, types, function):
         if types in self.typemap:
             raise TypeError("duplicate registration")
         self.typemap[types] = function
 
 
+# Maps function.__name__ -> _MultiMethod object.
 _multi_registry = {}
 
 def multimethod(*types):
@@ -63,4 +69,5 @@ if __name__ == '__main__':
     e = Ellipse()
 
     intersect(r1, e)
+    intersect(e, r1)
     intersect(r1, r2)
