d5bb10fec9ebc3273ddcc22cdb45ebeaaf0df47f
Eli Bendersky <eliben@gmail.com>
2016-03-31 06:06:40 -0700

    simple C++ sample for article

---
+++ 2016/multiple-dispatch/c++/singledispatch.cpp
@@ -0,0 +1,36 @@
+// Eli Bendersky [http://eli.thegreenplace.net]
+// This code is in the public domain.
+#include <iostream>
+#include <memory>
+#include <string>
+
+class Shape {
+public:
+  virtual void ComputeArea() const = 0;
+};
+
+class Rectangle : public Shape {
+public:
+  virtual void ComputeArea() const {
+    std::cout << "Rectangle: width times height\n";
+  }
+};
+
+class Ellipse : public Shape {
+public:
+  virtual void ComputeArea() const {
+    std::cout << "Ellipse: width times height times pi/4\n";
+  }
+};
+
+
+int main(int argc, const char** argv) {
+  std::unique_ptr<Shape> pr(new Rectangle);
+  std::unique_ptr<Shape> pe(new Ellipse);
+
+  pr->ComputeArea();
+  pe->ComputeArea();
+  
+  return 0;
+}
+
