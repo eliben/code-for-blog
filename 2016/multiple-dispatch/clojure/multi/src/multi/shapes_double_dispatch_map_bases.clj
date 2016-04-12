daded722f38ccd4e0ab6c9047277f1195f7ec144
Eli Bendersky <eliben@gmail.com>
2016-04-12 11:59:47 -0700

    New Clojure sample with map and bases

---
+++ 2016/multiple-dispatch/clojure/multi/src/multi/shapes_double_dispatch_map_bases.clj
@@ -0,0 +1,35 @@
+; Variation of shapes-double-dispatch but with "base class" functionality
+; and maps instead of records.
+;
+; Eli Bendersky [http://eli.thegreenplace.net]
+; This code is in the public domain.
+(ns multi.shapes-double-dispatch-map-bases)
+
+(derive ::rectangle ::shape)
+(derive ::ellipse ::shape)
+(derive ::triangle ::shape)
+
+(defmulti intersect
+  (fn [a b]
+    [(:kind a) (:kind b)]))
+
+; Helper constructors to create new "instances" of shapes. In a real program
+; they could take parameters that would be assigned to fields.
+(defn make-shape [] {:kind ::shape})
+(defn make-rectangle [] {:kind ::rectangle})
+(defn make-ellipse [] {:kind ::ellipse})
+(defn make-triangle [] {:kind ::triangle})
+
+(defmethod intersect [::rectangle ::ellipse]
+  [r e] (printf "Rectangle x Ellipse"))
+
+(defmethod intersect [::rectangle ::rectangle]
+  [r1 r2] (printf "Rectangle x Rectangle"))
+
+(defmethod intersect [::rectangle ::shape]
+  [r s] (printf "Rectangle x Shape"))
+
+(intersect (make-rectangle) (make-ellipse))
+(intersect (make-rectangle) (make-rectangle))
+(intersect (make-rectangle) (make-shape))
+(intersect (make-rectangle) (make-triangle))
