3b6f29ad64ab6db3b6f8e2072e98c6354c2e48cf
Eli Bendersky <eliben@gmail.com>
2016-04-21 06:35:53 -0700

    Adding basic go server

---
+++ 2016/grpc-stringdb/go/src/db_server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net"
+	pb "stringdb"
+	"sync"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+)
+
+type stringdbServer struct {
+	mu sync.Mutex
+	db map[string]string
+}
+
+func (s *stringdbServer) GetValue(ctx context.Context, r *pb.GetValueRequest) (*pb.GetValueReply, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// Here we rely on the map's default of an empty string when no key exists.
+	return &pb.GetValueReply{s.db[r.Key]}, nil
+}
+
+func (s *stringdbServer) SetValue(ctx context.Context, r *pb.SetValueRequest) (*pb.SetValueReply, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.db[r.Key] = r.Value
+	return &pb.SetValueReply{r.Value}, nil
+}
+
+func (s *stringdbServer) CountValue(ctx context.Context, r *pb.CountValueRequest) (*pb.CountValueReply, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	retval := -1
+	v, ok := s.db[r.Key]
+	if ok {
+		retval = len(v)
+	}
+	return &pb.CountValueReply{int64(retval)}, nil
+}
+
+func newServer() *stringdbServer {
+	s := new(stringdbServer)
+	s.db = make(map[string]string)
+	return s
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":4050")
+	if err != nil {
+		grpclog.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterStringDbServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
