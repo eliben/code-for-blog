0440c944ef53c4d21fe1e94bbb2f755f1e3f98b5
Eli Bendersky <eliben@gmail.com>
2016-04-28 09:50:25 -0700

    Moar

--- 2016/patterns-composite-interpreter/interp.cpp
+++ 2016/patterns-composite-interpreter/interp.cpp
@@ -5,18 +5,21 @@
 #include <sstream>
 #include <string>
 
+// Maps symbol names to their values. An expression is evaluated in the context
+// of a symbol table, in order to assign concrete values to variables referenced
+// within it.
 typedef std::map<std::string, double> SymbolTable;
 
 class Expr {
 public:
-  virtual double Eval(SymbolTable* st = nullptr) const = 0;
+  virtual double Eval(SymbolTable* st) const = 0;
 };
 
 class Constant : public Expr {
 public:
   Constant(double value) : value_(value) {}
 
-  double Eval(SymbolTable* st = nullptr) const {
+  double Eval(SymbolTable* st) const {
     return value_;
   }
 
@@ -28,7 +31,7 @@ class VarRef : public Expr {
 public:
   VarRef(const char* varname) : varname_(varname) {}
 
-  double Eval(SymbolTable* st = nullptr) const {
+  double Eval(SymbolTable* st) const {
     return (*st)[varname_];
   }
 
@@ -36,6 +39,7 @@ private:
   std::string varname_;
 };
 
+// A function type for computing the result of a binary operation.
 typedef std::function<double(double, double)> BinaryFunction;
 
 class BinaryOp : public Expr {
@@ -43,7 +47,7 @@ public:
   BinaryOp(BinaryFunction func, const Expr& lhs, const Expr& rhs)
       : func_(func), lhs_(lhs), rhs_(rhs) {}
 
-  double Eval(SymbolTable* st = nullptr) const {
+  double Eval(SymbolTable* st) const {
     return func_(lhs_.Eval(st), rhs_.Eval(st));
   }
 
@@ -54,15 +58,17 @@ private:
 };
 
 int main(int argc, const char** argv) {
+  // Define a couple of constants and a reference to the variable 'x'.
   std::unique_ptr<Expr> c1(new Constant(2.0));
   std::unique_ptr<Expr> c2(new Constant(3.3));
   std::unique_ptr<Expr> v(new VarRef("x"));
 
-  SymbolTable st{{"x", 1.1}};
-
+  // Define a binary expression representing "2.0 * 3.3 + x"
   std::unique_ptr<Expr> e1(new BinaryOp(std::multiplies<double>(), *c1, *c2));
   std::unique_ptr<Expr> e2(new BinaryOp(std::plus<double>(), *e1, *v));
 
+  // Evaluate in the context of a symbol table where x has the value 1.1
+  SymbolTable st{{"x", 1.1}};
   std::cout << e2->Eval(&st) << "\n";
 
   return 0;
