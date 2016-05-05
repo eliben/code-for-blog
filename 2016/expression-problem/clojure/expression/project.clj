b13eb3b261cec0e5b12bc2916842a312839c55e4
Eli Bendersky <eliben@gmail.com>
2016-05-05 06:53:23 -0700

    Adding basic Clojure multimethod approach

---
+++ 2016/expression-problem/clojure/expression/project.clj
@@ -0,0 +1,9 @@
+(defproject expression "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot expression.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ 2016/expression-problem/clojure/expression/src/expression/multimethod.clj
@@ -0,0 +1,36 @@
+; Eli Bendersky [http://eli.thegreenplace.net]
+; This code is in the public domain.
+(ns expression.multimethod)
+
+(defrecord Constant [value])
+(defrecord BinaryPlus [lhs rhs])
+
+(defmulti evaluate class)
+
+(defmethod evaluate Constant
+  [c] (:value c))
+
+(defmethod evaluate BinaryPlus
+  [bp] (+ (evaluate (:lhs bp)) (evaluate (:rhs bp))))
+
+(defmulti stringify class)
+
+(defmethod stringify Constant
+  [c] (str (:value c)))
+
+(defmethod stringify BinaryPlus
+  [bp]
+  (clojure.string/join " + " [(stringify (:lhs bp))
+                              (stringify (:rhs bp))]))
+
+(def c1 (Constant. 1.1))
+(def c2 (Constant. 2.2))
+(def p1 (BinaryPlus. c1 c2))
+(def p2 (BinaryPlus. p1 c2))
+
+(evaluate c1)
+(evaluate p1)
+(evaluate p2)
+
+(stringify p1)
+(stringify p2)
