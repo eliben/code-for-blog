97b848f5577c0740070f693b3acde79618eee372
Eli Bendersky <eliben@gmail.com>
2016-09-20 06:56:27 -0700

    Cosmetic fixes

--- 2016/readline-samples/readline-keep-history.cpp
+++ 2016/readline-samples/readline-keep-history.cpp
@@ -15,18 +15,18 @@ int main(int argc, char** argv) {
   if (argc > 1 && std::string(argv[1]) == "-d") {
     // By default readline does filename completion. With -d, we disable this
     // by asking readline to just insert the TAB character itself.
-    ::rl_bind_key('\t', rl_insert);
+    rl_bind_key('\t', rl_insert);
   }
 
   char* buf;
   while ((buf = ::readline(">> ")) != nullptr) {
     if (strlen(buf) > 0) {
-      ::add_history(buf);
+      add_history(buf);
     }
 
     printf("[%s]\n", buf);
 
-    // ::readline malloc's a new buffer every time.
+    // readline malloc's a new buffer every time.
     free(buf);
   }
 
