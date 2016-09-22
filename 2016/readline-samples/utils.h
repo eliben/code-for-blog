d2ce058d8df21e478536756e9a65f54508332af9
Eli Bendersky <eliben@gmail.com>
2016-09-22 07:03:48 -0700

    Move hierarchical's utils to utils.h too

--- 2016/readline-samples/utils.h
+++ 2016/readline-samples/utils.h
@@ -14,4 +14,18 @@
 std::string longest_common_prefix(std::string s,
                                   const std::vector<std::string>& candidates);
 
+// Represents tokens in the line buffer. For each token, we have its text, as
+// well as its index in the buffer (the offset of its first character from the
+// buffer's beginning).
+struct Token {
+  std::string text;
+  size_t buf_index;
+};
+
+// Split the given buffer to a vector of Tokens.
+std::vector<Token> tokenize_line_buffer(const std::string& buf);
+
+// Show a vector of tokens by printing it to stdout.
+void show_tokens(const std::vector<Token>& tv);
+
 #endif /* UTILS_H */
