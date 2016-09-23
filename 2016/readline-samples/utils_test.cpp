b6c40c20844b9ff0b0a13b86458d140f895bb341
Eli Bendersky <eliben@gmail.com>
2016-09-22 20:00:34 -0700

    Added unit tests for tokenize_line_buffer

--- 2016/readline-samples/utils_test.cpp
+++ 2016/readline-samples/utils_test.cpp
@@ -18,12 +18,35 @@ void test_longest_common_prefix() {
   assert(longest_common_prefix("k", {"kba1", "kba2", "kba3"}) == "kba");
 }
 
+
+void test_tokenize_line_buffer() {
+  std::vector<Token> t1 = tokenize_line_buffer("one \ttwo three");
+  assert(t1.size() == 3);
+  assert(t1[0].text == "one");
+  assert(t1[0].buf_index == 0);
+  assert(t1[1].text == "two");
+  assert(t1[1].buf_index == 5);
+  assert(t1[2].text == "three");
+  assert(t1[2].buf_index == 9);
+
+  std::vector<Token> t2 = tokenize_line_buffer("  ");
+  assert(t2.size() == 0);
+
+  std::vector<Token> t3 = tokenize_line_buffer(" \t\tfoo  ");
+  assert(t3.size() == 1);
+  assert(t3[0].text == "foo");
+  assert(t3[0].buf_index == 3);
+}
+
+
 int main(int argc, const char** argv) {
 #ifdef NDEBUG
   std::cout << "NDEBUG defined: assertions disabled\n";
 #endif
 
   test_longest_common_prefix();
+  test_tokenize_line_buffer();
+
   std::cout << "OK!\n";
   return 0;
 }
