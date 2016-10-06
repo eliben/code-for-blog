77ee0f7eaf44f7fdc067dab38e20fe831b7004b1
Eli Bendersky <eliben@google.com>
2016-10-06 16:01:19 -0700

    Clean up and add comments

--- 2016/readline-samples/readline-complete-subcommand.cpp
+++ 2016/readline-samples/readline-complete-subcommand.cpp
@@ -17,18 +17,20 @@
 
 #include "utils.h"
 
-// Global logfile for debugging.
+// Global logfile for debugging. Using a separate file so that output doesn't
+// intermix with the actual readline terminal. That file can be watched with
+// "tail -f" in a separate window.
 FILE* logfile;
 
+// Mapping commands to possible subcommands. Populate this map with
+// register_command.
 using CommandMap = std::unordered_map<std::string, std::vector<std::string>>;
-
 CommandMap command_map;
 
 void register_command(const std::string& command, const
                       std::vector<std::string>& subcommands) {
   command_map[command] = subcommands;
 }
-
 
 char** completer(const char* text, int start, int end) {
   fprintf(logfile, "[%s (%d:%d)]\n", text, start, end);
@@ -40,20 +42,39 @@ char** completer(const char* text, int start, int end) {
   size_t cursor = start;
   if (line_tokens.size() == 0 ||
       (line_tokens.size() == 1 && cursor == line_tokens[0].buf_index)) {
-    // If start points at the beginning of this token, this means we're still
-    // completing it. So we have to complete a command.
+    // If either:
+    //
+    // - There are no tokens yet, or
+    // - There's one token already and start points at its beginning of
+    //
+    // This means we're trying to complete the first token -- a command.
     for (auto& kv : command_map) {
       vocabulary.push_back(kv.first);
     }
   } else if ((line_tokens.size() == 1 &&
               cursor > line_tokens[0].buf_index + line_tokens[0].text.size()) ||
              (line_tokens.size() == 2 && cursor == line_tokens[1].buf_index)) {
-    // Completing second token.
-    const auto it = command_map.find(line_tokens[0].text);
+    // If either:
+    //
+    // - There is a single token already and starts points past its end, or
+    // - There are two tokens already and start points at the beginning of the
+    //   second
+    //
+    // This means we're trying to complete the second token -- a subcommand.
+
+    const std::string command = line_tokens[0].text;
+    if (command == "file") {
+      // File is special. We attempt filename completion.
+      rl_attempted_completion_over = 0;
+      return nullptr;
+    }
+
+    const auto it = command_map.find(command);
     if (it != command_map.end()) {
       vocabulary = it->second;
     }
   } else {
+    // Otherwise don't complete at all.
     rl_attempted_completion_over = 1;
     return nullptr;
   }
@@ -93,6 +114,7 @@ int main(int argc, char** argv) {
     std::cout << "Unable to open log file for writing!\n";
     return 1;
   }
+  // Disable buffering so that logging appears immediately.
   setbuf(logfile, NULL);
 
   printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
