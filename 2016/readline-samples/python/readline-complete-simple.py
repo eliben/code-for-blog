da52eb29d44cde34d34da6aa4ee643f15aca5f9e
Eli Bendersky <eliben@google.com>
2016-10-07 12:32:15 -0700

    Ass "simple" completion sample equivalent in Python

---
+++ 2016/readline-samples/python/readline-complete-simple.py
@@ -0,0 +1,32 @@
+# Simple completion with the readline module.
+#
+# Tested with Python 3.4
+#
+# Eli Bendersky [http://eli.thegreenplace.net]
+# This code is in the public domain.
+import readline
+
+
+def make_completer(vocabulary):
+    def custom_complete(text, state):
+        # None is returned for the end of the completion session.
+        results = [x for x in vocabulary if x.startswith(text)] + [None]
+        return results[state]
+    return custom_complete
+
+
+def main():
+    vocabulary = {'cat', 'dog', 'canary', 'cow', 'hamster'}
+    readline.parse_and_bind('tab: complete')
+    readline.set_completer(make_completer(vocabulary))
+
+    try:
+        while True:
+            s = input('>> ').strip()
+            print('[{0}]'.format(s))
+    except (EOFError, KeyboardInterrupt) as e:
+        print('\nShutting down...')
+
+
+if __name__ == '__main__':
+    main()
