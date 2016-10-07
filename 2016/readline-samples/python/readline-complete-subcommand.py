42b17a5de8fa20b5d54f7800a517bb8e6232ce7a
Eli Bendersky <eliben@google.com>
2016-10-07 15:53:28 -0700

    Implement simple completion for 'file' commands in the subcommand completer

--- 2016/readline-samples/python/readline-complete-subcommand.py
+++ 2016/readline-samples/python/readline-complete-subcommand.py
@@ -4,30 +4,46 @@
 #
 # Eli Bendersky [http://eli.thegreenplace.net]
 # This code is in the public domain.
+import glob
 import readline
 
 
 def make_subcommand_completer(commands):
     def custom_complete(text, state):
+        # Simplistic parsing of the command-line so far. We want to know if the
+        # user is still entering the command, or if the command is already there
+        # and now we have to complete the subcommand.
         linebuf = readline.get_line_buffer()
         parts = linebuf.split()
 
-        # If we're done with the first word, means we're looking for the 2nd
-        # part.
-        if len(linebuf) == 0 or linebuf.endswith(' '):
+        if len(parts) >= 1 and linebuf.endswith(' '):
+            # If we're past the first part and there is whitespace at the end of
+            # the buffer, it means we're already completing the next part.
             parts.append('')
 
-        if len(parts) == 1:
-            return [w for w in commands.keys() if w.startswith(text)][state]
+        if len(parts) <= 1:
+            matches = [w for w in commands.keys()
+                         if w.startswith(text)] + [None]
+            return matches[state]
         elif len(parts) == 2:
-            return [w for w in commands[parts[0]] if w.startswith(text)][state]
+            command = parts[0]
+
+            if command == 'file':
+                # Treat 'file' specially, by looking for matching files in the
+                # current directory.
+                matches = glob.glob(parts[1] + '*') + [None]
+            else:
+                matches = [w for w in commands[command]
+                             if w.startswith(text)] + [None]
+            return matches[state]
     return custom_complete
 
 
 def main():
     commands = {
         'season': {'winter', 'spring', 'summer', 'fall'},
-        'animal': {'cat', 'dog', 'canary', 'cow', 'hamster'}
+        'animal': {'cat', 'dog', 'canary', 'cow', 'hamster'},
+        'file': {},
     }
     readline.parse_and_bind('tab: complete')
     readline.set_completer(make_subcommand_completer(commands))
