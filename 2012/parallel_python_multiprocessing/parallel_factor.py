28c5dba9889ce6e9288830221bff0b24503d04b0
Eli Bendersky <eliben@gmail.com>
2017-01-31 05:24:19 -0800

    Move mp_worker out of mp_factorizer
    multiprocessing uses pickle which may have issues serializing nested functions
    on some platforms. There have been reports by users on Windows that this code
    raises pickling errors - this is an attempt to mitigate the issue (though it
    works fine for me on Ubuntu).

--- 2012/parallel_python_multiprocessing/parallel_factor.py
+++ 2012/parallel_python_multiprocessing/parallel_factor.py
@@ -91,16 +91,18 @@ def threaded_factorizer(nums, nthreads):
     return {k: v for out_d in outs for k, v in out_d.items()}
 
 
+def mp_worker(nums, out_q):
+    """ The worker function, invoked in a process. 'nums' is a
+        list of numbers to factor. The results are placed in
+        a dictionary that's pushed to a queue.
+    """
+    outdict = {}
+    for n in nums:
+        outdict[n] = factorize_naive(n)
+    out_q.put(outdict)
+
+
 def mp_factorizer(nums, nprocs):
-    def worker(nums, out_q):
-        """ The worker function, invoked in a process. 'nums' is a
-            list of numbers to factor. The results are placed in
-            a dictionary that's pushed to a queue.
-        """
-        outdict = {}
-        for n in nums:
-            outdict[n] = factorize_naive(n)
-        out_q.put(outdict)
 
     # Each process will get 'chunksize' nums and a queue to put his out
     # dict into
@@ -110,7 +112,7 @@ def mp_factorizer(nums, nprocs):
 
     for i in range(nprocs):
         p = multiprocessing.Process(
-                target=worker,
+                target=mp_worker,
                 args=(nums[chunksize * i:chunksize * (i + 1)],
                       out_q))
         procs.append(p)
