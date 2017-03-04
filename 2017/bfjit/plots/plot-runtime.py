1d7f8090b95d3c47e86b40422c878eff01b19e90
Eli Bendersky <eliben@gmail.com>
2017-03-04 06:46:37 -0800

    Adding more plot to plot-runtime

--- 2017/bfjit/plots/plot-runtime.py
+++ 2017/bfjit/plots/plot-runtime.py
@@ -6,25 +6,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
 
-mandelbrot_runtime = (38.6, 18.4)
-factor_runtime = (16.5, 6.7)
+simple_runtime = (38.6, 16.5)
+opt_runtime = (18.4, 6.7)
+opt2_runtime = (11.9, 3.7)
 
-N = len(mandelbrot_runtime)
+N = len(simple_runtime)
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.25       # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, mandelbrot_runtime, width)
-
-rects2 = ax.bar(ind + width, factor_runtime, width, color='y')
+rects1 = ax.bar(ind, simple_runtime, width)
+rects2 = ax.bar(ind + width, opt_runtime, width, color='y')
+rects3 = ax.bar(ind + width * 2, opt2_runtime, width, color='orange')
 
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Run-time (sec)', fontsize=14)
 ax.set_xticks(ind + width)
 ax.set_xticklabels(('mandelbrot', 'factor'), fontsize=14)
 
-ax.legend((rects1[0], rects2[0]), ('simpleinterp', 'optinterp'), fontsize=14)
+ax.legend((rects1[0],
+           rects2[0],
+           rects3[0]),
+           ('simpleinterp', 'optinterp', 'optinterp2'), fontsize=14)
 
 fig = plt.gcf()
 fig.set_tight_layout(True)
