746dbe5996d398489692c2bbbec8ca7c7593228a
Eli Bendersky <eliben@gmail.com>
2017-03-04 20:04:34 -0800

    Fix width of plotted bars and location of x ticks

--- 2017/bfjit/plots/plot-runtime.py
+++ 2017/bfjit/plots/plot-runtime.py
@@ -9,26 +9,33 @@ import seaborn
 simple_runtime = (38.6, 16.5)
 opt_runtime = (18.4, 6.7)
 opt2_runtime = (11.9, 3.7)
+opt3_runtime = (3.9, 1.97)
 
 N = len(simple_runtime)
 
 ind = np.arange(N)  # the x locations for the groups
-width = 0.25       # the width of the bars
+width = 0.18       # the width of the bars
 
 fig, ax = plt.subplots()
 rects1 = ax.bar(ind, simple_runtime, width)
 rects2 = ax.bar(ind + width, opt_runtime, width, color='y')
 rects3 = ax.bar(ind + width * 2, opt2_runtime, width, color='orange')
+rects4 = ax.bar(ind + width * 3, opt3_runtime, width, color='red')
 
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Run-time (sec)', fontsize=14)
-ax.set_xticks(ind + width)
+ax.set_xticks(ind + width * 2.0)
 ax.set_xticklabels(('mandelbrot', 'factor'), fontsize=14)
 
 ax.legend((rects1[0],
            rects2[0],
-           rects3[0]),
-           ('simpleinterp', 'optinterp', 'optinterp2'), fontsize=14)
+           rects3[0],
+           rects4[0]),
+           ('simpleinterp',
+            'optinterp',
+            'optinterp2',
+            'optinterp3'
+            ), fontsize=14)
 
 fig = plt.gcf()
 fig.set_tight_layout(True)
