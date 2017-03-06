81f0008f50c1567ae1bc730dd6fbd21452ce529e
Eli Bendersky <eliben@gmail.com>
2017-03-06 07:02:06 -0800

    Update plot to only compare opt3 vs. simplejit
    [will have to revert to earlier revisions for this file to see interpreter comparisons]

--- 2017/bfjit/plots/plot-runtime.py
+++ 2017/bfjit/plots/plot-runtime.py
@@ -6,35 +6,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
 
-simple_runtime = (38.6, 16.5)
-opt_runtime = (18.4, 6.7)
-opt2_runtime = (11.9, 3.7)
+#simple_runtime = (38.6, 16.5)
+#opt_runtime = (18.4, 6.7)
+#opt2_runtime = (11.9, 3.7)
 opt3_runtime = (3.9, 1.97)
+sjit_runtime = (2.89, 0.94)
 
-N = len(simple_runtime)
+N = len(opt3_runtime)
 
 ind = np.arange(N)  # the x locations for the groups
 width = 0.18       # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(ind, simple_runtime, width)
-rects2 = ax.bar(ind + width, opt_runtime, width, color='y')
-rects3 = ax.bar(ind + width * 2, opt2_runtime, width, color='orange')
-rects4 = ax.bar(ind + width * 3, opt3_runtime, width, color='red')
+rects1 = ax.bar(ind, opt3_runtime, width, color='r')
+rects2 = ax.bar(ind + width, sjit_runtime, width, color='g')
 
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Run-time (sec)', fontsize=14)
-ax.set_xticks(ind + width * 2.0)
+ax.set_xticks(ind + width)
 ax.set_xticklabels(('mandelbrot', 'factor'), fontsize=14)
 
 ax.legend((rects1[0],
-           rects2[0],
-           rects3[0],
-           rects4[0]),
-           ('simpleinterp',
-            'optinterp',
-            'optinterp2',
-            'optinterp3'
+           rects2[0]),
+           ('opt3interp',
+            'simplejit',
             ), fontsize=14)
 
 fig = plt.gcf()
