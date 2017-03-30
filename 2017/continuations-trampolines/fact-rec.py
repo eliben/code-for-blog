6a1f4e1aa2b6ce7256d6095e67dfca0945f66ade
Eli Bendersky <eliben@gmail.com>
2017-03-30 07:04:03 -0700

    add some cps

--- 2017/continuations-trampolines/fact-rec.py
+++ 2017/continuations-trampolines/fact-rec.py
@@ -15,6 +15,18 @@ def fact_tailrec(n, result=1):
     else:
         return fact_tailrec(n - 1, result * n)
 
+end_cont = lambda value: value
+
+def fact_cps(n, cont):
+    if n == 0:
+        return cont(1)
+    else:
+        return fact_cps(n - 1, lambda value: cont(n * value))
+
+
+def fact_cps_main(n):
+    return fact_cps(n, end_cont)
+
 
 if __name__ == '__main__':
     import sys
@@ -22,3 +34,5 @@ if __name__ == '__main__':
 
     print(fact_rec(19))
     print(fact_tailrec(19))
+
+    print(fact_cps_main(19))
