ece752399864dff6a44e688788ec149d961930a0
Eli Bendersky <eliben@gmail.com>
2017-03-30 05:38:16 -0700

    Adding more examples

---
+++ 2017/continuations-trampolines/mergesort.py
@@ -0,0 +1,46 @@
+# Merge sort.
+#
+# Eli Bendersky [http://eli.thegreenplace.net]
+# This code is in the public domain.
+
+def merge_sort(lst):
+    n = len(lst)
+    if n <= 1:
+        return lst
+    else:
+        mid = int(n / 2)
+        return merge(merge_sort(lst[:mid]), merge_sort(lst[mid:]))
+
+def merge(lst1, lst2):
+    """Merges two sorted lists into a single sorted list.
+
+    Returns new list. lst1 and lst2 are destroyed in the process."""
+    result = []
+    while lst1 or lst2:
+        if not lst1:
+            return result + lst2
+        elif not lst2:
+            return result + lst1
+        if lst1[0] < lst2[0]:
+            # Note: pop(0) may be slow -- this isn't optimized code.
+            result.append(lst1.pop(0))
+        else:
+            result.append(lst2.pop(0))
+    return result
+
+def test_sorter(sortfunc):
+    assert sortfunc([1]) == [1]
+    assert sortfunc([]) == []
+    assert sortfunc([1, 2]) == [1, 2]
+    assert sortfunc([3, 2]) == [2, 3]
+    assert sortfunc([4, 3, 2]) == [2, 3, 4]
+    assert sortfunc([1, 3, 2]) == [1, 2, 3]
+    assert sortfunc([7, 1, 3, 2]) == [1, 2, 3, 7]
+    assert sortfunc([7, 1, 5, 3, 2]) == [1, 2, 3, 5, 7]
+    assert sortfunc([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]) == list(range(10))
+    assert sortfunc([1, 8, 7, 6, 5, 4, 3, 2, 9, 0]) == list(range(10))
+    assert sortfunc([4, 3, 7, 6, 5, 0, 1, 2, 9, 8]) == list(range(10))
+
+
+if __name__ == '__main__':
+    test_sorter(merge_sort)
---
+++ 2017/continuations-trampolines/rdparser-cps.py
@@ -0,0 +1,76 @@
+# Grammar:
+#
+# <expr>    : <term> + <expr>
+#             <term>
+# <term>    : <factor> * <factor>
+#             <factor>
+# <factor>  : <number>
+#           | '(' <expr> ')'
+# <number>  : \d+
+#
+# Note: our implementation here is simplistic, and suffers from the
+# associativity problem described in
+# http://eli.thegreenplace.net/2009/03/14/some-problems-of-recursive-descent-parsers/
+
+def parse_expr(tokens):
+    lval = parse_term(tokens)
+    if len(tokens) == 0:
+        return lval
+    op_tok = tokens[0]
+    if op_tok == '+':
+        op = lambda a, b: a + b
+    else:
+        return lval
+    tokens.pop(0)
+    rval = parse_expr(tokens)
+    return op(lval, rval)
+
+def parse_term(tokens):
+    lval = parse_factor(tokens)
+    if len(tokens) == 0:
+        return lval
+    op_tok = tokens[0]
+    if op_tok == '*':
+        op = lambda a, b: a * b
+    else:
+        return lval
+    tokens.pop(0)
+    rval = parse_term(tokens)
+    return op(lval, rval)
+
+def parse_factor(tokens):
+    tok = tokens.pop(0)
+    try:
+        toknum = int(tok)
+        return toknum
+    except ValueError:
+        pass
+    if tok == '(':
+        exprval = parse_expr(tokens)
+    if tokens[0] != ')':
+        raise RuntimeError('expected matching ")", got', tokens[0])
+    tokens.pop(0)
+    return exprval
+
+
+e = '2 + 3 * ( 5 + 4 * 2 )'
+etoks = e.split()
+print('parsing:', etoks)
+print(parse_expr(etoks))
+
+
+def test_parser(parserfunc):
+    assert parserfunc('2'.split()) == 2
+    assert parserfunc('2 * 4'.split()) == 8
+    assert parserfunc('2 + 4'.split()) == 6
+    assert parserfunc('2 + 4 + 15'.split()) == 21
+    assert parserfunc('2 * 4 + 15'.split()) == 23
+    assert parserfunc('2 + 4 * 15'.split()) == 62
+    assert parserfunc('( 2 + 4 ) * 15'.split()) == 90
+    assert parserfunc('2 + ( 4 * 15 )'.split()) == 62
+    assert parserfunc('2 * ( 4 + 15 )'.split()) == 38
+    assert parserfunc('2 * ( 4 + 15 ) * 3 '.split()) == 114
+    assert parserfunc('( 4 * ( 3 + 7 ) ) * 3 '.split()) == 120
+
+
+test_parser(parse_expr)
