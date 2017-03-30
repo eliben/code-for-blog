cd4d267956fce135932bd05871611caacd9e5dc8
Eli Bendersky <eliben@gmail.com>
2017-03-30 06:36:03 -0700

    State machine example with recursive calls works now

--- 2017/continuations-trampolines/statemachine.py
+++ 2017/continuations-trampolines/statemachine.py
@@ -2,17 +2,16 @@
 #
 # Eli Bendersky [http://eli.thegreenplace.net]
 # This code is in the public domain.
-class ProtocolWrapper(object):
-    def __init__(self,
-            header=0x61,
-            footer=0x62,
-            dle=0xAB,
-            after_dle_func=lambda x: x):
-        self.header = header
-        self.footer = footer
-        self.dle = dle
-        self.after_dle_func = after_dle_func
 
+# The framing protocol is as described in http://eli.thegreenplace.net/2009/08/29/co-routines-as-an-alternative-to-state-machines
+HEADER = 0x61
+FOOTER = 0x62
+DLE = 0xAB
+
+
+# Basic state machine
+class ProtocolWrapper:
+    def __init__(self):
         self.state = self.WAIT_HEADER
         self.frame = bytearray()
 
@@ -25,27 +24,77 @@ class ProtocolWrapper(object):
             frame is returned. Otherwise, None
             is returned.
         """
-        print('got byte: "{0}"'.format(byte))
         if self.state == self.WAIT_HEADER:
-            if byte == self.header:
+            if byte == HEADER:
                 self.state = self.IN_MSG
                 self.frame = bytearray()
             return None
         elif self.state == self.IN_MSG:
-            if byte == self.footer:
+            if byte == FOOTER:
                 self.state = self.WAIT_HEADER
                 return self.frame
-            elif byte == self.dle:
+            elif byte == DLE:
                 self.state = self.AFTER_DLE
             else:
                 self.frame.append(byte)
             return None
         elif self.state == self.AFTER_DLE:
-            self.frame.append(self.after_dle_func(byte))
+            self.frame.append(byte)
             self.state = self.IN_MSG
             return None
         else:
             raise AssertionError()
+
+
+# State machine using recursive tail calls. Avoiding python generators on
+# purpose to demonstrate a point - generators/yield provide for a better general
+# approach in Python.
+class ProtocolStateRecursive:
+    def __init__(self, input_feed, frame_callback):
+        """Creates a new protocol.
+
+        input_feed:
+            A function that provides the next byte when called. If it returns
+            None, the protocol stops.
+        frame_callback:
+            A function that will be called for every decoded frame, with the
+            frame as the single argument.
+        """
+        self.input_feed = input_feed
+        self.frame_callback = frame_callback
+        self.frame = bytearray()
+        self.state_wait_header()
+
+    def state_wait_header(self):
+        byte = self.input_feed()
+        if byte is None:
+            return
+        elif byte == HEADER:
+            self.frame = bytearray()
+            self.state_in_msg()
+        else:
+            self.state_wait_header()
+
+    def state_in_msg(self):
+        byte = self.input_feed()
+        if byte is None:
+            return
+        elif byte == FOOTER:
+            self.frame_callback(self.frame)
+            self.state_wait_header()
+        elif byte == DLE:
+            self.state_after_dle()
+        else:
+            self.frame.append(byte)
+            self.state_in_msg()
+
+    def state_after_dle(self):
+        byte = self.input_feed()
+        if byte is None:
+            return
+        else:
+            self.frame.append(byte)
+            self.state_in_msg()
 
 
 if __name__ == "__main__":
@@ -62,3 +111,18 @@ if __name__ == "__main__":
         frame = pw.input(byte)
         if frame:
             print('Got frame:', codecs.encode(frame, 'hex'))
+
+    byteptr = 0
+    def bytes_feeder():
+        global byteptr
+        if byteptr >= len(bytes):
+            return None
+        else:
+            nextbyte = bytes[byteptr]
+            byteptr += 1
+            return nextbyte
+
+    def frame_callback(frame):
+        print('Callback got frame:', codecs.encode(frame, 'hex'))
+
+    psr = ProtocolStateRecursive(bytes_feeder, frame_callback)
