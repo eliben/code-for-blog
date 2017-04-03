39a283f2fa9c317245d0d973a98249565c331fb5
Eli Bendersky <eliben@gmail.com>
2017-04-03 06:55:50 -0700

    Add trampolined version of evenodd
    + rename fact

--- 2017/continuations-trampolines/evenodd.py
+++ 2017/continuations-trampolines/evenodd.py
@@ -1,13 +1,15 @@
-# Examples of mutual recursion with even/odd.
+# Example of mutual recursion with even/odd.
 #
 # Eli Bendersky [http://eli.thegreenplace.net]
 # This code is in the public domain.
+
 
 def is_even(n):
     if n == 0:
         return True
     else:
         return is_odd(n - 1)
+
 
 def is_odd(n):
     if n == 0:
@@ -16,5 +18,30 @@ def is_odd(n):
         return is_even(n - 1)
 
 
+def is_even_thunked(n):
+    if n == 0:
+        return True
+    else:
+        return lambda: is_odd_thunked(n - 1)
+
+
+def is_odd_thunked(n):
+    if n == 0:
+        return False
+    else:
+        return lambda: is_even_thunked(n - 1)
+
+
+def trampoline(f, *args):
+    v = f(*args)
+    while callable(v):
+        v = v()
+    return v
+
+
 if __name__ == '__main__':
     print(is_even(800))
+
+    # If I try to run is_even(1000) with the default system recursion limit, it
+    # blows up. But trampolining keeps the stack depth constant and small.
+    print(trampoline(is_even_thunked, 1000))
