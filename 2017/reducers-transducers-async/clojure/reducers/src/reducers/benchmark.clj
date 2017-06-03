d83bb680173603b6ac3441b601a0048028412a3f
Eli Bendersky <eliben@gmail.com>
2017-06-03 09:30:48 -0700

    Moved benchmark file to proper location and added manually fused code

--- 2017/reducers-transducers-async/clojure/reducers/src/reducers/benchmark.clj
+++ 2017/reducers-transducers-async/clojure/reducers/src/reducers/benchmark.clj
@@ -4,3 +4,10 @@
 ;(def s (range 0 9999999))
 ;(time (reduce + 0 (map inc (filter even? s))))
 ;(time (reduce + 0 (r/map inc (r/filter even? s)))))
+
+(def s (range 0 999))
+(reduce (fn [acc item] (if (even? item)
+                         (+ acc (inc item))
+                         acc))
+        0 s)
+
