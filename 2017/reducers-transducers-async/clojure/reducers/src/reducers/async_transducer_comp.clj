83f060edd4e196ae94de2fe8286b531401db49e3
Eli Bendersky <eliben@gmail.com>
2017-06-04 13:36:31 -0700

    Composing transducers for async/chan

--- 2017/reducers-transducers-async/clojure/reducers/src/reducers/async_transducer_comp.clj
+++ 2017/reducers-transducers-async/clojure/reducers/src/reducers/async_transducer_comp.clj
@@ -2,7 +2,7 @@
 ;;; 
 ;;; Eli Bendersky [http://eli.thegreenplace.net]
 ;;; This code is in the public domain. 
-(ns reducers.async-transducer
+(ns reducers.async-transducer-comp
   (:require [clojure.core.async :as async]))
 
 (defn square [x] (* x x))
@@ -14,3 +14,11 @@
     (map square)
     (map inc)))
 
+(def c (async/chan 1 xform))
+
+(async/go
+  (async/onto-chan c [5 6 8 12 15]))
+(loop [n (async/<!! c)]
+  (when n
+    (println n)
+    (recur (async/<!! c))))
