38c27842ba2c490ba5d6b10be3419a94928f2b6a
Eli Bendersky <eliben@gmail.com>
2017-06-15 20:31:24 -0700

    HTTP client sample with async mode

---
+++ 2017/clojure-blocking-async/src/clojure_blocking_async/http_client_async.clj
@@ -0,0 +1,35 @@
+;;; Concurrent HTTP client using the asynchronous API of clj-http in conjunction
+;;; with go-blocks.
+(ns clojure-blocking-async.http-client-async
+  (:require [clojure.core.async :as async])
+  (:require [clj-http.client]))
+
+(def url-template "https://github.com/eliben/pycparser/pull/%d")
+
+(defn go-async-generator
+  [c start n]
+  (doseq [i (range start (+ start n))]
+    (clj-http.client/get
+      (format url-template i)
+      {:async? true}
+      (fn [response]
+        (async/go (async/>! c response)))
+      ;; Exception callback.
+      (fn [exc]
+        (throw exc)))))
+
+(defn get-multiple
+  [generator-fn start n]
+  (let [c (async/chan)]
+    (generator-fn c start n)
+    (loop [i 0
+           res []]
+      (if (= i n)
+        res
+        (recur (inc i) (conj res (async/<!! c)))))))
+
+(def start 10)
+(def num-results 20)
+
+(let [results (time (get-multiple go-async-generator start num-results))]
+  (printf "Have %d results%n" (count results)))
