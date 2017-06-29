f1e50c1caeeec5b8e443f634534bfed46f26dbdf
Eli Bendersky <eliben@gmail.com>
2017-06-29 05:54:07 -0700

    Modify client to read the socket concurrently

--- 2017/async-socket-server/simple-client.py
+++ 2017/async-socket-server/simple-client.py
@@ -1,21 +1,37 @@
 import sys, time
 import socket
+import threading
+
+
+class ReadThread(threading.Thread):
+    def __init__(self, sockobj):
+        super().__init__()
+        self.sockobj = sockobj
+        self.bufsize = 8 * 1024
+
+    def run(self):
+        while True:
+            buf = self.sockobj.recv(self.bufsize)
+            print('Received:', buf)
+            if b'1111' in buf:
+                break
 
 
 def make_new_connection(name, host, port):
     sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sockobj.connect((host, port))
 
+    rthread = ReadThread(sockobj)
+    rthread.start()
     sockobj.send(b'foo^1234$jo')
+    time.sleep(1.0)
     sockobj.send(b'sdfsdfsdfsdf^a')
+    time.sleep(1.0)
     sockobj.send(b'fkfkf0000$dfk^$sdf^a$^kk$')
-
-    buf = b''
-    while True:
-        buf += sockobj.recv(1024)
-        print(buf)
+    time.sleep(1.0)
 
     sockobj.close()
+    rthread.join()
 
 
 def main():
