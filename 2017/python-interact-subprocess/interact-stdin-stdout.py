b398750bd28c4386107ca67d58a0daf6afd9640c
Eli Bendersky <eliben@gmail.com>
2017-07-02 15:32:58 -0700

    Updating initial samples

--- 2017/python-interact-subprocess/interact-stdin-stdout.py
+++ 2017/python-interact-subprocess/interact-stdin-stdout.py
@@ -8,22 +8,21 @@ def main():
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
 
-    # communicate will close stdin in the end...
+    # communicate() will close stdin in the end, so the python subprocess will
+    # exit.
+
+    # To avoid deadlocks: careful to: add \n to output, flush output, use
+    # readline() rather than read()
     proc.stdin.write(b'2+2\n')
-    proc.stdin.flush()
-    time.sleep(0.1)
+    proc.stdin.flush() # without this it hangs...
     print(proc.stdout.readline())
 
     proc.stdin.write(b'2+3\n')
     proc.stdin.flush()
-    time.sleep(0.1)
     print(proc.stdout.readline())
-    #out, _ = proc.communicate(b'2+2\n')
-    #print(out)
-    #print(proc.returncode)
 
-    #out, _ = proc.communicate(b'3+2\n')
-    #print(out)
+    proc.terminate()
+    proc.wait(timeout=0.2)
 
 
 if __name__ == '__main__':
