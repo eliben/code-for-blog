e78316d811f12fa5fcee086b08dd63df5ae087b7
Eli Bendersky <eliben@gmail.com>
2017-07-03 05:36:37 -0700

    Refine interact sample + add helper emitter script

---
+++ 2017/python-interact-subprocess/emitter.py
@@ -0,0 +1,16 @@
+import sys
+import time
+
+
+def main():
+    count = 1
+    while True:
+        sys.stdout.write(f'{count} ')
+        if count % 20 == 0:
+            sys.stdout.write('\n')
+        time.sleep(0.05)
+        count += 1
+
+
+if __name__ == '__main__':
+    main()
