860a517c4eaa7fedfd46747038ca60bf2f24965b
Eli Bendersky <eliben@gmail.com>
2017-10-21 10:20:09 -0700

    Implementing peer connection callback

--- 2017/async-socket-server/uv-server.c
+++ 2017/async-socket-server/uv-server.c
@@ -9,10 +9,44 @@
 #define N_BACKLOG 64
 
 
+// Wraps malloc with error checking: dies if malloc fails.
+void* xmalloc(size_t size) {
+  void* ptr = malloc(size);
+  if (!ptr) {
+    die("malloc failed");
+  }
+  return ptr;
+}
+
+
+void on_handle_closed(uv_handle_t* handle) {
+  free(handle);
+}
+
+
 void on_peer_connected(uv_stream_t* server, int status) {
   if (status < 0) {
     fprintf(stderr, "Peer connection error: %s\n", uv_strerror(status));
     return;
+  }
+
+  uv_tcp_t* client = (uv_tcp_t*)xmalloc(sizeof(*client));
+  int rc;
+  if ((rc = uv_tcp_init(uv_default_loop(), client)) < 0) {
+    die("uv_tcp_init failed: %s", uv_strerror(rc));
+  }
+
+  if (uv_accept(server, (uv_stream_t*)client) == 0) {
+    struct sockaddr peername;
+    int namelen;
+
+    if ((rc = uv_tcp_getpeername(client, &peername, &namelen)) < 0) {
+      die("uv_tcp_getpeername failed: %s", uv_strerror(rc));
+    }
+    report_peer_connected((const struct sockaddr_in*)&peername, namelen);
+    /*uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);*/
+  } else {
+    uv_close((uv_handle_t*)client, NULL);
   }
 }
 
