d2f821a75f53551db3a428631dc99caed93e125c
Eli Bendersky <eliben@gmail.com>
2018-01-30 06:20:10 -0800

    Adding BinaryMul

--- 2018/expression-problem-cont/go/goexprsolution.go
+++ 2018/expression-problem-cont/go/goexprsolution.go
@@ -17,7 +17,7 @@ type Constant struct {
 	value float64
 }
 
-type BinPlus struct {
+type BinaryPlus struct {
 	left  Expr
 	right Expr
 }
@@ -34,22 +34,39 @@ func (c *Constant) Eval() float64 {
 	return c.value
 }
 
-func (bp *BinPlus) Eval() float64 {
+func (bp *BinaryPlus) Eval() float64 {
 	return bp.left.(Eval).Eval() + bp.right.(Eval).Eval()
 }
 
 func (c *Constant) ToString() string {
-	return strconv.FormatFloat(c.value, 'E', -1, 64)
+	return strconv.FormatFloat(c.value, 'f', -1, 64)
 }
 
-func (bp *BinPlus) ToString() string {
+func (bp *BinaryPlus) ToString() string {
 	// The moment of truth is here... bp.left is an Expr, which does not
 	// have a ToString method. Obviously this will only work if left and right
 	// implement the Stringable interface. The type assertion makes this
 	// expectation explicit and will panic otherwise.
 	ls := bp.left.(Stringify)
 	rs := bp.right.(Stringify)
-	return fmt.Sprintf("%s + %s", ls.ToString(), rs.ToString())
+	return fmt.Sprintf("(%s + %s)", ls.ToString(), rs.ToString())
+}
+
+// Now adding a new node...
+
+type BinaryMul struct {
+	left  Expr
+	right Expr
+}
+
+func (bm *BinaryMul) Eval() float64 {
+	return bm.left.(Eval).Eval() * bm.right.(Eval).Eval()
+}
+
+func (bm *BinaryMul) ToString() string {
+	ls := bm.left.(Stringify)
+	rs := bm.right.(Stringify)
+	return fmt.Sprintf("(%s * %s)", ls.ToString(), rs.ToString())
 }
 
 func main() {
@@ -64,8 +81,13 @@ func main() {
 	c11 := Constant{value: 1.1}
 	c22 := Constant{value: 2.2}
 	c33 := Constant{value: 3.3}
-	bp := BinPlus{left: &BinPlus{left: &c11, right: &c22}, right: &c33}
+	bp := BinaryPlus{left: &BinaryPlus{left: &c11, right: &c22}, right: &c33}
 
 	fmt.Printf("bp Eval = %g\n", bp.Eval())
 	fmt.Printf("bp ToString = %s\n", bp.ToString())
+
+	bm := BinaryMul{left: &bp, right: &c22}
+
+	fmt.Printf("bm Eval = %g\n", bm.Eval())
+	fmt.Printf("bm ToString = %s\n", bm.ToString())
 }
