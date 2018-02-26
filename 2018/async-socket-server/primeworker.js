3c773e4e5ade796e2aae2fd6e31d110d6a5b3b30
Eli Bendersky <eliben@gmail.com>
2018-02-26 05:33:10 -0800

    Start adding child offloading

---
+++ 2018/async-socket-server/primeworker.js
@@ -0,0 +1,19 @@
+// This file should be used as a subprocess (loaded with the child_process
+// module).
+//
+// It receives a number as a message and returns true/false if the numbers
+// are/aren't prime. It exits once it returns the answer. It also emulates a
+// delay proportional to the number in milliseconds.
+//
+// Eli Bendersky [http://eli.thegreenplace.net]
+// This code is in the public domain.
+var utils = require('./utils.js');
+
+process.on('message', message => {
+  console.log('[child] received message from server:', message);
+
+  // Compute the result (with emulate ddelay) and send back a message.
+  process.send({task: message, result: utils.isPrime(message, true)});
+  process.disconnect();
+  process.exit();
+});
