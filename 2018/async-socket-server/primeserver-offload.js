34fdc7719c82a7049f26150bb6b1ecf4296e0ac1
Eli Bendersky <eliben@gmail.com>
2018-02-26 05:45:07 -0800

    Worker offloading works initially

--- 2018/async-socket-server/primeserver-offload.js
+++ 2018/async-socket-server/primeserver-offload.js
@@ -4,6 +4,51 @@
 // This code is in the public domain.
 
 var child_process = require('child_process');
-var primeworker = child_process.fork('./primeworker.js');
+var net = require('net');
+var utils = require('./utils.js');
 
+var portnum = 8070;
+if (process.argv.length > 2) {
+  portnum = process.argv[2];
+}
 
+var server = net.createServer();
+server.on('connection', handleConnection);
+
+server.listen(portnum, function() {
+  console.log('Serving on port %d', portnum);
+});
+
+function handleConnection(conn) {
+  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+  console.log('peer %s connected', remoteAddress);
+
+  conn.on('data', onConnData);
+  conn.once('close', onConnClose);
+  conn.on('error', onConnError);
+
+  function onConnData(d) {
+    var num = utils.buf2num(d);
+    console.log('num %d', num);
+
+    var worker = child_process.fork('./primeworker.js');
+    worker.send(num);
+    worker.on('message', message => {
+      if (message.result) {
+        conn.write('prime\n');
+        console.log('... %d is prime', num);
+      } else {
+        conn.write('composite\n');
+        console.log('... %d is composite', num);
+      }
+    });
+  }
+
+  function onConnClose() {
+    console.log('connection from %s closed', remoteAddress);
+  }
+
+  function onConnError(err) {
+    console.log('connection %s error: %s', remoteAddress, err.message);
+  }
+}
