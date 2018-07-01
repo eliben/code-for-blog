14caac97686e8c1800e5d726374e326ac75c965c
Eli Bendersky <eliben@gmail.com>
2018-07-01 09:21:35 -0700

    Now no deadlocks, with the proper while loop conditions around futex()

--- 2018/futex-basics/futex-basic-process.c
+++ 2018/futex-basics/futex-basic-process.c
@@ -10,17 +10,10 @@
 #include <time.h>
 #include <unistd.h>
 
-
-int futex_safe(int* uaddr, int futex_op, int val,
-               const struct timespec* timeout, int* uaddr2, int val3) {
-  int rc = syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
-  if (rc < 0) {
-    perror("SYS_futex");
-    exit(1);
-  }
-  return rc;
+int futex(int* uaddr, int futex_op, int val, const struct timespec* timeout,
+          int* uaddr2, int val3) {
+  return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
 }
-
 
 int main(int argc, char** argv) {
   int shm_id = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0666);
@@ -29,6 +22,7 @@ int main(int argc, char** argv) {
     exit(1);
   }
   int* shared_data = shmat(shm_id, NULL, 0);
+  *shared_data = 0;
 
   int forkstatus = fork();
   if (forkstatus < 0) {
@@ -42,28 +36,32 @@ int main(int argc, char** argv) {
     printf("child waiting for A\n");
     // Wait until 0xA is written to the shared data, taking spurious wake-ups
     // into account.
-    futex_safe(shared_data, FUTEX_WAIT, 0xA, NULL, NULL, 0);
+    while (futex(shared_data, FUTEX_WAIT, 0xA, NULL, NULL, 0) != 0) {
+      sched_yield();
+    }
 
     printf("child writing B\n");
     // Write 0xB to the shared data and send a wakeup call.
     *shared_data = 0xB;
-    while (!futex_safe(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0)) {
+    while (futex(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0) == 0) {
       sched_yield();
     }
   } else {
     // Parent process.
 
     printf("parent writing A\n");
-    // Write 0xA to the shared data and send a wakeup call.
+    // Write 0xA to the shared data and send a wakeup call (until someone is
+    // actually woken up).
     *shared_data = 0xA;
-    while (!futex_safe(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0)) {
+    while (futex(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0) == 0) {
       sched_yield();
     }
 
     printf("parent waiting for B\n");
-    // Wait until 0xB is written to the shared data, taking spurious wake-ups
-    // into account.
-    futex_safe(shared_data, FUTEX_WAIT, 0xB, NULL, NULL, 0);
+    // Wait until 0xB is written to the shared data.
+    while (futex(shared_data, FUTEX_WAIT, 0xB, NULL, NULL, 0) != 0) {
+      sched_yield();
+    }
 
     wait(NULL);
     exit(0);
