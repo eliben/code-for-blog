e6628fa2044c5fbe2642fe77eff3d65a12bd4d20
Eli Bendersky <eliben@gmail.com>
2018-07-01 07:13:57 -0700

    TODO: handle spurious wakeups and the EAGAIN returned by the futex in child (?)

--- 2018/futex-basics/futex-basic-process.c
+++ 2018/futex-basics/futex-basic-process.c
@@ -1,9 +1,12 @@
 #include <linux/futex.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/shm.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -36,16 +39,35 @@ int main(int argc, char** argv) {
   if (forkstatus == 0) {
     // Child process
 
+    printf("child waiting for A\n");
+    // Wait until 0xA is written to the shared data, taking spurious wake-ups
+    // into account.
+    futex_safe(shared_data, FUTEX_WAIT, 0xA, NULL, NULL, 0);
+
+    printf("child writing B\n");
+    // Write 0xB to the shared data and send a wakeup call.
+    *shared_data = 0xB;
+    while (!futex_safe(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0)) {
+      sched_yield();
+    }
   } else {
     // Parent process.
 
-    // Write 0xA to the shared data.
+    printf("parent writing A\n");
+    // Write 0xA to the shared data and send a wakeup call.
     *shared_data = 0xA;
+    while (!futex_safe(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0)) {
+      sched_yield();
+    }
+
+    printf("parent waiting for B\n");
+    // Wait until 0xB is written to the shared data, taking spurious wake-ups
+    // into account.
+    futex_safe(shared_data, FUTEX_WAIT, 0xB, NULL, NULL, 0);
 
     wait(NULL);
     exit(0);
   }
-
 
   return 0;
 }
