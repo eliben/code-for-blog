488e2eae540a0698ed6e699ae2b0d8b594c72969
Eli Bendersky <eliben@gmail.com>
2018-07-03 06:07:54 -0700

    Refactor FUTEX_WAKE to its own standalone function

--- 2018/futex-basics/futex-basic-process.c
+++ 2018/futex-basics/futex-basic-process.c
@@ -27,13 +27,28 @@ void wait_on_futex_value(int* futex_addr, int val) {
         perror("futex");
         exit(1);
       }
-      sched_yield();
     } else if (futex_rc == 0) {
       if (*futex_addr == val) {
+        // This is a real wakeup.
         return;
       }
     } else {
       abort();
+    }
+    sched_yield();
+  }
+}
+
+// A blocking wrapper for waking a futex. Only returns when a waiter has been
+// woken up.
+void wake_futex_blocking(int* futex_addr) {
+  while (1) {
+    int futex_rc = futex(futex_addr, FUTEX_WAKE, 1, NULL, NULL, 0);
+    if (futex_rc == -1) {
+      perror("futex wake");
+      exit(1);
+    } else if (futex_rc > 0) {
+      return;
     }
   }
 }
@@ -60,21 +75,16 @@ int main(int argc, char** argv) {
     wait_on_futex_value(shared_data, 0xA);
 
     printf("child writing B\n");
-    // Write 0xB to the shared data and send a wakeup call.
+    // Write 0xB to the shared data and wake up parent.
     *shared_data = 0xB;
-    while (futex(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0) == 0) {
-      sched_yield();
-    }
+    wake_futex_blocking(shared_data);
   } else {
     // Parent process.
 
     printf("parent writing A\n");
-    // Write 0xA to the shared data and send a wakeup call (until someone is
-    // actually woken up).
+    // Write 0xA to the shared data and wake up child.
     *shared_data = 0xA;
-    while (futex(shared_data, FUTEX_WAKE, 1, NULL, NULL, 0) == 0) {
-      sched_yield();
-    }
+    wake_futex_blocking(shared_data);
 
     printf("parent waiting for B\n");
     wait_on_futex_value(shared_data, 0xB);
