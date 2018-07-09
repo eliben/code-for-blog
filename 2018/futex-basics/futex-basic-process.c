d47b07f923f46dda1659ea3681de0d51f3691989
Eli Bendersky <eliben@gmail.com>
2018-07-09 06:00:21 -0700

    Add comment and call shmdt

--- 2018/futex-basics/futex-basic-process.c
+++ 2018/futex-basics/futex-basic-process.c
@@ -11,6 +11,8 @@
 #include <time.h>
 #include <unistd.h>
 
+// The C runtime doesn't provide a wrapper for the futex(2) syscall, so we roll
+// our own.
 int futex(int* uaddr, int futex_op, int val, const struct timespec* timeout,
           int* uaddr2, int val3) {
   return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
@@ -91,6 +93,7 @@ int main(int argc, char** argv) {
 
     // Wait for the child to terminate.
     wait(NULL);
+    shmdt(shared_data);
   }
 
   return 0;
