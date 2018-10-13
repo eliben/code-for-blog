49f6f46e18795d5c2fd4de8e960f1c3fbb64c1b0
Eli Bendersky <eliben@gmail.com>
2018-10-13 09:34:41 -0700

    Update the sample with covariant markings

--- 2018/covar/covar-typecheck.py
+++ 2018/covar/covar-typecheck.py
@@ -1,4 +1,13 @@
-from typing import TypeVar, List
+# Sample of using typing.TypeVar with covariant settings.
+# Run with python3.6+
+#
+# For type-checking with mypy:
+#
+# > mypy covar-typecheck.py
+#
+# Eli Bendersky [https://eli.thegreenplace.net]
+# This code is in the public domain.
+from typing import List, TypeVar, Iterable, Generic
 
 class Mammal:
     pass
@@ -6,13 +15,32 @@ class Mammal:
 class Cat(Mammal):
     pass
 
-T = TypeVar('T')
-
-def count_mammals(seq : List[Mammal]) -> int:
+def count_mammals_list(seq : List[Mammal]) -> int:
     return len(seq)
 
-lst = [1, 2, 3]
 mlst = [Mammal(), Mammal()]
-clst = [Cat(), Cat()]
+print(count_mammals_list(mlst))
 
-print(count_mammals(clst))
+# This will fail a mypy check, because List is not covariant.
+clst = [Cat(), Cat()]
+print(count_mammals_list(clst))
+
+# Now we define a simplistic immutable list wrapper with a covariant type
+# parameter. This will pass type checking
+T_co = TypeVar('T_co', covariant=True)
+
+class ImmutableList(Generic[T_co]):
+    def __init__(self, items: Iterable[T_co]) -> None:
+        self.lst = list(items)
+
+    def __len__(self) -> int:
+        return len(self.lst)
+
+def count_mammals_ilist(seq : ImmutableList[Mammal]) -> int:
+    return len(seq)
+
+mimmlst = ImmutableList([Mammal(), Mammal()])
+print(count_mammals_ilist(mimmlst))
+
+cimmlst = ImmutableList([Cat(), Cat()])
+print(count_mammals_ilist(cimmlst))
