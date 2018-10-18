9fc10284ae32dc354d837bb36d0124d035d1ceaf
Eli Bendersky <eliben@gmail.com>
2018-10-18 06:08:07 -0700

    Start parsing code

--- 2018/unif/unifier.py
+++ 2018/unif/unifier.py
@@ -1,4 +1,5 @@
 # Python 3.6
+import lexer
 
 class Expr:
     pass
@@ -29,10 +30,68 @@ class Const(Expr):
         return self.value
 
 
-# TODO: implement these
-def parse_expr(s):
-    """Parses an expression in 's' into an Expr."""
+class ParseError(Exception): pass
+
+
+class ExprParser:
+    def __init__(self, text):
+        self.text = text
+        self.cur_token = None
+        lexrules = (
+            ('\d+',             'NUMBER'),
+            ('[a-zA-Z_]\w+',    'ID'),
+            (',',               'COMMA'),
+            ('\(',              'LP'),
+            ('\)',              'RP'),
+        )
+        self.lex = lexer.Lexer(lexrules, skip_whitespace=True)
+        self.lex.input(text)
+        self._get_next_token()
+
+    def _get_next_token(self):
+        try:
+            self.cur_token = self.lexer.token()
+
+            if self.cur_token is None:
+                self.cur_token = lexer.Token(None, None, None)
+        except lexer.LexerError, e:
+            self._error('Lexer error at position %d' % e.pos)
+
+    def _error(self, msg):
+        raise ParseError(msg)
+
+    def parse_expr(self):
+        if self.cur_token.type == 'NUMBER':
+            expr = Const(self.cur_token.val)
+            # Consume the current token and return the Const expr.
+            self._get_next_token()
+            return expr
+        elif self.cur_token.type == 'ID':
+            # We have to look at the next token to distinguish between App and
+            # Var.
+            idtok = self.cur_token
+            self._get_next_token()
+            if self.cur_token.type == 'LP':
+                args = []
+                while True:
+                    args.append(self.parse_expr())
+                    if self.cur_token.type == 'RP':
+                        break
+                    elif self.cur_token.type == 'COMMA':
+                        # Consume the comma and continue to the next arg
+                        self._get_next_token()
+                    else:
+                        self._error("Expected ',' or ')' in application")
+                # Consume the ')'
+                self._get_next_token()
+                return App(fname=idtok.val, args=args)
+            else:
+                return Var(idtok.val)
+
+
+
+# TODO: Need a bindings map to pass around for unify
+
+
+if __name__ == '__main__':
     pass
-
-
-# Need a bindings map to pass around for unify
