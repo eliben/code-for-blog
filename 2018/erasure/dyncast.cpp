cd896b2612b39d647a0a7eb472b51948588b4dc3
Eli Bendersky <eliben@gmail.com>
2018-11-26 05:48:32 -0800

    Adding dyncast sample

---
+++ 2018/erasure/dyncast.cpp
@@ -0,0 +1,31 @@
+// $ g++ --rtti --std=c++11 -O3 dyncast.cpp -o dyncast
+#include <stdio.h>
+
+struct Base {
+  virtual void basefunc() {
+    printf("basefunc\n");
+  }
+};
+
+struct Derived : public Base {
+  void derivedfunc() {
+    printf("derived\n");
+  }
+};
+
+void call_derived(Base* b) {
+  Derived* d = dynamic_cast<Derived*>(b);
+  if (d != nullptr) {
+    d->derivedfunc();
+  } else {
+    printf("cast failed\n");
+  }
+}
+
+int main() {
+  Derived d;
+  call_derived(&d);
+
+  Base b;
+  call_derived(&b);
+}
