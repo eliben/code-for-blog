e00dcbb47ea243a4b8401071b17817825a21ff12
Eli Bendersky <eliben@gmail.com>
2018-12-21 15:35:35 -0800

    Adding two code sample

---
+++ 2019/go-copying-mutex/method-mutex-pointer-receiver.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Container struct {
+	sync.Mutex
+	counters map[string]int
+}
+
+// Here the receiver is by-pointer, so the locking works as expected.
+func (c *Container) inc(name string) {
+	c.Lock()
+	defer c.Unlock()
+	c.counters[name]++
+}
+
+func main() {
+	c := Container{counters: map[string]int{"a": 0, "b": 0}}
+
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			c.inc("a")
+		}
+	}
+
+	go doIncrement("a", 100000)
+	go doIncrement("a", 100000)
+
+	// Wait a bit for the goroutines to finish
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(c.counters)
+}
