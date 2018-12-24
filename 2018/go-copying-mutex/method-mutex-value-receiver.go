3de4cf9d43f988d920ff219a560929bc90c7c71c
Eli Bendersky <eliben@gmail.com>
2018-12-24 05:48:54 -0800

    Moving samples for go-copying-mutex to the proper year
    Decided to push the post out before year-end

---
+++ 2018/go-copying-mutex/method-mutex-value-receiver.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Container struct {
+	sync.Mutex
+	counters map[string]int
+}
+
+// Whenever inc is called, the value "c" is copied inside. Since maps are just
+// references, they end up pointing to the same map.
+// https://github.com/golang/go/blob/master/src/runtime/map.go has some details
+// on the internal implementation of maps
+// Here the embedded mutex is copied too, so the locking isn't really effective.
+func (c Container) inc(name string) {
+	c.Lock()
+	defer c.Unlock()
+	c.counters[name]++
+}
+
+func main() {
+	c := Container{counters: map[string]int{"a": 0, "b": 0}}
+
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			c.inc("a")
+		}
+	}
+
+	// The Go runtime should detect concurrent map writes and abort the program.
+	// When run with -race, more details will be provided.
+	go doIncrement("a", 100000)
+	go doIncrement("a", 100000)
+
+	// Wait a bit for the goroutines to finish
+	time.Sleep(300 * time.Millisecond)
+	fmt.Println(c.counters)
+}
