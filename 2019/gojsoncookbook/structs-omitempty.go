c6c4f15cf0b8c96ec995fac07af017553e3ed5a7
Eli Bendersky <eliben@gmail.com>
2019-01-29 06:38:52 -0800

    More struct samples

---
+++ 2019/gojsoncookbook/structs-omitempty.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Food struct {
+	Id             int     `json:"id,omitempty"`
+	Name           string  `json:"name"`
+	FatPerServ     float64 `json:"fat_per_serv"`
+	ProteinPerServ float64 `json:"protein_per_serv"`
+	CarbPerServ    float64 `json:"carb_per_serv"`
+}
+
+func main() {
+	f := Food{0, "Broccoli", 0.3, 2.5, 3.5}
+	fS, _ := json.MarshalIndent(f, "", "  ")
+	fmt.Println(string(fS))
+}
