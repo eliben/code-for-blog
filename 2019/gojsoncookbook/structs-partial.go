f7e8b96a7a5c9d872fe8902be94b3682d6f585d1
Eli Bendersky <eliben@gmail.com>
2019-01-29 06:38:52 -0800

    More struct samples

---
+++ 2019/gojsoncookbook/structs-partial.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Food struct {
+	Id             int
+	Name           string
+	FatPerServ     float64
+	ProteinPerServ float64
+	CarbPerServ    float64
+}
+
+func main() {
+	bb := []byte(`
+{
+  "Name": "Broccoli",
+  "FatPerServ": 0.3,
+  "ProteinPerServ": 2.5,
+  "CarbPerServ": 3.5
+}`)
+	var fD Food
+	if err := json.Unmarshal(bb, &fD); err != nil {
+		panic(err)
+	}
+	fmt.Println("unmarshaled Food:", fD)
+}
