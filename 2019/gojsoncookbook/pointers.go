d3a5fb1f8609569631eeb81b94f751c27bd4512b
Eli Bendersky <eliben@gmail.com>
2019-01-30 06:06:09 -0800

    New file with reference/pointer examples

---
+++ 2019/gojsoncookbook/pointers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type NamePtr struct {
+	Id   int
+	Name *string
+}
+
+type BoolAndVals struct {
+	Fresh bool
+	Vals  []float64
+}
+
+func main() {
+	// The string "Sam" assigned to a variable so that we can take its address
+	// while initializing a NamePtr.
+	name := "Sam"
+	np := NamePtr{101, &name}
+
+	npS, _ := json.Marshal(np)
+	fmt.Println(string(npS))
+
+	var npD NamePtr
+	if err := json.Unmarshal(npS, &npD); err != nil {
+		panic(err)
+	}
+	fmt.Println(npD.Id, *npD.Name)
+
+	bb := []byte(`
+{
+	"Fresh": true,
+	"Vals": [1.2, 3.24, 18.99]
+}`)
+	var bvD BoolAndVals
+	if err := json.Unmarshal(bb, &bvD); err != nil {
+		panic(err)
+	}
+	fmt.Println(bvD)
+}
