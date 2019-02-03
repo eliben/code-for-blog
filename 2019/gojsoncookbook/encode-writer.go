11e2fbb53a51896cceb990ff56048d4d53ef7e31
Eli Bendersky <eliben@gmail.com>
2019-02-03 09:47:27 -0800

    Add json.Encoder example

---
+++ 2019/gojsoncookbook/encode-writer.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"os"
+)
+
+func main() {
+	// Create an io.Writer that encodes bytes written into it as base64 and emits
+	// them to stdout.
+	base64writer := base64.NewEncoder(base64.StdEncoding, os.Stdout)
+
+	// Create a new JSON encoder, hooking up its output to base64writer.
+	je := json.NewEncoder(base64writer)
+	je.Encode(map[string]float64{"foo": 4.12, "pi": 3.14159})
+
+	// Flush any partially encoded blocks left in the base64 encoder.
+	base64writer.Close()
+}
