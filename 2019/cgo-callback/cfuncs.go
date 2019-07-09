517c88f340a5896d5112b32e24d8ebc2f5f66333
Eli Bendersky <eliben@gmail.com>
2019-07-09 06:46:31 -0700

    Initial commit

---
+++ 2019/cgo-callback/cfuncs.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+extern void goStart(void*, int);
+extern void goEnd(void*, int, int);
+
+void startCgo(void* data, int i) {
+  goStart(data, i);
+}
+
+void endCgo(void* data, int a, int b) {
+	goEnd(data, a, b);
+}
+*/
+import "C"
---
+++ 2019/cgo-callback/go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.12
+
+require github.com/mattn/go-pointer v0.0.0-20180825124634-49522c3f3791
