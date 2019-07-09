8c2ba4becaac642cc12bf21db87565c274627692
Eli Bendersky <eliben@gmail.com>
2019-07-09 06:51:50 -0700

    Add traversal with state and clean up debug printouts

--- 2019/cgo-callback/clibrary.c
+++ 2019/cgo-callback/clibrary.c
@@ -3,13 +3,12 @@
 #include "clibrary.h"
 
 void traverse(void* data, Callbacks cbs) {
-  if (cbs.start) {
-    printf("calling start\n");
+  // Simulate some traversal that calls the start callback and then the end
+  // callback, if they are defined.
+  if (cbs.start != NULL) {
     cbs.start(data, 100);
   }
-  if (cbs.end) {
-    printf("calling end\n");
+  if (cbs.end != NULL) {
     cbs.end(data, 2, 3);
   }
-  printf("returning\n");
 }
