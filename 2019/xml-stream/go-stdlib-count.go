23b05d80c0ae42a70daa2bb4e21d98af1696b18c
Eli Bendersky <eliben@gmail.com>
2019-07-17 11:22:44 -0700

    Adding baseline Go example

---
+++ 2019/xml-stream/go-stdlib-count.go
@@ -0,0 +1,53 @@
+// An example streaming XML parser using encoding/xml.
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type location struct {
+	Data string `xml:",chardata"`
+}
+
+func main() {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	d := xml.NewDecoder(f)
+	count := 0
+	for {
+		tok, err := d.Token()
+		if tok == nil || err == io.EOF {
+			// EOF means we're done.
+			break
+		} else if err != nil {
+			log.Fatalf("Error decoding token: %s", err)
+		}
+
+		switch ty := tok.(type) {
+		case xml.StartElement:
+			if ty.Name.Local == "location" {
+				// If this is a start element named "location", parse this element
+				// fully.
+				var loc location
+				if err = d.DecodeElement(&loc, &ty); err != nil {
+					log.Fatalf("Error decoding item: %s", err)
+				}
+				if strings.Contains(loc.Data, "Africa") {
+					count++
+				}
+			}
+		default:
+		}
+	}
+
+	fmt.Println("count =", count)
+}
