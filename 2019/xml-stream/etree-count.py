1b2b0567cf1811143f8fbcc9ec575053d1c64157
Eli Bendersky <eliben@gmail.com>
2019-07-17 11:42:11 -0700

    Add Python samples

---
+++ 2019/xml-stream/etree-count.py
@@ -0,0 +1,12 @@
+# Using the standard library's xml.etree.ElementTree module.
+import sys
+import xml.etree.ElementTree as ET
+
+count = 0
+for event, elem in ET.iterparse(sys.argv[1], events=("end",)):
+    if event == "end":
+        if elem.tag == 'location' and elem.text and 'Africa' in elem.text:
+            count += 1
+        elem.clear()
+
+print('count =', count)
---
+++ 2019/xml-stream/lxml-count.py
@@ -0,0 +1,14 @@
+# Using lxml.
+# Make sure to install lxml first. I run this from a virtualenv where lxml is
+# installed with pip.
+import sys
+from lxml import etree
+
+count = 0
+for event, elem in etree.iterparse(sys.argv[1], events=("end",)):
+    if event == "end":
+        if elem.tag == 'location' and elem.text and 'Africa' in elem.text:
+            count += 1
+        elem.clear()
+
+print('count =', count)
