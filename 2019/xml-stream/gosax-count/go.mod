a40b952b89c12e592fe6bf7c93ee8ee8d89b1d2d
Eli Bendersky <eliben@gmail.com>
2019-07-19 05:39:27 -0700

    Add gosax version

---
+++ 2019/xml-stream/gosax-count/go.mod
@@ -0,0 +1,5 @@
+module gosax-count
+
+go 1.12
+
+require github.com/eliben/gosax v0.0.0-20190718132438-eb4c7e7172b6
---
+++ 2019/xml-stream/gosax-count/gosax-count.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unsafe"
+
+	"github.com/eliben/gosax"
+)
+
+func main() {
+	counter := 0
+	inLocation := false
+
+	scb := gosax.SaxCallbacks{
+		// We're not interested in attributes here, only tag names; register
+		// the StartElementNoAttr callback because it's faster than StartElement.
+		StartElementNoAttr: func(name string) {
+			if name == "location" {
+				inLocation = true
+			} else {
+				inLocation = false
+			}
+		},
+
+		EndElement: func(name string) {
+			inLocation = false
+		},
+
+		// Use CharactersRaw to speed things up, since we're only interested in
+		// unpacking the character data inside 'location' tags, not for every
+		// tag.
+		CharactersRaw: func(ch unsafe.Pointer, chlen int) {
+			if inLocation {
+				if strings.Contains(gosax.UnpackString(ch, chlen), "Africa") {
+					counter++
+				}
+			}
+		},
+	}
+
+	err := gosax.ParseFile(os.Args[1], scb)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("counter =", counter)
+}
