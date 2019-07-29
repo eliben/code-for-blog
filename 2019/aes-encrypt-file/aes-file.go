a168ea8646e00a5f2efb38d8357d79293bec166e
Eli Bendersky <eliben@gmail.com>
2019-07-29 11:23:08 -0700

    Decryption works too

--- 2019/aes-encrypt-file/aes-file.go
+++ 2019/aes-encrypt-file/aes-file.go
@@ -13,6 +13,10 @@ import (
 	"os"
 )
 
+// encryptFile encrypts the file specified by filename with the given key,
+// placing the result in outFilename (or filename + ".enc" if outFilename is
+// empty). The key has to be 16, 24 or 32 bytes long to select between AES-128,
+// AES-192 or AES-256. Returns the name of the output file if successful.
 func encryptFile(key []byte, filename string, outFilename string) (string, error) {
 	if len(outFilename) == 0 {
 		outFilename = filename + ".enc"
@@ -51,17 +55,70 @@ func encryptFile(key []byte, filename string, outFilename string) (string, error
 		return "", err
 	}
 
+	// Ciphertext has the same size as the padded plaintext.
 	ciphertext := make([]byte, len(plaintext))
+
+	// Use AES implementation of the cipher.Block interface to encrypt the whole
+	// file in CBC mode.
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext, plaintext)
+
+	if _, err = of.Write(ciphertext); err != nil {
+		return "", err
+	}
+	return outFilename, nil
+}
+
+// decryptFile decrypts the file specified by filename with the given key. See
+// doc for encryptFile for more details.
+func decryptFile(key []byte, filename string, outFilename string) (string, error) {
+	if len(outFilename) == 0 {
+		outFilename = filename + ".dec"
+	}
+
+	ciphertext, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	of, err := os.Create(outFilename)
+	if err != nil {
+		return "", err
+	}
+	defer of.Close()
+
+	// cipertext has the original plaintext size in the first 8 bytes, then IV
+	// in the next 16 bytes, then the actual ciphertext in the rest of the buffer.
+	// Read the original plaintext size, and the IV.
+	var origSize uint64
+	buf := bytes.NewReader(ciphertext)
+	if err = binary.Read(buf, binary.LittleEndian, &origSize); err != nil {
+		return "", err
+	}
+	iv := make([]byte, aes.BlockSize)
+	if _, err = buf.Read(iv); err != nil {
+		return "", err
+	}
+
+	// The remaining ciphertext has size=paddedSize.
+	paddedSize := len(ciphertext) - 8 - aes.BlockSize
+	if paddedSize%aes.BlockSize != 0 {
+		return "", fmt.Errorf("want padded plaintext size to be aligned to block size")
+	}
+	plaintext := make([]byte, paddedSize)
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(plaintext, ciphertext[8+aes.BlockSize:])
 
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext, plaintext)
-
-	if _, err = of.Write(ciphertext); err != nil {
+	if _, err := of.Write(plaintext[:origSize]); err != nil {
 		return "", err
 	}
 	return outFilename, nil
@@ -82,6 +139,13 @@ func main() {
 		}
 		fmt.Println("Encrypted output file:", outFilename)
 	} else if *decFlag {
-
+		outFilename, err := decryptFile(key, filename, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Decrypted output file:", outFilename)
+	} else {
+		fmt.Println(flag.Usage)
+		os.Exit(1)
 	}
 }
