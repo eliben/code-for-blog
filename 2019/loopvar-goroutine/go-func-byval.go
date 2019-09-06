0f9bba3ac45373edb59a15a023a91801c3a97ad9
Eli Bendersky <eliben@gmail.com>
2019-09-06 06:09:08 -0700

    Add first two samples for loopvar-goroutine

---
+++ 2019/loopvar-goroutine/go-func-byval.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func foobyval(n int) {
+	fmt.Println(n)
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		go foobyval(i)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+}
