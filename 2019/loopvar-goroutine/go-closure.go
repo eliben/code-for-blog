f9dc3231af7bbb59cc9bf8809e3972371a6adf04
Eli Bendersky <eliben@gmail.com>
2019-09-16 05:47:00 -0700

    Add comment headers

--- 2019/loopvar-goroutine/go-closure.go
+++ 2019/loopvar-goroutine/go-closure.go
@@ -1,3 +1,7 @@
+// Closure capture of loop variables.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import (
--- 2019/loopvar-goroutine/go-func-byval.go
+++ 2019/loopvar-goroutine/go-func-byval.go
@@ -1,3 +1,7 @@
+// By value passing of loop vars.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import (
