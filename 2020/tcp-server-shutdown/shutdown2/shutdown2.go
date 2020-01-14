c07c1eb6b5089e9c79f3fec1b40fceceb5227960
Eli Bendersky <eliben@gmail.com>
2020-01-14 05:55:13 -0800

    Rejigger shutdown2 based on newer shutdown1

--- 2020/tcp-server-shutdown/shutdown2/shutdown2.go
+++ 2020/tcp-server-shutdown/shutdown2/shutdown2.go
@@ -1,5 +1,8 @@
 // Socket server that can be shut down -- stop serving, in a graceful manner.
-// In addition to closing the listener, it closes active connections.
+// This version shuts down idle open connections from clients.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package shutdown2
 
 import (
@@ -12,14 +15,10 @@ import (
 
 type Server struct {
 	listener net.Listener
-
-	quit chan interface{}
-
-	wg sync.WaitGroup
+	quit     chan interface{}
+	wg       sync.WaitGroup
 }
 
-// NewServer creates and launches a new server listening on addr. When the
-// server is returned, it's already listening for new connections.
 func NewServer(addr string) *Server {
 	s := &Server{
 		quit: make(chan interface{}),
@@ -33,26 +32,15 @@ func NewServer(addr string) *Server {
 	return s
 }
 
-// Stop asks the server to stop and blocks until the server is actually stopped.
 func (s *Server) Stop() {
-	log.Println("asking the server to stop")
-
-	// This sequence is important: first signal on s.quit, then close the
-	// listener. This way when an Accept call fails due to the closed listener,
-	// the s.quit channel is guaranteed to be already closed.
 	close(s.quit)
 	s.listener.Close()
-
 	s.wg.Wait()
-	log.Println("server stopped")
 }
 
 func (s *Server) serve() {
 	s.wg.Add(1)
-
-	defer func() {
-		s.wg.Done()
-	}()
+	defer s.wg.Done()
 
 	for {
 		conn, err := s.listener.Accept()
@@ -61,25 +49,20 @@ func (s *Server) serve() {
 			case <-s.quit:
 				return
 			default:
+				log.Println("accept error", err)
 			}
-
-			log.Println("accept error:", err)
 		} else {
-			go s.handleConection(conn)
+			go func() {
+				s.wg.Add(1)
+				s.handleConection(conn)
+				s.wg.Done()
+			}()
 		}
 	}
 }
 
 func (s *Server) handleConection(conn net.Conn) {
-	s.wg.Add(1)
-	log.Printf("connection from %v", conn.RemoteAddr())
-
-	defer func() {
-		log.Printf("connection from %v done", conn.RemoteAddr())
-		conn.Close()
-		s.wg.Done()
-	}()
-
+	defer conn.Close()
 	buf := make([]byte, 2048)
 ReadLoop:
 	for {
@@ -100,19 +83,10 @@ ReadLoop:
 			if n == 0 {
 				return
 			}
-			log.Printf("received from %v: %s", conn.RemoteAddr(), string(buf[:n]))
 		}
 	}
 }
 
 func init() {
 	log.SetFlags(log.Ltime | log.Lmicroseconds)
-}
-
-func main() {
-	const addr = "localhost:16161"
-	s := NewServer(addr)
-	time.Sleep(10 * time.Second)
-	s.Stop()
-	select {}
 }
