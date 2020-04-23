c4e14624544a225c127eba62b69431d6c635283d
Eli Bendersky <eliben@gmail.com>
2020-04-23 07:04:30 -0700

    Adding example_test

---
+++ 2020/go-fake-stdio/example_test.go
@@ -0,0 +1,32 @@
+package fakestdio
+
+import (
+	"fmt"
+	"log"
+)
+
+func ExampleFakeInOut() {
+	// Create a new fakestdio with some input to feed into Stdin.
+	fs, err := New("input text")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Scan from Stdin. Beware of scanning too much without closing Stdin, or the
+	// program may hang. Consider calling fs.CloseStdin() if needed.
+	var scanned string
+	fmt.Scanf("%s", &scanned)
+
+	// Emit text to Stdout - it will be captured.
+	fmt.Print("some output")
+
+	b, err := fs.ReadAndRestore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This will go to the actual os.Stdout because we're no longer capturing.
+	fmt.Printf("Scanned: %q, Captured: %q", scanned, string(b))
+
+	// Output: Scanned: "input", Captured: "some output"
+}
