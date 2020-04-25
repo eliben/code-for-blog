eb4785cfb7d57491ec5d212eaca4c12c3fb00219
Eli Bendersky <eliben@gmail.com>
2020-04-25 06:35:55 -0700

    Blocking sample

---
+++ 2020/go-fake-stdio/snippets/pipe-stdout-block.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	origStdout := os.Stdout
+	os.Stdout = w
+
+	for i := 0; i < 5000; i++ {
+		fmt.Print("hello to stdout")
+	}
+
+	buf := make([]byte, 1024)
+	n, err := r.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Restore
+	os.Stdout = origStdout
+
+	fmt.Println("Written to stdout:", string(buf[:n]))
+}
