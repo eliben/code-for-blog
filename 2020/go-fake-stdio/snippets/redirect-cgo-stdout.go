59a2ff6d9b453e331b99b9ebdf62e353496ab8be
Eli Bendersky <eliben@gmail.com>
2020-04-28 06:22:53 -0700

    Add snippet redirecting cgo stdout

---
+++ 2020/go-fake-stdio/snippets/redirect-cgo-stdout.go
@@ -0,0 +1,62 @@
+package main
+
+// #include <stdio.h>
+//
+// static inline void foo() {
+//   fprintf(stderr, "** log\n");
+//   printf("foo called\n");
+// }
+//
+import "C"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"syscall"
+)
+
+func main() {
+	// Clone Stdout to origStdout.
+	origStdout, err := syscall.Dup(syscall.Stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Clone the pipe's writer to the actual Stdout descriptor; from this point
+	// on, writes to Stdout will go to w.
+	if err = syscall.Dup2(int(w.Fd()), syscall.Stdout); err != nil {
+		log.Fatal(err)
+	}
+
+	// Background goroutine that drains the reading end of the pipe.
+	out := make(chan []byte)
+	go func() {
+		var b bytes.Buffer
+		io.Copy(&b, r)
+		out <- b.Bytes()
+	}()
+
+	// ----> The actual cgo call <----
+	C.foo()
+
+	// Cleanup
+	C.fflush(nil)
+	w.Close()
+	syscall.Close(syscall.Stdout)
+
+	// Rendezvous with the reading goroutine.
+	b := <-out
+
+	// Restore original Stdout.
+	syscall.Dup2(origStdout, syscall.Stdout)
+	syscall.Close(origStdout)
+
+	fmt.Println("Captured:", string(b))
+}
