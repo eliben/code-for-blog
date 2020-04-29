0bb6526b357bfea005ec4cd6ccbe0f588e5a6ddc
Eli Bendersky <eliben@gmail.com>
2020-04-29 06:16:41 -0700

    Complete copyright comments

--- 2020/go-fake-stdio/snippets/pipe-stdout-block.go
+++ 2020/go-fake-stdio/snippets/pipe-stdout-block.go
@@ -1,3 +1,7 @@
+// Sample demonstrating blocking of os.Pipe write when its buffer fills up.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import (
