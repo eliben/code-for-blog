3522bd359988a2b275f4d73217963f94f51d81af
Eli Bendersky <eliben@gmail.com>
2020-08-11 06:52:30 -0700

    Flesh out example with methods, etc.

--- 2020/go-embed/embed-struct-in-struct-basics.go
+++ 2020/go-embed/embed-struct-in-struct-basics.go
@@ -6,16 +6,27 @@ type Base struct {
 	b int
 }
 
+func (base Base) Describe() string {
+	return fmt.Sprintf("base %d belongs to us", base.b)
+}
+
 type Container struct {
 	Base
 	c string
 }
 
 func main() {
-	bb := Base{b: 10}
+	co := Container{}
+	co.b = 1
+	co.c = "string"
+	fmt.Printf("co -> {b: %v, c: %v}\n", co.b, co.c)
 
 	// Note: using "Base" in this literal to initialize the field.
-	cc := Container{Base: bb, c: "foo"}
-
+	cc := Container{Base: Base{b: 10}, c: "foo"}
 	fmt.Printf("cc -> {b: %v, c: %v}\n", cc.b, cc.c)
+
+	// Fully qualified access.
+	fmt.Println(cc.Base.b)
+
+	fmt.Println(cc.Describe())
 }
