c333fa79034f45cf391930615275050db71c7b42
Eli Bendersky <eliben@gmail.com>
2020-08-12 06:20:36 -0700

    Example of shadowing embedded field

---
+++ 2020/go-embed/embed-struct-in-struct-field-name-conflict.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Base struct {
+	b   int
+	tag string
+}
+
+func (base Base) DescribeTag() string {
+	return fmt.Sprintf("tag is %s", base.tag)
+}
+
+type Container struct {
+	Base
+	c   string
+	tag string
+}
+
+func (co Container) DescribeTag() string {
+	return fmt.Sprintf("tag is %s", co.tag)
+}
+
+func main() {
+	b := Base{b: 10, tag: "b's tag"}
+	co := Container{Base: b, c: "foo", tag: "co's tag"}
+
+	fmt.Println(b.DescribeTag())
+	fmt.Println(co.DescribeTag())
+	fmt.Println(co.Base.DescribeTag())
+
+	fmt.Println(co.Base.tag)
+}
