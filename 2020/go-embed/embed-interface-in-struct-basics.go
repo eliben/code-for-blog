c904f9b452b25c2f91bf549ed466b45fdb47f3ac
Eli Bendersky <eliben@gmail.com>
2020-08-15 06:26:42 -0700

    Embed interface samples

---
+++ 2020/go-embed/embed-interface-in-struct-basics.go
@@ -0,0 +1,41 @@
+package main
+
+// Show how embedding an interface in a struct makes the struct automatically
+// implement the interface; but unless the embedded field is initialized with
+// something, actual calls to these methods will panic.
+
+import "fmt"
+
+type Fooer interface {
+	Foo() string
+}
+
+type Container struct {
+	Fooer
+}
+
+func sink(f Fooer) {
+	fmt.Println("sink:", f.Foo())
+}
+
+type TheRealFoo struct {
+}
+
+func (trf TheRealFoo) Foo() string {
+	return "TheRealFoo Foo"
+}
+
+func main() {
+	// Create a container without initializing the embedded slot. So it's the
+	// nil value of an interface now.
+	co := Container{}
+	_ = co
+
+	// This compiles but panics at runtime: nil dereference when trying to call
+	// the interface's function.
+	sink(co)
+
+	// This will actually work fine.
+	co2 := Container{Fooer: TheRealFoo{}}
+	sink(co2)
+}
