a6c8376c8e79581ff8358d0ad9f6edfd0c84f778
Eli Bendersky <eliben@gmail.com>
2020-08-15 06:53:41 -0700

    Example with sort.Reverse

---
+++ 2020/go-embed/embed-interface-in-struct-sort-reverse.go
@@ -0,0 +1,31 @@
+// Demonstrates how to use sort, and how to reverse-sort using an embedded
+// interface.
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// This replicates what's already in the sort package. Instead of Reverse,
+// we can use sort.Reverse.
+type reverse struct {
+	sort.Interface
+}
+
+func (r reverse) Less(i, j int) bool {
+	return r.Interface.Less(j, i)
+}
+
+func Reverse(data sort.Interface) sort.Interface {
+	return &reverse{data}
+}
+
+func main() {
+	lst := []int{4, 5, 2, 8, 1, 9, 3}
+	sort.Sort(sort.IntSlice(lst))
+	fmt.Println(lst)
+
+	sort.Sort(Reverse(sort.IntSlice(lst)))
+	fmt.Println(lst)
+}
