694efca0f48c6e3332dfd999ed08f74a593829b5
Eli Bendersky <eliben@gmail.com>
2020-12-10 08:45:24 -0800

    Clean up curl script a bit

--- 2021/go-rest-servers/testing/manual.sh
+++ 2021/go-rest-servers/testing/manual.sh
@@ -4,13 +4,14 @@ set -eux
 set -o pipefail
 
 SERVERPORT=4112
+SERVERADDR=localhost:${SERVERPORT}
 
 # Start by deleting all existing tasks on the server
-curl -iL -w "\n" -X DELETE localhost:${SERVERPORT}/task/
+curl -iL -w "\n" -X DELETE ${SERVERADDR}/task/
 
 # Add some tasks
-curl -iL -w "\n" -X POST -H "Content-Type: application/json" --data '{"text":"task first","tags":["todo", "life"], "due":"2016-01-02T15:04:05+00:00"}' localhost:${SERVERPORT}/task/
-curl -iL -w "\n" -X POST -H "Content-Type: application/json" --data '{"text":"buy milk","tags":["todo"], "due":"2016-01-03T15:04:05+00:00"}' localhost:${SERVERPORT}/task/
+curl -iL -w "\n" -X POST -H "Content-Type: application/json" --data '{"text":"task first","tags":["todo", "life"], "due":"2016-01-02T15:04:05+00:00"}' ${SERVERADDR}/task/
+curl -iL -w "\n" -X POST -H "Content-Type: application/json" --data '{"text":"buy milk","tags":["todo"], "due":"2016-01-03T15:04:05+00:00"}' ${SERVERADDR}/task/
 
 # Get tasks by tag
-curl -iL -w "\n" localhost:${SERVERPORT}/tag/todo/
+curl -iL -w "\n" ${SERVERADDR}/tag/todo/
