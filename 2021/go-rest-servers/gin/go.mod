2da26f4fad76ecabb4d1e127de6e49a85e75bee3
Eli Bendersky <eliben@gmail.com>
2021-01-15 06:54:55 -0800

    Basic handler at /task/ working

--- 2021/go-rest-servers/gin/go.mod
+++ 2021/go-rest-servers/gin/go.mod
@@ -1,3 +1,5 @@
 module example.com
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
--- 2021/go-rest-servers/gin/gorest-gin.go
+++ 2021/go-rest-servers/gin/gorest-gin.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+	"sync"
+
+	"example.com/internal/taskstore"
+	"github.com/gin-gonic/gin"
+)
+
+type taskServer struct {
+	// Mutex protects access to the 'store' field. We don't make assumptions
+	// about the safety of any method of store, so all accesses are protected.
+	sync.Mutex
+	store *taskstore.TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
+
+func (ts *taskServer) getAllTasksHandler(c *gin.Context) {
+	ts.Lock()
+	allTasks := ts.store.GetAllTasks()
+	ts.Unlock()
+
+	c.JSON(http.StatusOK, allTasks)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	engine := gin.Default()
+	server := NewTaskServer()
+
+	engine.GET("/task/", server.getAllTasksHandler)
+
+	// TODO: need StrictSlash equivalent?
+	// TODO: note that Default() already has some default middleware setup
+
+	engine.Run("localhost:" + os.Getenv("SERVERPORT"))
 }
