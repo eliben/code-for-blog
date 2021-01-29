4cf119afa09cf8d766f00dd63575310bbabbe308
Eli Bendersky <eliben@gmail.com>
2021-01-29 06:42:26 -0800

    Move/rename/fix imports to get main.go running

--- 2021/go-rest-servers/swagger/swag-server/main.go
+++ 2021/go-rest-servers/swagger/swag-server/main.go
@@ -12,20 +12,13 @@ import (
 	"log"
 	"net/http"
 
-	// WARNING!
-	// Change this to a fully-qualified import path
-	// once you place this file into your project.
-	// For example,
-	//
-	//    sw "github.com/myname/myrepo/go"
-	//
-	sw "./go"
+	"example.com/swagger"
 )
 
 func main() {
 	log.Printf("Server started")
 
-	router := sw.NewRouter()
+	router := swagger.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
