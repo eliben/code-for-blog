377df70be2bd568d1af3c4c9a8a10973cda7d912
Eli Bendersky <eliben@gmail.com>
2021-02-10 06:12:47 -0800

    Add sample of basic mux object

---
+++ 2021/go-life-http-request/basic-server-mux-object.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8090", mux)
+}
