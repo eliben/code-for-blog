1e3d8e7147692e713462e7921eccd6d4582d4847
Eli Bendersky <eliben@gmail.com>
2021-02-18 07:02:56 -0800

    Add basic middleware example with logging

---
+++ 2021/go-rest-servers/stdlib-middleware/stdlib-middleware.go
@@ -0,0 +1,219 @@
+// Basic stdlib-only REST server with some middleware.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"example.com/internal/middleware"
+	"example.com/internal/taskstore"
+)
+
+type taskServer struct {
+	store *taskstore.TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
+
+// renderJSON renders 'v' as JSON and writes it as a response into w.
+func renderJSON(w http.ResponseWriter, v interface{}) {
+	js, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (ts *taskServer) taskHandler(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path == "/task/" {
+		// Request is plain "/task/", without trailing ID.
+		if req.Method == http.MethodPost {
+			ts.createTaskHandler(w, req)
+		} else if req.Method == http.MethodGet {
+			ts.getAllTasksHandler(w, req)
+		} else if req.Method == http.MethodDelete {
+			ts.deleteAllTasksHandler(w, req)
+		} else {
+			http.Error(w, fmt.Sprintf("expect method GET, DELETE or POST at /task/, got %v", req.Method), http.StatusMethodNotAllowed)
+			return
+		}
+	} else {
+		// Request has an ID, as in "/task/<id>".
+		path := strings.Trim(req.URL.Path, "/")
+		pathParts := strings.Split(path, "/")
+		if len(pathParts) < 2 {
+			http.Error(w, "expect /task/<id> in task handler", http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(pathParts[1])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if req.Method == http.MethodDelete {
+			ts.deleteTaskHandler(w, req, int(id))
+		} else if req.Method == http.MethodGet {
+			ts.getTaskHandler(w, req, int(id))
+		} else {
+			http.Error(w, fmt.Sprintf("expect method GET or DELETE at /task/<id>, got %v", req.Method), http.StatusMethodNotAllowed)
+			return
+		}
+	}
+}
+
+func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling task create at %s\n", req.URL.Path)
+
+	// Types used internally in this handler to (de-)serialize the request and
+	// response from/to JSON.
+	type RequestTask struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
+	}
+
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	// Enforce a JSON Content-Type.
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if mediatype != "application/json" {
+		http.Error(w, "expect application/json Content-Type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	dec := json.NewDecoder(req.Body)
+	dec.DisallowUnknownFields()
+	var rt RequestTask
+	if err := dec.Decode(&rt); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	renderJSON(w, ResponseId{Id: id})
+}
+
+func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling get all tasks at %s\n", req.URL.Path)
+
+	allTasks := ts.store.GetAllTasks()
+	renderJSON(w, allTasks)
+}
+
+func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
+	log.Printf("handling get task at %s\n", req.URL.Path)
+
+	task, err := ts.store.GetTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	renderJSON(w, task)
+}
+
+func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
+	log.Printf("handling delete task at %s\n", req.URL.Path)
+
+	err := ts.store.DeleteTask(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+}
+
+func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling delete all tasks at %s\n", req.URL.Path)
+	ts.store.DeleteAllTasks()
+}
+
+func (ts *taskServer) tagHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling tasks by tag at %s\n", req.URL.Path)
+
+	if req.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("expect method GET /tag/<tag>, got %v", req.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	path := strings.Trim(req.URL.Path, "/")
+	pathParts := strings.Split(path, "/")
+	if len(pathParts) < 2 {
+		http.Error(w, "expect /tag/<tag> path", http.StatusBadRequest)
+		return
+	}
+	tag := pathParts[1]
+
+	tasks := ts.store.GetTasksByTag(tag)
+	renderJSON(w, tasks)
+}
+
+func (ts *taskServer) dueHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("handling tasks by tag at %s\n", req.URL.Path)
+
+	if req.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("expect method GET /due/<date>, got %v", req.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	path := strings.Trim(req.URL.Path, "/")
+	pathParts := strings.Split(path, "/")
+
+	badRequestError := func() {
+		http.Error(w, fmt.Sprintf("expect /due/<year>/<month>/<day>, got %v", req.URL.Path), http.StatusBadRequest)
+	}
+	if len(pathParts) != 4 {
+		badRequestError()
+		return
+	}
+
+	year, err := strconv.Atoi(pathParts[1])
+	if err != nil {
+		badRequestError()
+		return
+	}
+	month, err := strconv.Atoi(pathParts[2])
+	if err != nil || month < int(time.January) || month > int(time.December) {
+		badRequestError()
+		return
+	}
+	day, err := strconv.Atoi(pathParts[3])
+	if err != nil {
+		badRequestError()
+		return
+	}
+
+	tasks := ts.store.GetTasksByDueDate(year, time.Month(month), day)
+	renderJSON(w, tasks)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	server := NewTaskServer()
+	mux.HandleFunc("/task/", server.taskHandler)
+	mux.HandleFunc("/tag/", server.tagHandler)
+	mux.Handle("/due/", middleware.Logging(http.HandlerFunc(server.dueHandler)))
+
+	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
+}
