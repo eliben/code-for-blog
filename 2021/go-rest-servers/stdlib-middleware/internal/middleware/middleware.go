e13f87043e0322b0529ad74c45e07f5fca15f0d1
Eli Bendersky <eliben@gmail.com>
2021-02-24 08:20:15 -0800

    Add panic recovery middleware

--- 2021/go-rest-servers/stdlib-middleware/internal/middleware/middleware.go
+++ 2021/go-rest-servers/stdlib-middleware/internal/middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"runtime/debug"
 	"time"
 )
 
@@ -11,5 +12,17 @@ func Logging(next http.Handler) http.Handler {
 		start := time.Now()
 		next.ServeHTTP(w, req)
 		log.Printf("%s %s %s", req.Method, req.RequestURI, time.Since(start))
+	})
+}
+
+func PanicRecovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Println(string(debug.Stack()))
+			}
+		}()
+		next.ServeHTTP(w, req)
 	})
 }
