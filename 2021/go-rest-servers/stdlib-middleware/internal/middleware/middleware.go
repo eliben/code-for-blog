84cf0601d9042d88ed9de90aba84f9b193d81253
Eli Bendersky <eliben@gmail.com>
2021-02-27 07:06:36 -0800

    Rmove connection close

--- 2021/go-rest-servers/stdlib-middleware/internal/middleware/middleware.go
+++ 2021/go-rest-servers/stdlib-middleware/internal/middleware/middleware.go
@@ -22,7 +22,6 @@ func PanicRecovery(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				w.Header().Set("Connection", "close")
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				log.Println(string(debug.Stack()))
 			}
