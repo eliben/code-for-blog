ee051d3ff9fe79fb0abb75f2bd753b385e493c1a
Eli Bendersky <eliben@gmail.com>
2021-03-02 06:49:34 -0800

    Make a copy of gin --> gin-middleware
    To start adding middleware code to gin-based server

---
+++ 2021/go-rest-servers/gin-middleware/gorest-gin.go
@@ -0,0 +1,125 @@
+// REST server implemented with Gin.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
+package main
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"example.com/internal/taskstore"
+	"github.com/gin-gonic/gin"
+)
+
+type taskServer struct {
+	store *taskstore.TaskStore
+}
+
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
+
+func (ts *taskServer) getAllTasksHandler(c *gin.Context) {
+	allTasks := ts.store.GetAllTasks()
+	c.JSON(http.StatusOK, allTasks)
+}
+
+func (ts *taskServer) deleteAllTasksHandler(c *gin.Context) {
+	ts.store.DeleteAllTasks()
+}
+
+func (ts *taskServer) createTaskHandler(c *gin.Context) {
+	type RequestTask struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
+	}
+
+	var rt RequestTask
+	if err := c.ShouldBindJSON(&rt); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+	}
+
+	id := ts.store.CreateTask(rt.Text, rt.Tags, rt.Due)
+	c.JSON(http.StatusOK, gin.H{"Id": id})
+}
+
+func (ts *taskServer) getTaskHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	task, err := ts.store.GetTask(id)
+	if err != nil {
+		c.String(http.StatusNotFound, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func (ts *taskServer) deleteTaskHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err = ts.store.DeleteTask(id); err != nil {
+		c.String(http.StatusNotFound, err.Error())
+	}
+}
+
+func (ts *taskServer) tagHandler(c *gin.Context) {
+	tag := c.Params.ByName("tag")
+	tasks := ts.store.GetTasksByTag(tag)
+	c.JSON(http.StatusOK, tasks)
+}
+
+func (ts *taskServer) dueHandler(c *gin.Context) {
+	badRequestError := func() {
+		c.String(http.StatusBadRequest, "expect /due/<year>/<month>/<day>, got %v", c.FullPath())
+	}
+
+	year, err := strconv.Atoi(c.Params.ByName("year"))
+	if err != nil {
+		badRequestError()
+		return
+	}
+
+	month, err := strconv.Atoi(c.Params.ByName("month"))
+	if err != nil || month < int(time.January) || month > int(time.December) {
+		badRequestError()
+		return
+	}
+
+	day, err := strconv.Atoi(c.Params.ByName("day"))
+	if err != nil {
+		badRequestError()
+		return
+	}
+
+	tasks := ts.store.GetTasksByDueDate(year, time.Month(month), day)
+	c.JSON(http.StatusOK, tasks)
+}
+
+func main() {
+	router := gin.Default()
+	server := NewTaskServer()
+
+	router.POST("/task/", server.createTaskHandler)
+	router.GET("/task/", server.getAllTasksHandler)
+	router.DELETE("/task/", server.deleteAllTasksHandler)
+	router.GET("/task/:id", server.getTaskHandler)
+	router.DELETE("/task/:id", server.deleteTaskHandler)
+	router.GET("/tag/:tag", server.tagHandler)
+	router.GET("/due/:year/:month/:day", server.dueHandler)
+
+	router.Run("localhost:" + os.Getenv("SERVERPORT"))
+}
