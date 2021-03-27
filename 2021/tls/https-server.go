c3000e7310d3ae2db5d311555dddcc2ce019d501
Eli Bendersky <eliben@gmail.com>
2021-03-27 09:57:16 -0700

    Tweak server to be shorter

--- 2021/tls/https-server.go
+++ 2021/tls/https-server.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
@@ -15,32 +14,22 @@ func main() {
 	keyFile := flag.String("keyfile", "key.pem", "key PEM file")
 	flag.Parse()
 
-	tlsConfig := &tls.Config{
-		PreferServerCipherSuites: true,
-		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256},
-	}
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Pong")
-	})
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// The "/" pattern matches everything, so we need to check
-		// that we're at the root here.
 		if req.URL.Path != "/" {
 			http.NotFound(w, req)
 			return
 		}
-		fmt.Fprintf(w, "Welcome to the home page!")
+		fmt.Fprintf(w, "Proudly served with Go and HTTPS!")
 	})
 
 	srv := &http.Server{
-		Addr:         *addr,
-		Handler:      mux,
-		TLSConfig:    tlsConfig,
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		Addr:    *addr,
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			MinVersion:               tls.VersionTLS13,
+			PreferServerCipherSuites: true,
+		},
 	}
 
 	log.Printf("Starting server on %s", *addr)
