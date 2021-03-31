13b3e1cdd34835df492d35d248950e323b4028aa
Eli Bendersky <eliben@gmail.com>
2021-03-31 08:52:40 -0700

    Add code for mtls server / client
    Also add option to tls-self-signed-cert ot generate certs good for clients

---
+++ 2021/tls/https-client-mtls.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := flag.String("addr", "localhost:4000", "HTTPS server address")
+	certFile := flag.String("certfile", "cert.pem", "server certificate")
+	clientCertFile := flag.String("clientcert", "clientcert.pem", "certificate PEM for client")
+	clientKeyFile := flag.String("clientkey", "clientkey.pem", "key PEM for client")
+	flag.Parse()
+
+	// Load our client certificate and key.
+	clientCert, err := tls.LoadX509KeyPair(*clientCertFile, *clientKeyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Trusted server certificate.
+	cert, err := os.ReadFile(*certFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	certPool := x509.NewCertPool()
+	if ok := certPool.AppendCertsFromPEM(cert); !ok {
+		log.Fatalf("unable to parse cert from %s", *certFile)
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:      certPool,
+				Certificates: []tls.Certificate{clientCert},
+			},
+		},
+	}
+
+	r, err := client.Get("https://" + *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	html, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v\n", r.Status)
+	fmt.Printf(string(html))
+}
