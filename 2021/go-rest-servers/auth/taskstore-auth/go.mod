89d465c5ba3463cb18241e3a12084aa196762537
Eli Bendersky <eliben@gmail.com>
2021-05-06 08:16:05 -0700

    Switch to gorilla here for earier routing and per-route middleware
    internal/middleware remains

--- 2021/go-rest-servers/auth/taskstore-auth/go.mod
+++ 2021/go-rest-servers/auth/taskstore-auth/go.mod
@@ -2,4 +2,8 @@ module example.com
 
 go 1.15
 
-require golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
+require (
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20210505212654-3497b51f5e64 // indirect
+)
--- 2021/go-rest-servers/auth/taskstore-auth/taskstore-auth.go
+++ 2021/go-rest-servers/auth/taskstore-auth/taskstore-auth.go
@@ -14,11 +14,11 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
-	"example.com/internal/middleware"
 	"example.com/internal/taskstore"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 type taskServer struct {
@@ -39,44 +39,6 @@ func renderJSON(w http.ResponseWriter, v interface{}) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-}
-
-func (ts *taskServer) taskHandler(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path == "/task/" {
-		// Request is plain "/task/", without trailing ID.
-		if req.Method == http.MethodPost {
-			ts.createTaskHandler(w, req)
-		} else if req.Method == http.MethodGet {
-			ts.getAllTasksHandler(w, req)
-		} else if req.Method == http.MethodDelete {
-			ts.deleteAllTasksHandler(w, req)
-		} else {
-			http.Error(w, fmt.Sprintf("expect method GET, DELETE or POST at /task/, got %v", req.Method), http.StatusMethodNotAllowed)
-			return
-		}
-	} else {
-		// Request has an ID, as in "/task/<id>".
-		path := strings.Trim(req.URL.Path, "/")
-		pathParts := strings.Split(path, "/")
-		if len(pathParts) < 2 {
-			http.Error(w, "expect /task/<id> in task handler", http.StatusBadRequest)
-			return
-		}
-		id, err := strconv.Atoi(pathParts[1])
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if req.Method == http.MethodDelete {
-			ts.deleteTaskHandler(w, req, int(id))
-		} else if req.Method == http.MethodGet {
-			ts.getTaskHandler(w, req, int(id))
-		} else {
-			http.Error(w, fmt.Sprintf("expect method GET or DELETE at /task/<id>, got %v", req.Method), http.StatusMethodNotAllowed)
-			return
-		}
-	}
 }
 
 func (ts *taskServer) createTaskHandler(w http.ResponseWriter, req *http.Request) {
@@ -121,8 +83,12 @@ func (ts *taskServer) getAllTasksHandler(w http.ResponseWriter, req *http.Reques
 	renderJSON(w, allTasks)
 }
 
-func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
+func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request) {
+	// Here and elsewhere, not checking error of Atoi because the router only
+	// matches the [0-9]+ regex.
+	id, _ := strconv.Atoi(mux.Vars(req)["id"])
 	task, err := ts.store.GetTask(id)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
@@ -131,8 +97,10 @@ func (ts *taskServer) getTaskHandler(w http.ResponseWriter, req *http.Request, i
 	renderJSON(w, task)
 }
 
-func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, req *http.Request, id int) {
+func (ts *taskServer) deleteTaskHandler(w http.ResponseWriter, req *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(req)["id"])
 	err := ts.store.DeleteTask(id)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 	}
@@ -143,55 +111,24 @@ func (ts *taskServer) deleteAllTasksHandler(w http.ResponseWriter, req *http.Req
 }
 
 func (ts *taskServer) tagHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(w, fmt.Sprintf("expect method GET /tag/<tag>, got %v", req.Method), http.StatusMethodNotAllowed)
-		return
-	}
-
-	path := strings.Trim(req.URL.Path, "/")
-	pathParts := strings.Split(path, "/")
-	if len(pathParts) < 2 {
-		http.Error(w, "expect /tag/<tag> path", http.StatusBadRequest)
-		return
-	}
-	tag := pathParts[1]
-
+	tag := mux.Vars(req)["tag"]
 	tasks := ts.store.GetTasksByTag(tag)
 	renderJSON(w, tasks)
 }
 
 func (ts *taskServer) dueHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(w, fmt.Sprintf("expect method GET /due/<date>, got %v", req.Method), http.StatusMethodNotAllowed)
-		return
-	}
-
-	path := strings.Trim(req.URL.Path, "/")
-	pathParts := strings.Split(path, "/")
-
+	vars := mux.Vars(req)
 	badRequestError := func() {
 		http.Error(w, fmt.Sprintf("expect /due/<year>/<month>/<day>, got %v", req.URL.Path), http.StatusBadRequest)
 	}
-	if len(pathParts) != 4 {
-		badRequestError()
-		return
-	}
 
-	year, err := strconv.Atoi(pathParts[1])
-	if err != nil {
+	year, _ := strconv.Atoi(vars["year"])
+	month, _ := strconv.Atoi(vars["month"])
+	if month < int(time.January) || month > int(time.December) {
 		badRequestError()
 		return
 	}
-	month, err := strconv.Atoi(pathParts[2])
-	if err != nil || month < int(time.January) || month > int(time.December) {
-		badRequestError()
-		return
-	}
-	day, err := strconv.Atoi(pathParts[3])
-	if err != nil {
-		badRequestError()
-		return
-	}
+	day, _ := strconv.Atoi(vars["day"])
 
 	tasks := ts.store.GetTasksByDueDate(year, time.Month(month), day)
 	renderJSON(w, tasks)
@@ -202,19 +139,28 @@ func main() {
 	keyFile := flag.String("keyfile", "key.pem", "key PEM file")
 	flag.Parse()
 
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
+	router.StrictSlash(true)
 	server := NewTaskServer()
-	mux.HandleFunc("/task/", server.taskHandler)
-	mux.HandleFunc("/tag/", server.tagHandler)
-	mux.HandleFunc("/due/", server.dueHandler)
 
-	handler := middleware.Logging(mux)
-	handler = middleware.PanicRecovery(handler)
+	router.HandleFunc("/task/", server.createTaskHandler).Methods("POST")
+	router.HandleFunc("/task/", server.getAllTasksHandler).Methods("GET")
+	router.HandleFunc("/task/", server.deleteAllTasksHandler).Methods("DELETE")
+	router.HandleFunc("/task/{id:[0-9]+}/", server.getTaskHandler).Methods("GET")
+	router.HandleFunc("/task/{id:[0-9]+}/", server.deleteTaskHandler).Methods("DELETE")
+	router.HandleFunc("/tag/{tag}/", server.tagHandler).Methods("GET")
+	router.HandleFunc("/due/{year:[0-9]+}/{month:[0-9]+}/{day:[0-9]+}/", server.dueHandler).Methods("GET")
+
+	// Set up logging and panic recovery middleware.
+	router.Use(func(h http.Handler) http.Handler {
+		return handlers.LoggingHandler(os.Stdout, h)
+	})
+	router.Use(handlers.RecoveryHandler(handlers.PrintRecoveryStack(true)))
 
 	addr := "localhost:" + os.Getenv("SERVERPORT")
 	srv := &http.Server{
 		Addr:    addr,
-		Handler: handler,
+		Handler: router,
 		TLSConfig: &tls.Config{
 			MinVersion:               tls.VersionTLS13,
 			PreferServerCipherSuites: true,
