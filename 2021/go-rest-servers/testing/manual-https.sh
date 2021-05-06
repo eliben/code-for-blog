0ab49f6088d8d1b5b344cae32c86d7c780cc4356
Eli Bendersky <eliben@gmail.com>
2021-05-06 08:52:18 -0700

    Tweak curl script

--- 2021/go-rest-servers/testing/manual-https.sh
+++ 2021/go-rest-servers/testing/manual-https.sh
@@ -11,8 +11,23 @@ CERTFILE=../auth/taskstore-auth/cert.pem
 curl -iL -w "\n" --cacert ${CERTFILE} -X DELETE ${SERVERADDR}/task/
 
 # Add some tasks
-curl -iL -w "\n" --cacert ${CERTFILE} -X POST -H "Content-Type: application/json" --data '{"text":"task first","tags":["todo", "life"], "due":"2016-01-02T15:04:05+00:00"}' ${SERVERADDR}/task/
-curl -iL -w "\n" --cacert ${CERTFILE} -X POST -H "Content-Type: application/json" --data '{"text":"buy milk","tags":["todo"], "due":"2016-01-03T15:04:05+00:00"}' ${SERVERADDR}/task/
+curl -iL -w "\n" --cacert ${CERTFILE} -X POST \
+    -H "Authorization: Basic am9lOjEyMzQ=" \
+    -H "Content-Type: application/json" \
+    --data '{"text":"task first","tags":["todo", "life"], "due":"2016-01-02T15:04:05+00:00"}' \
+    ${SERVERADDR}/task/
+curl -iL -w "\n" --cacert ${CERTFILE} -X POST \
+    -H "Authorization: Basic am9lOjEyMzQ=" \
+    -H "Content-Type: application/json" \
+    --data '{"text":"buy milk","tags":["todo"], "due":"2016-01-03T15:04:05+00:00"}' \
+    ${SERVERADDR}/task/
+
+# This task won't be added because auth is wrong
+curl -iL -w "\n" --cacert ${CERTFILE} -X POST \
+    -H "Authorization: Basic zzzm9lOjEyMzQ=" \
+    -H "Content-Type: application/json" \
+    --data '{"text":"buy yogurt","tags":["todo"], "due":"2016-01-03T15:04:05+00:00"}' \
+    ${SERVERADDR}/task/
 
 # Get tasks by tag
 curl -iL -w "\n" --cacert ${CERTFILE} ${SERVERADDR}/tag/todo/
