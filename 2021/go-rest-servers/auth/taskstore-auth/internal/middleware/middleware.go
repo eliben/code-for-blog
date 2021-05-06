a860261fa7159c28d8eb7dcd17d66083d9104315
Eli Bendersky <eliben@gmail.com>
2021-05-06 07:58:36 -0700

    Initial copy of taskstore-auth

---
+++ 2021/go-rest-servers/auth/taskstore-auth/internal/middleware/middleware.go
@@ -0,0 +1,35 @@
+// Basic stdlib-only REST server with some middleware.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+	"time"
+)
+
+// Logging is middleware for logging information about each request.
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, req)
+		log.Printf("%s %s %s", req.Method, req.RequestURI, time.Since(start))
+	})
+}
+
+// PanicRecovery is middleware for recovering from panics in `next` and
+// returning a StatusInternalServerError to the client.
+func PanicRecovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				log.Println(string(debug.Stack()))
+			}
+		}()
+		next.ServeHTTP(w, req)
+	})
+}
