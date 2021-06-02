6251682755334bdbcd709d1a50829c0291a12838
Eli Bendersky <eliben@gmail.com>
2021-06-02 07:06:59 -0700

    Move around taskstore package, and set up resolver

--- 2021/taskstore-graphql/graph/resolver.go
+++ 2021/taskstore-graphql/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "example.com/internal/taskstore"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	store *taskstore.TaskStore
+}
