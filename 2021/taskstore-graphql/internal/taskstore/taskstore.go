7aa10c45eb2baf34cd2727f27ef771dc25a9a90c
Eli Bendersky <eliben@gmail.com>
2021-06-04 06:42:07 -0700

    Rejiggered to have attachments
    To simplify things, made our task store work on *model.Task directly

--- 2021/taskstore-graphql/internal/taskstore/taskstore.go
+++ 2021/taskstore-graphql/internal/taskstore/taskstore.go
@@ -1,6 +1,10 @@
 // package taskstore provides a simple in-memory "data store" for tasks.
 // Tasks are uniquely identified by numeric IDs.
 //
+// This package was modified from the earlier parts in the REST series to use
+// graph/model.Task and its subtypes and add attachments; each Task here has a
+// slice of Attachment.
+//
 // Eli Bendersky [https://eli.thegreenplace.net]
 // This code is in the public domain.
 package taskstore
@@ -9,40 +13,36 @@ import (
 	"fmt"
 	"sync"
 	"time"
-)
 
-type Task struct {
-	ID   int       `json:"id"`
-	Text string    `json:"text"`
-	Tags []string  `json:"tags"`
-	Due  time.Time `json:"due"`
-}
+	"example.com/graph/model"
+)
 
 // TaskStore is a simple in-memory database of tasks; TaskStore methods are
 // safe to call concurrently.
 type TaskStore struct {
 	sync.Mutex
 
-	tasks  map[int]Task
+	tasks  map[int]*model.Task
 	nextId int
 }
 
 func New() *TaskStore {
 	ts := &TaskStore{}
-	ts.tasks = make(map[int]Task)
+	ts.tasks = make(map[int]*model.Task)
 	ts.nextId = 0
 	return ts
 }
 
 // CreateTask creates a new task in the store.
-func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
+func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time, attachments []*model.Attachment) int {
 	ts.Lock()
 	defer ts.Unlock()
 
-	task := Task{
-		ID:   ts.nextId,
-		Text: text,
-		Due:  due}
+	task := &model.Task{
+		ID:          ts.nextId,
+		Text:        text,
+		Due:         due,
+		Attachments: attachments}
 	task.Tags = make([]string, len(tags))
 	copy(task.Tags, tags)
 
@@ -53,7 +53,7 @@ func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
 
 // GetTask retrieves a task from the store, by id. If no such id exists, an
 // error is returned.
-func (ts *TaskStore) GetTask(id int) (Task, error) {
+func (ts *TaskStore) GetTask(id int) (*model.Task, error) {
 	ts.Lock()
 	defer ts.Unlock()
 
@@ -61,7 +61,7 @@ func (ts *TaskStore) GetTask(id int) (Task, error) {
 	if ok {
 		return t, nil
 	} else {
-		return Task{}, fmt.Errorf("task with id=%d not found", id)
+		return nil, fmt.Errorf("task with id=%d not found", id)
 	}
 }
 
@@ -84,16 +84,16 @@ func (ts *TaskStore) DeleteAllTasks() error {
 	ts.Lock()
 	defer ts.Unlock()
 
-	ts.tasks = make(map[int]Task)
+	ts.tasks = make(map[int]*model.Task)
 	return nil
 }
 
 // GetAllTasks returns all the tasks in the store, in arbitrary order.
-func (ts *TaskStore) GetAllTasks() []Task {
+func (ts *TaskStore) GetAllTasks() []*model.Task {
 	ts.Lock()
 	defer ts.Unlock()
 
-	allTasks := make([]Task, 0, len(ts.tasks))
+	allTasks := make([]*model.Task, 0, len(ts.tasks))
 	for _, task := range ts.tasks {
 		allTasks = append(allTasks, task)
 	}
@@ -102,11 +102,11 @@ func (ts *TaskStore) GetAllTasks() []Task {
 
 // GetTasksByTag returns all the tasks that have the given tag, in arbitrary
 // order.
-func (ts *TaskStore) GetTasksByTag(tag string) []Task {
+func (ts *TaskStore) GetTasksByTag(tag string) []*model.Task {
 	ts.Lock()
 	defer ts.Unlock()
 
-	var tasks []Task
+	var tasks []*model.Task
 
 taskloop:
 	for _, task := range ts.tasks {
@@ -122,11 +122,11 @@ taskloop:
 
 // GetTasksByDueDate returns all the tasks that have the given due date, in
 // arbitrary order.
-func (ts *TaskStore) GetTasksByDueDate(year int, month time.Month, day int) []Task {
+func (ts *TaskStore) GetTasksByDueDate(year int, month time.Month, day int) []*model.Task {
 	ts.Lock()
 	defer ts.Unlock()
 
-	var tasks []Task
+	var tasks []*model.Task
 
 	for _, task := range ts.tasks {
 		y, m, d := task.Due.Date()
