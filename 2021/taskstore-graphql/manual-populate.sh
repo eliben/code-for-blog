7ff808cddb9684ced06e7b883b32b9f294d007fe
Eli Bendersky <eliben@gmail.com>
2021-06-04 06:51:00 -0700

    Adjust manual-populate to create something with attachments

--- 2021/taskstore-graphql/manual-populate.sh
+++ 2021/taskstore-graphql/manual-populate.sh
@@ -7,8 +7,6 @@ SERVERPORT=8080
 SERVERADDR=localhost:${SERVERPORT}
 GQLPATH=http://${SERVERADDR}/query
 
-echo ${PATH}
-
 # Clean up the task store initially
 curl ${GQLPATH} \
     -w "\n" -H 'Content-Type: application/json' \
@@ -21,3 +19,7 @@ curl ${GQLPATH} \
 curl ${GQLPATH} \
     -w "\n" -H 'Content-Type: application/json' \
     --data-binary '{"query":"mutation {\n  createTask(input:\n    {Text:\"Pay taxes\",\n     Tags:[\"todo\", \"bills\"],\n      Due: \"2021-08-01T15:04:05Z\"\n      })\n  {\n    Id\n  }\n}"}'
+
+curl ${GQLPATH} \
+    -w "\n" -H 'Content-Type: application/json' \
+    --data-binary '{"query":"mutation {\n  createTask(input:\n    {Text:\"Buy stuff some day\",\n     Tags:[\"shopping\"],\n      Due: \"2021-08-01T15:04:05Z\",\n      Attachments: [\n        {Name:\"file1\", Date:\"2021-06-01T15:04:05Z\", Contents:\"hello file\"},\n        {Name:\"folder2\", Date:\"2021-05-11T15:04:05Z\", Contents:\"hello folder\"}\n      ],\n      })\n  {\n    Id\n  }\n}"}'
