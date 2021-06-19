3c7d952c4d867570659fd0b2e3aea592b18fb596
Eli Bendersky <eliben@gmail.com>
2021-06-19 07:46:32 -0700

    Move to new version of protobuf/grpc
    Tweak code and build, add go.mod
    
    Server seems to be running now; not tested yet

--- 2016/grpc-stringdb/go/src/db_server.go
+++ 2016/grpc-stringdb/go/src/db_server.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 
-	pb "stringdb"
+	pb "grpc-stringdb-sample/stringdb"
 )
 
 var (
@@ -23,20 +23,22 @@ var (
 type stringdbServer struct {
 	mu sync.Mutex
 	db map[string]string
+
+	pb.UnsafeStringDbServer
 }
 
 func (s *stringdbServer) GetValue(ctx context.Context, r *pb.GetValueRequest) (*pb.GetValueReply, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// Here we rely on the map's default of an empty string when no key exists.
-	return &pb.GetValueReply{s.db[r.Key]}, nil
+	return &pb.GetValueReply{Value: s.db[r.Key]}, nil
 }
 
 func (s *stringdbServer) SetValue(ctx context.Context, r *pb.SetValueRequest) (*pb.SetValueReply, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.db[r.Key] = r.Value
-	return &pb.SetValueReply{r.Value}, nil
+	return &pb.SetValueReply{Value: r.Value}, nil
 }
 
 func (s *stringdbServer) CountValue(ctx context.Context, r *pb.CountValueRequest) (*pb.CountValueReply, error) {
@@ -47,7 +49,7 @@ func (s *stringdbServer) CountValue(ctx context.Context, r *pb.CountValueRequest
 	if ok {
 		retval = len(v)
 	}
-	return &pb.CountValueReply{int64(retval)}, nil
+	return &pb.CountValueReply{Count: int64(retval)}, nil
 }
 
 func newServer() *stringdbServer {
---
+++ 2016/grpc-stringdb/go/src/go.mod
@@ -0,0 +1,10 @@
+module grpc-stringdb-sample
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
+)
