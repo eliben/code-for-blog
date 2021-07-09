769080f146cb8d47f17092eb6da345ff34531b5c
Eli Bendersky <eliben@gmail.com>
2021-07-08 21:16:10 -0700

    Now initial step of DB setup works
    Added readme

--- 2021/go-postgresql/migrations/000001_create_tables.down.sql
+++ 2021/go-postgresql/migrations/000001_create_tables.down.sql
@@ -1,2 +1,4 @@
 drop table if exists courses;
 drop table if exists projects;
+drop table if exists users;
+drop table if exists course_user;
--- 2021/go-postgresql/migrations/000001_create_tables.up.sql
+++ 2021/go-postgresql/migrations/000001_create_tables.up.sql
@@ -1,24 +1,23 @@
-create table if not exists courses {
+create table if not exists courses (
     id bigserial primary key,
     created_at timestamp(0) with time zone not null default now(),
     title text not null
-}
+);
 
-create table if not exists projects {
+create table if not exists projects (
     id bigserial primary key,
     name text not null,
     content text not null,
     course_id bigint not null references courses (id) on delete cascade
-}
+);
 
-create table if not exists users {
+create table if not exists users (
     id bigserial primary key,
     name text not null
-}
+);
 
-create table if not exists course_user {
+create table if not exists course_user (
     course_id bigint not null references courses (id) on delete cascade,
     user_id bigint not null references users (id) on delete cascade,
     constraint course_user_key primary key (course_id, user_id)
-
-}
+);
