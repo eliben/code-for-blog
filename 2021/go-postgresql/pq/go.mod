e78fc6bb74aa0cc99bd20604644e4edc87cd1904
Eli Bendersky <eliben@gmail.com>
2021-07-09 11:17:38 -0700

    Initial code that works for a simple query

---
+++ 2021/go-postgresql/pq/go.mod
@@ -0,0 +1,5 @@
+module example.com
+
+go 1.16
+
+require github.com/lib/pq v1.10.2 // indirect
---
+++ 2021/go-postgresql/pq/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func Check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	dbpath := "postgresql://testuser:testpassword@localhost/testmooc"
+	db, err := sql.Open("postgres", dbpath)
+	Check(err)
+	defer db.Close()
+
+	users, err := dbAllUsersForCourse(db, 2)
+	Check(err)
+	fmt.Println(users)
+}
