ab8e46a0a2ed0ac2675b8d2bac4bf89e8b09419f
Eli Bendersky <eliben@gmail.com>
2021-07-13 06:34:01 -0700

    Querying with sqlx works

---
+++ 2021/go-postgresql/sqlx/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func Check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	db, err := sqlx.Open("postgres", os.Getenv("MOOCDSN"))
+	Check(err)
+	defer db.Close()
+
+	users, err := dbAllUsersForCourse(db, 2)
+	Check(err)
+	fmt.Println(users)
+
+	courses, err := dbAllCoursesForUser(db, 5)
+	Check(err)
+	fmt.Println(courses)
+
+	projects, err := dbAllProjectsForUser(db, 5)
+	Check(err)
+	fmt.Println(projects)
+}
