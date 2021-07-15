6fd5daebeb61727042e89efcb7c4d1848d8fcb59
Eli Bendersky <eliben@gmail.com>
2021-07-15 06:08:05 -0700

    Add copyright comments to all Go files

--- 2021/go-postgresql/pq/main.go
+++ 2021/go-postgresql/pq/main.go
@@ -1,3 +1,7 @@
+// Interacting with a PostgreSQL database using pq.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import (
--- 2021/go-postgresql/scany/main.go
+++ 2021/go-postgresql/scany/main.go
@@ -1,3 +1,7 @@
+// Interacting with a PostgreSQL database using scany.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import (
