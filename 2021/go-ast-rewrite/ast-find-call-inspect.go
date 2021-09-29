8ca914e41f37b7e53d06ccc31c24830fdbc86f17
Eli Bendersky <eliben@gmail.com>
2021-09-29 09:57:27 -0700

    Sample with inspect

---
+++ 2021/go-ast-rewrite/ast-find-call-inspect.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+)
+
+func main() {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "src.go", os.Stdin, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.CallExpr:
+			id, ok := x.Fun.(*ast.Ident)
+			if ok {
+				if id.Name == "pred" {
+					fmt.Printf("Inspect found call to pred() at %s\n", fset.Position(n.Pos()))
+				}
+			}
+		}
+		return true
+	})
+}
