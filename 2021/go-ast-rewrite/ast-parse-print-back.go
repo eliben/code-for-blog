f81490a4210ad41c4294161799a62dcb75a770c2
Eli Bendersky <eliben@gmail.com>
2021-10-02 05:35:22 -0700

    Add header comments to all code

--- 2021/go-ast-rewrite/ast-parse-print-back.go
+++ 2021/go-ast-rewrite/ast-parse-print-back.go
@@ -1,3 +1,8 @@
+// Simple example of parsing Go source text (from stdin) to an AST and printing
+// the AST back (to stdout).
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import (
