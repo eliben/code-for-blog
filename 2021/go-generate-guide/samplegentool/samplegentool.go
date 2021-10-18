5ce96bbb018a7d2c129086e1bf97d4ceb219492c
Eli Bendersky <eliben@gmail.com>
2021-10-18 11:32:30 -0700

    Add cwd reporting

--- 2021/go-generate-guide/samplegentool/samplegentool.go
+++ 2021/go-generate-guide/samplegentool/samplegentool.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	fmt.Printf("Running %s go on %s\n", os.Args[0], os.Getenv("GOFILE"))
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("  cwd = %s\n", cwd)
 	fmt.Printf("  os.Args = %v\n", os.Args)
+
 	for _, ev := range []string{"GOARCH", "GOOS", "GOFILE", "GOLINE", "GOPACKAGE", "DOLLAR"} {
 		fmt.Println("  ", ev, "=", os.Getenv(ev))
 	}
