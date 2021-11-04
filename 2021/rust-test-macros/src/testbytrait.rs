f2f2a5c26e21c6b905aa26cb404ec900b09a8184
Eli Bendersky <eliben@gmail.com>
2021-11-04 06:18:53 -0700

    Move constructor into trait

--- 2021/rust-test-macros/src/testbytrait.rs
+++ 2021/rust-test-macros/src/testbytrait.rs
@@ -1,5 +1,3 @@
-// TODO: issues -- need to pass object, not type (how to test various
-// ctors?
 // if I have multiple test functions, each test_$Type has to invoke each
 // one of them, and they all cannot run in parallel, be seen in reports,
 // invoked by name w/ "cargo test" etc.
@@ -8,18 +6,19 @@
 mod tests {
     use crate::calculator::{Bar, Calculator, Foo};
 
-    fn trait_tester<C: Calculator>(c: &C) {
+    fn trait_tester<C: Calculator>() {
+        let c = C::new();
         assert_eq!(c.add(2, 3), 5);
         assert_eq!(c.add(10, 43), 53);
     }
 
     #[test]
     fn test_foo() {
-        trait_tester(&Foo::new());
+        trait_tester::<Foo>();
     }
 
     #[test]
     fn test_bar() {
-        trait_tester(&Bar::new());
+        trait_tester::<Bar>();
     }
 }
