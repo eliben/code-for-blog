5d4dbb62331b1f42fa254a09d39aa21610946980
Eli Bendersky <eliben@gmail.com>
2022-01-26 12:33:26 -0800

    byteorder example

--- 2022/rust-traits-on-builtin-types/serde-rayon-example/src/main.rs
+++ 2022/rust-traits-on-builtin-types/serde-rayon-example/src/main.rs
@@ -1,6 +1,8 @@
 use serde::Serialize;
 use serde_json;
 
+use byteorder::{LittleEndian, WriteBytesExt};
+
 fn main() {
     let mut serializer = serde_json::Serializer::new(std::io::stdout());
 
@@ -14,4 +16,9 @@ fn main() {
     print!("Serializing a vector of integers: ");
     vec![2, 3, 4].serialize(&mut serializer).unwrap();
     println!("");
+
+    let mut wv = vec![];
+    wv.write_u16::<LittleEndian>(259).unwrap();
+    wv.write_u16::<LittleEndian>(517).unwrap();
+    println!("{:?}", wv);
 }
