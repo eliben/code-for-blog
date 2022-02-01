908bf7ba6920cf9c5afe921d815340d13b323473
Eli Bendersky <eliben@gmail.com>
2022-02-01 11:19:47 -0800

    Initial code import

---
+++ 2022/depthtree/main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+func main() {
+	dl := Parse("[[4,8],[3,5]]")
+	t := dl.BuildTree()
+	t.Verify()
+	fmt.Println(t)
+}
+
+// "Depth-list" representation of nested pairs: a slice of DItem, each of which
+// is a value and its depth in the pairs tree.
+type DItem struct {
+	Value int
+	Depth int
+}
+
+type DList []DItem
+
+func (dl DList) String() string {
+	var parts []string
+	for _, di := range dl {
+		parts = append(parts, fmt.Sprintf("(%v %v)", di.Value, di.Depth))
+	}
+	return strings.Join(parts, " ")
+}
+
+// Parse parses a textual representation of nested paisr into a DList.
+// Nested pairs are wrapped in [...] and separated by commas; for example,
+// this is a nested pair: "[[1,2],[[3,4],5]]"
+// Bad inputs are not checked and will likely produce wrong output.
+func Parse(s string) DList {
+	depth := 0
+	var lst DList
+
+	for _, c := range strings.TrimSpace(s) {
+		switch c {
+		case '[':
+			depth++
+		case ']':
+			depth--
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			lst = append(lst, DItem{int(c) - '0', depth})
+		default:
+		}
+	}
+
+	return lst
+}
+
+// BuildTree builds a Tree from a DList using an iterative algorithm.
+func (dl DList) BuildTree() *Tree {
+	if len(dl) == 0 {
+		return nil
+	}
+	// result is the tree this function is building. The result pointer always
+	// points at the root, so we can return it to the caller. t points to the
+	// current node being constructed throughout the algorithm.
+	result := &Tree{}
+	t := result
+
+	// depth is the current depth of t in the tree.
+	depth := 1
+
+	// stack of parent nodes to implement backtracking up the tree once we're done
+	// with a subtree.
+	var stack []*Tree
+
+	// The outer loop iterates over all the items in a DList, inserting each one
+	// into the tree. Loop invariant: all items preceding this item in dl have
+	// already been inserted into the tree, and t points to the node where the
+	// last insertion was made.
+nextItem:
+	for _, item := range dl {
+		// The inner loop find the right place for item in the tree and performs
+		// insertion.
+		// Loop invariant: t points at the node where we're trying to insert, depth
+		// is its depth in the tree and stack holds a stack of t's parents.
+		for {
+			// Check if item can be inserted as a child of t; this can be done only if
+			// our depth matches the item's and t doesn't have both its children yet.
+			// Otherwise, t is not the right place and we have to keep looking.
+			if item.Depth == depth && t.Left == nil {
+				t.Left = &Tree{Value: item.Value}
+				continue nextItem
+			} else if item.Depth == depth && t.Right == nil {
+				t.Right = &Tree{Value: item.Value}
+				continue nextItem
+			}
+
+			// We can't insert at t.
+			// * If t does not have a left child yet, create it and repeat loop with
+			//   this left child as t.
+			// * If t does not have a right child yet, create it and repeat loop with
+			//   this right child as t.
+			// * If t has both children, we have to backtrack up the tree to t's
+			//   parent.
+			if t.Left == nil {
+				stack = append(stack, t)
+				t.Left = &Tree{}
+				t = t.Left
+				depth++
+			} else if t.Right == nil {
+				stack = append(stack, t)
+				t.Right = &Tree{}
+				t = t.Right
+				depth++
+			} else {
+				// Pop from the stack to make t point to its parent
+				t, stack = stack[len(stack)-1], stack[:len(stack)-1]
+				depth--
+			}
+		}
+	}
+
+	return result
+}
+
+// BuildTreeRec builds a Tree from a DList using recursive algorithm.
+func (dl DList) BuildTreeRec() *Tree {
+	cursor := 0
+
+	var builder func(depth int) *Tree
+	builder = func(depth int) *Tree {
+		if cursor >= len(dl) {
+			return nil
+		}
+
+		var left *Tree
+		if dl[cursor].Depth == depth {
+			left = &Tree{Value: dl[cursor].Value}
+			cursor++
+		} else {
+			left = builder(depth + 1)
+		}
+
+		var right *Tree
+		if dl[cursor].Depth == depth {
+			right = &Tree{Value: dl[cursor].Value}
+			cursor++
+		} else {
+			right = builder(depth + 1)
+		}
+		return &Tree{Left: left, Right: right}
+	}
+
+	return builder(1)
+}
+
+type Tree struct {
+	Value       int
+	Left, Right *Tree
+}
+
+// Verify checks that the tree is structured as expected: internal nodes have a
+// default value and two children; leaves have a non-default value and no
+// children.
+func (t *Tree) Verify() {
+	if t == nil {
+		return
+	}
+
+	if t.Left == nil && t.Right == nil {
+		if t.Value == 0 {
+			log.Fatalf("want leaf with value 0, got %v", t.Value)
+		}
+	} else if t.Left != nil && t.Right != nil {
+		if t.Value != 0 {
+			log.Fatalf("want internal node with value !=0, got %v", t.Value)
+		}
+		t.Left.Verify()
+		t.Right.Verify()
+	} else {
+		log.Fatalf("want either 0 or 2 children in node, got 1")
+	}
+}
+
+func (t *Tree) String() string {
+	if t == nil {
+		return "()"
+	} else if t.Left == nil && t.Right == nil {
+		return fmt.Sprint(t.Value)
+	}
+
+	// Assume the tree has been verified: we have both left and right children.
+	return "(" + t.Left.String() + " " + t.Right.String() + ")"
+}
---
+++ 2022/depthtree/main_test.go
@@ -0,0 +1,64 @@
+package main
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  string
+	}{
+		{"[]", ""},
+		{"[1,2]", "(1 1) (2 1)"},
+		{"[1,[2,8]]", "(1 1) (2 2) (8 2)"},
+		{"[[1,9],2]", "(1 2) (9 2) (2 1)"},
+		{"[[4,8],[3,5]]", "(4 2) (8 2) (3 2) (5 2)"},
+		{"[[6,9],[[3,4],2]]", "(6 2) (9 2) (3 3) (4 3) (2 2)"},
+		{"[[1,2],[[3,4],5]]", "(1 2) (2 2) (3 3) (4 3) (5 2)"},
+		{"[[[[1,2],3],4],5]", "(1 4) (2 4) (3 3) (4 2) (5 1)"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			ans := Parse(tt.input).String()
+			if ans != tt.want {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestBuildTree(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  string
+	}{
+		{"[]", "()"},
+		{"[1,2]", "(1 2)"},
+		{"[1,[2,8]]", "(1 (2 8))"},
+		{"[[4,8],3]", "((4 8) 3)"},
+		{"[[4,8],[3,5]]", "((4 8) (3 5))"},
+		{"[[6,9],[[3,4],2]]", "((6 9) ((3 4) 2))"},
+		{"[[[[1,2],3],4],5]", "((((1 2) 3) 4) 5)"},
+		{"[7,[5,[4,[9,1]]]]", "(7 (5 (4 (9 1))))"},
+		{"[[[4,8],[3,5]], [[3,7],[1,9]]]", "(((4 8) (3 5)) ((3 7) (1 9)))"},
+		{"[6,[[[4,8],[3,5]], [[3,7],[1,9]]]]", "(6 (((4 8) (3 5)) ((3 7) (1 9))))"},
+		{"[[[[4,8],[3,5]], [[3,7],[1,9]]],6]", "((((4 8) (3 5)) ((3 7) (1 9))) 6)"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			dl := Parse(tt.input)
+			ansRec := dl.BuildTreeRec()
+			ansRec.Verify()
+			if ansRec.String() != tt.want {
+				t.Errorf("rec got %v, want %v", ansRec.String(), tt.want)
+			}
+
+			ansIter := dl.BuildTree()
+			ansIter.Verify()
+			if ansIter.String() != tt.want {
+				t.Errorf("iter got %v, want %v", ansIter.String(), tt.want)
+			}
+		})
+	}
+}
