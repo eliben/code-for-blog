78dffbd70b5d7ae663cf8fe8fbeb74cce62f3135
Eli Bendersky <eliben@gmail.com>
2022-02-02 06:09:22 -0800

    Add copyright comments

--- 2022/depthtree/main_test.go
+++ 2022/depthtree/main_test.go
@@ -1,3 +1,7 @@
+// Binary tree construction from inorder-depth representation.
+//
+// Eli Bendersky [https://eli.thegreenplace.net]
+// This code is in the public domain.
 package main
 
 import "testing"
