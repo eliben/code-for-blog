83e45593e7a4514f7e62df343a3dde23de45a0bb
Eli Bendersky <eliben@gmail.com>
2022-02-12 08:19:06 -0800

    Another test + comments

--- 2022/filetest/somepackage/somepackage_test.go
+++ 2022/filetest/somepackage/somepackage_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 func TestFormatFiles(t *testing.T) {
+	// Find the paths of all input files in the data directory.
 	paths, err := filepath.Glob(filepath.Join("testdata", "*.input"))
 	if err != nil {
 		t.Fatal(err)
@@ -18,17 +19,23 @@ func TestFormatFiles(t *testing.T) {
 		_, filename := filepath.Split(path)
 		testname := filename[:len(filename)-len(filepath.Ext(path))]
 
+		// Each path turns into a test: the test name is the filename without the
+		// extension.
 		t.Run(testname, func(t *testing.T) {
 			source, err := os.ReadFile(path)
 			if err != nil {
 				t.Fatal("error reading source file:", err)
 			}
 
+			// >>> This is the actual code under test.
 			output, err := format.Source(source)
 			if err != nil {
 				t.Fatal("error formatting:", err)
 			}
+			// <<<
 
+			// Each input file is expected to have a "golden output" file, with the
+			// same path except the .input extension is replaced by .golden
 			goldenfile := filepath.Join("testdata", testname+".golden")
 			want, err := os.ReadFile(goldenfile)
 			if err != nil {
