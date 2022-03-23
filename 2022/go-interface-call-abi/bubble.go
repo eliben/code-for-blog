48812f453fbfb5a1876391533466da83a313a830
Eli Bendersky <eliben@gmail.com>
2022-03-23 09:36:32 -0700

    Start adding example for blog post

---
+++ 2022/go-interface-call-abi/bubble.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// bubbleUp is one pass of the bubble sort algorithm. It will bubble up the
+// largest element in x to the last position, and may reorder some other
+// elements.
+func bubbleUp(x sort.Interface) {
+	n := x.Len()
+	for i := 1; i < n; i++ {
+		if x.Less(i, i-1) {
+			x.Swap(i, i-1)
+		}
+	}
+}
+
+func main() {
+	is := []int{4, 2, 8, 3, 2, 1}
+	bubbleUp(sort.IntSlice(is))
+	fmt.Println(is)
+}
