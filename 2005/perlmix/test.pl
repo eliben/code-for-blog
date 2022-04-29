325f96d8ad403be96184eac956d237f98e43d073
Eli Bendersky <eliben@gmail.com>
2022-04-29 08:52:26 -0700

    Some formatting cleanup

--- 2005/perlmix/test.pl
+++ 2005/perlmix/test.pl
@@ -7,7 +7,7 @@
 ##
 #
 # Runs some regression tests on PerlMIX.
-# 
+#
 # Input: expects a directory "test/" divided internally as
 # one directory per each test, which is a file ending with
 # .mixal
@@ -15,7 +15,7 @@
 # Output: each test prints OK to the terminal if it passed.
 # If you don't see OK for some test, it probably failed.
 #
-# Take a look at the test/ directory structure and run 
+# Take a look at the test/ directory structure and run
 # test.pl once to get a feel for how it works.
 #
 use warnings;
@@ -30,16 +30,16 @@ find(\&test_dir, "test/");
 
 sub test_dir
 {
-	my $filename = $_;
-	
-	if ($filename =~ /\.mixal$/)
-	{
-		init_assembler();
-		my ($mref, $init_addr) = assemble_file("$filename");
-		
-		init_sim(-mem_ref => $mref, -init_addr => $init_addr);
-		print "Test $File::Find::name: ";
-		run_sim();
-		print "\n";
-	}
+  my $filename = $_;
+
+  if ($filename =~ /\.mixal$/)
+  {
+    init_assembler();
+    my ($mref, $init_addr) = assemble_file("$filename");
+
+    init_sim(-mem_ref => $mref, -init_addr => $init_addr);
+    print "Test $File::Find::name: ";
+    run_sim();
+    print "\n";
+  }
 }
