655cc1cf59641b0b149ffd32171881e3ae18b38f
Eli Bendersky <eliben@gmail.com>
2022-09-23 15:08:02 -0700

    Clean up and clarify in comment

--- 2022/go-static-http-server/file-server-with-prefix.go
+++ 2022/go-static-http-server/file-server-with-prefix.go
@@ -1,3 +1,6 @@
+// Local file server that serves the files to a non-root path. For this to
+// work properly, we have to use the http.StripPrefix middleware which
+// rewrites requests to remove the prefix.
 package main
 
 import "net/http"
@@ -5,7 +8,6 @@ import "net/http"
 func main() {
 	port := ":9999"
 	handler := http.StripPrefix("/static/", http.FileServer(http.Dir("files")))
-	//handler := http.FileServer(http.Dir("files"))
 	http.Handle("/static/", handler)
 	http.ListenAndServe(port, nil)
 }
