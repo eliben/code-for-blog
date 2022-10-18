e82a30b9511b533187393dcc30abc009b33f9e62
Eli Bendersky <eliben@gmail.com>
2022-10-18 07:35:47 -0700

    Start new directory for Go proxy server examples
    First in - a simple http server that dumps headers

---
+++ 2022/go-and-proxies/http-server-debug-request-headers.go
@@ -0,0 +1,36 @@
+// Server for debugging HTTP requests; answers all paths successfully and
+// dumps information about the request to the log.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	var addr = flag.String("addr", "127.0.0.1:8080", "listen address")
+	flag.Parse()
+
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, req *http.Request) {
+			var b strings.Builder
+
+			fmt.Fprintf(&b, "%v\t%v\t%v\tHost: %v\n", req.RemoteAddr, req.Method, req.URL, req.Host)
+			for name, headers := range req.Header {
+				for _, h := range headers {
+					fmt.Fprintf(&b, "%v: %v\n", name, h)
+				}
+			}
+			log.Println(b.String())
+
+			fmt.Fprintf(w, "hello %s\n", req.URL)
+		})
+
+	log.Println("Starting server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
