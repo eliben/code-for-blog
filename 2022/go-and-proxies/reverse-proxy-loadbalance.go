28a516f49a864fca25f542f396ec083ea45ee805
Eli Bendersky <eliben@gmail.com>
2022-10-20 06:08:04 -0700

    Example of simple load-balancing reverse proxy

---
+++ 2022/go-and-proxies/reverse-proxy-loadbalance.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	fromAddr := flag.String("from", "127.0.0.1:9090", "proxy's listening address")
+	toAddr1 := flag.String("to1", "127.0.0.1:8080", "the first address this proxy will forward to")
+	toAddr2 := flag.String("to2", "127.0.0.1:8081", "the second address this proxy will forward to")
+	flag.Parse()
+
+	toUrl1 := parseToUrl(*toAddr1)
+	toUrl2 := parseToUrl(*toAddr2)
+
+	proxy := loadBalancingReverseProxy(toUrl1, toUrl2)
+	log.Println("Starting proxy server on", *fromAddr)
+	if err := http.ListenAndServe(*fromAddr, proxy); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+// parseToUrl parses a "to" address to url.URL value
+func parseToUrl(addr string) *url.URL {
+	if !strings.HasPrefix(addr, "http") {
+		addr = "http://" + addr
+	}
+	toUrl, err := url.Parse(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return toUrl
+}
+
+func loadBalancingReverseProxy(target1, target2 *url.URL) *httputil.ReverseProxy {
+	var targetNum = 1
+
+	director := func(req *http.Request) {
+		var target *url.URL
+		// Simple round robin between the two targets
+		if targetNum == 1 {
+			target = target1
+			targetNum = 2
+		} else {
+			target = target2
+			targetNum = 1
+		}
+
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
+		// For simplicity, we don't handle RawQuery or the User-Agent header here:
+		// see the full code of NewSingleHostReverseProxy for an example of doint
+		// that.
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+// singleJoiningSlash is taken from net/http/httputil
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+// joinURLPath is taken from net/http/httputil
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
