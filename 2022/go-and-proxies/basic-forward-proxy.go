b7aef921671df77f097b580c37d80065eff5a161
Eli Bendersky <eliben@gmail.com>
2022-10-20 06:58:19 -0700

    Port basic forward proxy code -- needs more work

---
+++ 2022/go-and-proxies/basic-forward-proxy.go
@@ -0,0 +1,129 @@
+// Implements a sample proxy server in Go. Adapted from httputil.ReverseProxy's
+// implementation
+//
+// Sample usage: run this program, then elsewhere run
+//
+// $ HTTP_PROXY=127.0.0.1:9999 go run http-client-get-url.go <some url>
+//
+// Then the client will request <some url> through this proxy. Note: if <some
+// url> is on localhost, Go clients will ignore HTTP_PROXY; to force them to use
+// the proxy, either set up a proxy explicitly in the Transport, or set up an
+// alias in /etc/hosts and use that instead of localhost.
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
+
+// Hop-by-hop headers. These are removed when sent to the backend.
+// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
+// Note: this may be out of date, see RFC 7230 Section 6.1
+var hopHeaders = []string{
+	"Connection",
+	"Proxy-Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te",      // canonicalized version of "TE"
+	"Trailer", // spelling per https://www.rfc-editor.org/errata_search.php?eid=4522
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func removeHopHeaders(header http.Header) {
+	for _, h := range hopHeaders {
+		header.Del(h)
+	}
+}
+
+// removeConnectionHeaders removes hop-by-hop headers listed in the "Connection"
+// header of h. See RFC 7230, section 6.1
+func removeConnectionHeaders(h http.Header) {
+	for _, f := range h["Connection"] {
+		for _, sf := range strings.Split(f, ",") {
+			if sf = strings.TrimSpace(sf); sf != "" {
+				h.Del(sf)
+			}
+		}
+	}
+}
+
+func appendHostToXForwardHeader(header http.Header, host string) {
+	// If we aren't the first proxy retain prior
+	// X-Forwarded-For information as a comma+space
+	// separated list and fold multiple headers into one.
+	if prior, ok := header["X-Forwarded-For"]; ok {
+		host = strings.Join(prior, ", ") + ", " + host
+	}
+	header.Set("X-Forwarded-For", host)
+}
+
+type proxy struct {
+}
+
+func (p *proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
+	// The "Host:" header is promoted to Request.Host and is removed from
+	// request.Header by net/http, so we print it out explicitly.
+	log.Println(req.RemoteAddr, "\t\t", req.Method, "\t\t", req.URL, "\t\t Host:", req.Host)
+	log.Println("\t\t\t\t\t", req.Header)
+
+	if req.URL.Scheme != "http" && req.URL.Scheme != "https" {
+		msg := "unsupported protocal scheme " + req.URL.Scheme
+		http.Error(wr, msg, http.StatusBadRequest)
+		log.Println(msg)
+		return
+	}
+
+	client := &http.Client{}
+	// When a http.Request is sent through an http.Client, RequestURI should not
+	// be set (see documentation of this field).
+	req.RequestURI = ""
+
+	removeHopHeaders(req.Header)
+	removeConnectionHeaders(req.Header)
+
+	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+		appendHostToXForwardHeader(req.Header, clientIP)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(wr, "Server Error", http.StatusInternalServerError)
+		log.Fatal("ServeHTTP:", err)
+	}
+	defer resp.Body.Close()
+
+	log.Println(req.RemoteAddr, " ", resp.Status)
+
+	removeHopHeaders(resp.Header)
+	removeConnectionHeaders(resp.Header)
+
+	copyHeader(wr.Header(), resp.Header)
+	wr.WriteHeader(resp.StatusCode)
+	io.Copy(wr, resp.Body)
+}
+
+func main() {
+	var addr = flag.String("addr", "127.0.0.1:9999", "proxy address")
+	flag.Parse()
+
+	handler := &proxy{}
+
+	log.Println("Starting proxy server on", *addr)
+	if err := http.ListenAndServe(*addr, handler); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
