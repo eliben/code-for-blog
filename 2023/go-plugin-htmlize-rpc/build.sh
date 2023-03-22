cbad199f02bbfb239c59b40e7523a947228f4daa
Eli Bendersky <eliben@gmail.com>
2023-03-22 05:54:26 -0700

    Rename the main binary --> htmlize

--- 2023/go-plugin-htmlize-rpc/build.sh
+++ 2023/go-plugin-htmlize-rpc/build.sh
@@ -6,4 +6,4 @@ set -o pipefail
 mkdir -p plugin-binaries
 go build -o ./plugin-binaries/tt ./sample-plugins/tt/
 go build -o ./plugin-binaries/nacrissist ./sample-plugins/narcissist/
-go build -o ./main ./main.go
+go build -o ./htmlize ./main.go
