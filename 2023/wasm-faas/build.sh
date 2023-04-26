c33dab77a3f6077a998a2da195b1244174d0576c
Eli Bendersky <eliben@gmail.com>
2023-04-26 09:48:43 -0700

    Add gotip invocation for Go

--- 2023/wasm-faas/build.sh
+++ 2023/wasm-faas/build.sh
@@ -8,6 +8,7 @@ wat2wasm examples/hello.wat -o target/hello.wasm
 wat2wasm examples/watenv.wat -o target/watenv.wasm
 tinygo build -o target/hellogo.wasm -target=wasi examples/hellogo/hellogo.go
 tinygo build -o target/goenv.wasm -target=wasi examples/goenv/goenv.go
+GOOS=wasip1 GOARCH=wasm gotip build -o target/goenvgc.wasm examples/goenv/goenv.go
 
 (cd examples/rustenv; cargo build --target wasm32-wasi --release)
 cp examples/rustenv/target/wasm32-wasi/release/rustenv.wasm target/
