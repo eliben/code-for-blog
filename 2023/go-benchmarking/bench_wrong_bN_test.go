f390dfec514f928936e98d1765586aafb71a6989
Eli Bendersky <eliben@gmail.com>
2023-06-23 19:20:38 -0700

    Example of wrong benchmark using i as input

---
+++ 2023/go-benchmarking/bench_wrong_bN_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"testing"
+)
+
+// Don't run this benchmark, it gets stuck
+func BenchmarkRandPrime(b *testing.B) {
+	fmt.Println(b.N)
+	for i := 0; i < b.N; i++ {
+		rand.Prime(rand.Reader, i)
+	}
+}
