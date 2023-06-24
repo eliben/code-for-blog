4d57a25998c173634f1d35c64a2f20731e16070e
Eli Bendersky <eliben@gmail.com>
2023-06-23 18:50:04 -0700

    Test showing number of runs

---
+++ 2023/go-benchmarking/bench_basic_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"hash/crc32"
+	"testing"
+)
+
+func BenchmarkBasic(b *testing.B) {
+	bs := bytes.Repeat([]byte{2, 3, 5, 7}, 2048)
+	fmt.Println("benchmark with N =", b.N)
+
+	for i := 0; i < b.N; i++ {
+		crc32.ChecksumIEEE(bs)
+	}
+}
