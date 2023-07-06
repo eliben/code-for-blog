ffbe916e5e354cee830f2e650bcf204efb3260af
Eli Bendersky <eliben@gmail.com>
2023-07-06 13:11:59 -0700

    more benches

--- 2023/go-benchmarking/bench_wrong_bN_test.go
+++ 2023/go-benchmarking/bench_wrong_bN_test.go
@@ -7,10 +7,24 @@ import (
 )
 
 // Don't run this benchmark, it gets stuck
-func BenchmarkRandPrime(b *testing.B) {
+func BenchmarkRandPrimeWrongUseI(b *testing.B) {
 	fmt.Println(b.N)
 	for i := 0; i < b.N; i++ {
 		rand.Prime(rand.Reader, i)
+	}
+	fmt.Println("... elapsed:", b.Elapsed())
+}
+
+func BenchmarkRandPrimeWrongNoLoop(b *testing.B) {
+	fmt.Println(b.N)
+	rand.Prime(rand.Reader, 200)
+	fmt.Println("... elapsed:", b.Elapsed())
+}
+
+func BenchmarkRandPrimeOK(b *testing.B) {
+	fmt.Println(b.N)
+	for i := 0; i < b.N; i++ {
+		rand.Prime(rand.Reader, 200)
 	}
 	fmt.Println("... elapsed:", b.Elapsed())
 }
