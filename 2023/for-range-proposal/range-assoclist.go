e130c9832ebd4e7f3a139a5759aa5bb0e0a90b03
Eli Bendersky <eliben@gmail.com>
2023-07-19 07:00:51 -0700

    Add new example and a README

---
+++ 2023/for-range-proposal/range-assoclist.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type AssocList[K comparable, V any] struct {
+	lst []pair[K, V]
+}
+
+type pair[K comparable, V any] struct {
+	key   K
+	value V
+}
+
+func (al *AssocList[K, V]) Add(key K, value V) {
+	al.lst = append(al.lst, pair[K, V]{key, value})
+}
+
+func (al *AssocList[K, V]) All(yield func(K, V) bool) bool {
+	for _, p := range al.lst {
+		if !yield(p.key, p.value) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	al := &AssocList[int, string]{}
+	al.Add(10, "ten")
+	al.Add(20, "twenty")
+	al.Add(5, "five")
+
+	fmt.Println(al)
+
+	for k, v := range al.All {
+		fmt.Printf("key=%v, value=%v\n", k, v)
+	}
+}
