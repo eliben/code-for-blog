ce1bdb070d0f8f89668d4bb36c47a5267eee510a
Eli Bendersky <eliben@gmail.com>
2023-07-27 14:05:42 -0700

    Fix benchmark and add comment

--- 2023/prime-sieve/benchmark.py
+++ 2023/prime-sieve/benchmark.py
@@ -7,12 +7,16 @@ from itertools import islice
 from sieve import gen_primes_upto, gen_primes   
 '''
 
-def bench_dict():
+def bench():
     gp = sieve.gen_primes()
     for i in range(9000):
         next(gp)
 
 def bench_upto():
+    # Note: for gen_primes_upto, we need to know ahead of time what limit to
+    # set. We happen to know that the first 9000 primes are < 100000, but only
+    # barely. If the limit is set too high, gen_primes_upto will do lots of
+    # unnecessary work.
     gp = sieve.gen_primes_upto(100000)
     for i in range(9000):
         next(gp)
@@ -21,7 +25,7 @@ t1 = timeit.Timer("bench_upto()", globals=locals())
 print("gen_primes_upto", t1.repeat(repeat=3, number=100))
 
 t2 = timeit.Timer("bench()", globals=locals())
-print("gen_primes", t1.repeat(repeat=3, number=100))
+print("gen_primes", t2.repeat(repeat=3, number=100))
 
 
 # print(timeit.timeit("bench_upto()", globals=locals(), number=100))
