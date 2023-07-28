a90f0e3c3fce60e89e34714eefdb0a1dd3ec5cf2
Eli Bendersky <eliben@gmail.com>
2023-07-28 06:50:21 -0700

    Fix issue in segmented and add test

--- 2023/prime-sieve/sieve.py
+++ 2023/prime-sieve/sieve.py
@@ -1,4 +1,4 @@
-from math import sqrt
+import math
 import itertools
 
 
@@ -11,7 +11,7 @@ def gen_primes_upto(n):
         return
 
     table = [True] * n
-    sqrtn = int(sqrt(n))
+    sqrtn = int(math.sqrt(n))
 
     for i in range(2, sqrtn + 1):
         if table[i]:
@@ -24,6 +24,35 @@ def gen_primes_upto(n):
             yield i
 
 
+def gen_primes_upto_segmented(n):
+    # Segment size
+    segsize = int(math.ceil(math.sqrt(n)))
+
+    # List of primes <= sqrt(n) which we'll use to sieve all segments
+    baseprimes = list(gen_primes_upto(segsize))
+
+    # print("base:", baseprimes)
+
+    for bp in baseprimes:
+        yield bp
+    
+    for segstart in range(segsize, n, segsize):
+        seg = [True] * segsize
+        segend = segstart + segsize
+
+        # print(f'seg start={segstart}, segend={segend}')
+        for bp in baseprimes:
+            first_multiple = segstart if segstart%bp==0 else segstart + bp - segstart%bp
+            for q in range(first_multiple, segend, bp):
+                seg[q % len(seg)] = False
+
+        for i in range(len(seg)):
+            if seg[i]:
+                if segstart+i >= n:
+                    break
+                yield segstart+i
+
+
 # TODO: measuring length of D -- insight:
 # it has all the primes in it, so its size is at least O(number of primes),
 # which is O(n / logn) per https://en.wikipedia.org/wiki/Prime-counting_function
@@ -32,6 +61,7 @@ def gen_primes_upto(n):
 # smaller than it, so they have to be *somewhere*.
 # An optimization would be only keep the primes below its square root, which
 # is what the segmentation optimizations are about
+
 
 def gen_primes():
     """Generate an infinite sequence of prime numbers."""
@@ -43,15 +73,15 @@ def gen_primes():
     q = 2
 
     while True:
-        print(f"-- {q}")
+        # print(f"-- {q}")
         # print("len of D =", len(D))
         if q not in D:
             # q is a new prime.
             # Yield it and mark its first multiple that isn't
             # already marked in previous iterations
             D[q * q] = [q]
-            print(f'Setting D[{q*q}]=[{q}]')
-            print(sorted(D.items()))
+            # print(f"Setting D[{q*q}]=[{q}]")
+            # print(sorted(D.items()))
             yield q
         else:
             # q is composite. D[q] is the list of primes that
@@ -64,24 +94,23 @@ def gen_primes():
             del D[q]
             # print(D)
             # print('len =', len(D), 'total size', sum(map(len, D.values())))
-            print(f'Removing D[{q}]')
-            print(sorted(D.items()))
+            # print(f"Removing D[{q}]")
+            # print(sorted(D.items()))
             # print(sum(map(len, D.values())))
 
         q += 1
-    
 
 
-# combine martelli's, hochberg's and beinecke optimizations from
-# https://code.activestate.com/recipes/117119-sieve-of-eratosthenes/#c2
-#
 # See Will Neiss's comment: relevant to segmented sieve (wikipedia) ?
 # And this tim peters answer: https://stackoverflow.com/a/19391111/8206
 # a bit more algorithm details on the answer above it
 # ... also related to the wheels mentioned in the paper
 # https://research.cs.wisc.edu/techreports/1990/TR909.pdf
 
+
 def gen_primes_opt():
+    # combines martelli's, hochberg's and beinecke optimizations from
+    # https://code.activestate.com/recipes/117119-sieve-of-eratosthenes/#c2
     yield 2
 
     D = {}
@@ -101,7 +130,10 @@ def gen_primes_opt():
 
 
 if __name__ == "__main__":
-    gen = gen_primes()
-    for i in range(30):
-        p = next(gen)
-        print(p)
+    # gen = gen_primes()
+    # for i in range(30):
+    #     p = next(gen)
+    #     print(p)
+
+    gg = gen_primes_upto_segmented(3)
+    print(list(gg))
--- 2023/prime-sieve/test_sieve.py
+++ 2023/prime-sieve/test_sieve.py
@@ -5,23 +5,35 @@ import unittest
 
 
 class TestSieve(unittest.TestCase):
-    def test_gen_primes_upto(self):
-        self.assertEqual(list(gen_primes_upto(2)), [])
-        self.assertEqual(list(gen_primes_upto(3)), [2])
-        self.assertEqual(list(gen_primes_upto(4)), [2, 3])
-        self.assertEqual(list(gen_primes_upto(6)), [2, 3, 5])
-        self.assertEqual(list(gen_primes_upto(7)), [2, 3, 5])
-        self.assertEqual(list(gen_primes_upto(8)), [2, 3, 5, 7])
-        self.assertEqual(list(gen_primes_upto(9)), [2, 3, 5, 7])
-        self.assertEqual(list(gen_primes_upto(10)), [2, 3, 5, 7])
-        self.assertEqual(list(gen_primes_upto(13)), [2, 3, 5, 7, 11])
-        self.assertEqual(list(gen_primes_upto(14)), [2, 3, 5, 7, 11, 13])
+    def _test_gen_primes_upto_variant(self, func):
+        self.assertEqual(list(func(2)), [])
+        self.assertEqual(list(func(3)), [2])
+        self.assertEqual(list(func(4)), [2, 3])
+        self.assertEqual(list(func(6)), [2, 3, 5])
+        self.assertEqual(list(func(7)), [2, 3, 5])
+        self.assertEqual(list(func(8)), [2, 3, 5, 7])
+        self.assertEqual(list(func(9)), [2, 3, 5, 7])
+        self.assertEqual(list(func(10)), [2, 3, 5, 7])
+        self.assertEqual(list(func(13)), [2, 3, 5, 7, 11])
+        self.assertEqual(list(func(14)), [2, 3, 5, 7, 11, 13])
+        self.assertEqual(list(func(15)), [2, 3, 5, 7, 11, 13])
+        self.assertEqual(list(func(16)), [2, 3, 5, 7, 11, 13])
+        self.assertEqual(list(func(17)), [2, 3, 5, 7, 11, 13])
+        self.assertEqual(list(func(18)), [2, 3, 5, 7, 11, 13, 17])
+        self.assertEqual(list(func(19)), [2, 3, 5, 7, 11, 13, 17])
+        self.assertEqual(list(func(20)), [2, 3, 5, 7, 11, 13, 17, 19])
+        self.assertEqual(list(func(21)), [2, 3, 5, 7, 11, 13, 17, 19])
 
-    def test_gen_primes(self):
-        self._test_gen_primes_variant(gen_primes)
-    
-    def test_gen_primes_opt(self):
-        self._test_gen_primes_variant(gen_primes_opt)
+        # Obtained by dowloading the list of the first 100k primes from
+        # https://www.mathsisfun.com/numbers/prime-number-lists.html
+        # and summing the list on the command line.
+        self.assertEqual(sum(gen_primes_upto(99992)), 454396537)
+
+    def test_gen_primes_upto(self):
+        self._test_gen_primes_upto_variant(gen_primes_upto)
+
+    def test_gen_primes_upto_segmented(self):
+        self._test_gen_primes_upto_variant(gen_primes_upto_segmented)
 
     def _test_gen_primes_variant(self, func):
         # fist 20 primes
@@ -60,6 +72,13 @@ class TestSieve(unittest.TestCase):
             sum(takewhile(lambda x: x < 100000, func())),
             sum(gen_primes_upto(100000)),
         )
+
+    def test_gen_primes(self):
+        self._test_gen_primes_variant(gen_primes)
+    
+    def test_gen_primes_opt(self):
+        self._test_gen_primes_variant(gen_primes_opt)
+
 
 if __name__ == "__main__":
     unittest.main()
