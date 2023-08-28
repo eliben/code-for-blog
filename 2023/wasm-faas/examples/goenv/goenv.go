1cb81568b17a202e6021e17afc7a7539e257fbb0
Eli Bendersky <eliben@gmail.com>
2023-08-28 06:03:56 -0700

    Add wasmimport sample

--- 2023/wasm-faas/examples/goenv/goenv.go
+++ 2023/wasm-faas/examples/goenv/goenv.go
@@ -6,7 +6,13 @@ import (
 	"os"
 )
 
+// Show how to import functions from the host using go:wasmimport
+
+//go:wasmimport env log_i32
+func logInt(i int32)
+
 func main() {
+	logInt(42)
 	fmt.Println("goenv environment:")
 
 	for _, e := range os.Environ() {
