05c3a04d1c238e548f804b9d9086e4c55f342df7
Eli Bendersky <eliben@gmail.com>
2023-09-05 08:01:57 -0700

    Starting sample for serving JSON API and access through CORS

---
+++ 2023/cors/go-server-cors/serve-api.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, `{"message": "hello"}`)
+}
+
+func main() {
+	port := ":8080"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", apiHandler)
+	http.ListenAndServe(port, mux)
+}
