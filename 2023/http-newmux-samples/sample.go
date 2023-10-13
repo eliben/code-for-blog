f464ec5b91286eeb55dca213d824c477c9b87f7b
Eli Bendersky <eliben@gmail.com>
2023-10-13 07:46:19 -0700

    Starting sample

---
+++ 2023/http-newmux-samples/sample.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /path/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "got path\n")
+	})
+
+	mux.HandleFunc("/task/{id}/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "handling task with id=%v\n", id)
+	})
+
+	http.ListenAndServe("localhost:8090", mux)
+}
