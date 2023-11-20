7c783b8494977fce95da330d5791fd2870fb1b7f
Eli Bendersky <eliben@gmail.com>
2023-11-20 07:27:26 -0800

    completion example

---
+++ 2023/ollama-go-langchain/ollama-completion-stream.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+func main() {
+	llm, err := ollama.New(ollama.WithModel("llama2"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := "very briefly, tell me the difference between a comet and a meteor"
+
+	ctx := context.Background()
+	_, err = llm.Call(ctx, query,
+		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			fmt.Printf("chunk len=%d: %s\n", len(chunk), chunk)
+			return nil
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
