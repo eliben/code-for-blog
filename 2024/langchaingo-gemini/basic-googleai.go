d3ad951eb46f9c968a5efb89d7fb3a8aed5c9e64
Eli Bendersky <eliben@gmail.com>
2024-01-26 13:20:18 -0800

    Basic example using googleai

---
+++ 2024/langchaingo-gemini/basic-googleai.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/googleai"
+)
+
+func main() {
+	ctx := context.Background()
+	apiKey := os.Getenv("API_KEY")
+	llm, err := googleai.NewGoogleAI(ctx, googleai.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prompt := "What is the L2 Lagrange point?"
+	answer, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(answer)
+}
