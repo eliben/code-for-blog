2e28dd412e2c0db3d9cd17767602399e8d0604e4
Eli Bendersky <eliben@gmail.com>
2024-01-26 16:15:40 -0800

    multimodal sample

---
+++ 2024/langchaingo-gemini/multimodal-googleai.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/googleai"
+	"github.com/tmc/langchaingo/schema"
+)
+
+const imagesPath = "../../2023/go-google-ai-gemini/images"
+
+func main() {
+	ctx := context.Background()
+	apiKey := os.Getenv("API_KEY")
+	llm, err := googleai.New(ctx, googleai.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imgData1, err := os.ReadFile(filepath.Join(imagesPath, "turtle1.png"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imgData2, err := os.ReadFile(filepath.Join(imagesPath, "turtle2.png"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parts := []llms.ContentPart{
+		llms.BinaryContent{
+			MIMEType: "image/png",
+			Data:     imgData1,
+		},
+		llms.BinaryContent{
+			MIMEType: "image/png",
+			Data:     imgData2,
+		},
+		llms.TextContent{
+			Text: "Describe the difference between these two pictures, with scientific detail",
+		},
+	}
+
+	content := []llms.MessageContent{
+		{
+			Role:  schema.ChatMessageTypeHuman,
+			Parts: parts,
+		},
+	}
+
+	resp, err := llm.GenerateContent(ctx, content, llms.WithModel("gemini-pro-vision"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bs, _ := json.MarshalIndent(resp, "", "    ")
+	fmt.Println(string(bs))
+}
