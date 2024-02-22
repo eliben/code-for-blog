1498abc94c6d4c689ca8d9189f99e5cedede005f
Eli Bendersky <eliben@gmail.com>
2024-02-22 15:23:57 -0800

    Update API usage for newer langchaingo

--- 2023/ollama-go-langchain/ollama-completion-stream.go
+++ 2023/ollama-go-langchain/ollama-completion-stream.go
@@ -22,7 +22,7 @@ func main() {
 	query := "very briefly, tell me the difference between a comet and a meteor"
 
 	ctx := context.Background()
-	_, err = llm.Call(ctx, query,
+	_, err = llms.GenerateFromSinglePrompt(ctx, llm, query,
 		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
 			fmt.Printf("chunk len=%d: %s\n", len(chunk), chunk)
 			return nil
