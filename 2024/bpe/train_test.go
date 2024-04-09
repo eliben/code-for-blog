178f1e4f177ae9fe319deb69910a0c8a0d66cc84
Eli Bendersky <eliben@gmail.com>
2024-04-09 06:05:53 -0700

    comments and cleanup

--- 2024/bpe/train_test.go
+++ 2024/bpe/train_test.go
@@ -5,6 +5,8 @@ import "testing"
 const gpt2splitPattern = `'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+`
 
 func TestTrainBasic(t *testing.T) {
+	debugTrain = true // TODO
+
 	txt := "low low low low low lowest lowest newer newer newer newer newer newer wider wider wider new new"
 	v := train(txt, 275, gpt2splitPattern)
 	_ = v
