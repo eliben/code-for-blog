adbb65900b25bb254eea749e3cdf0febe42b64e2
Eli Bendersky <eliben@gmail.com>
2024-04-09 09:15:29 -0700

    add loading with test and a vocab

--- 2024/bpe/encode.go
+++ 2024/bpe/encode.go
@@ -9,7 +9,15 @@ import (
 
 const debugEncode = false
 
+// encode runs a trained BPE tokenizer over the given text. vocab is the learned
+// map of tokens (mapping token strings to their numeric IDs). splitPattern is
+// the regexp pattern to use for the initial splitting of text to words. Returns
+// a list of tokens representing the text.
+// Note: expects all tokens in text - including any single byte value - to
+// appear in vocab; if an unknown token is found, this function panics.
 func encode(text string, vocab map[string]int, splitPattern string) []int {
+	// Split the input into words using the given splitPattern.
+	// Each word is split further into a list of single bytes.
 	var words [][]string
 	re := regexp2.MustCompile(splitPattern, regexp2.None)
 	m, _ := re.FindStringMatch(text)
@@ -31,8 +39,14 @@ func encode(text string, vocab map[string]int, splitPattern string) []int {
 
 	var tokens []int
 
+	// We convert each word in order, adding its tokenized representation to
+	// tokens.
 	for _, word := range words {
 		for {
+			// Find a pair of tokens in word that we can combine to a longer token
+			// appearing in vocab. BPE requires us to find the earliest token that
+			// was merged in the training process (it has the lowest "order" - ID
+			// in vocab).
 			minIdx := -1
 			minVocabOrder := len(vocab) + 1
 			for i := 0; i < len(word)-1; i++ {
@@ -45,13 +59,19 @@ func encode(text string, vocab map[string]int, splitPattern string) []int {
 			}
 
 			if minIdx >= 0 {
+				// If we find a pair to merge, we merge it in word, leaving the other
+				// tokens intact, and run again...
 				tokenText := word[minIdx] + word[minIdx+1]
 				word = slices.Replace(word, minIdx, minIdx+2, tokenText)
 			} else {
+				// No more pairs found to merge; we're done with this word.
 				break
 			}
 		}
 
+		// Now we're guaranteed to have the word consist of tokens which appear in
+		// vocab (since we assume vocab contains each single byte as a token too).
+		// Convert the word into a list of token IDs.
 		for _, tokenText := range word {
 			if token, ok := vocab[tokenText]; ok {
 				tokens = append(tokens, token)
---
+++ 2024/bpe/load_test.go
@@ -0,0 +1,30 @@
+package bpe
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestLoadCl100kBase(t *testing.T) {
+	r, err := os.Open(filepath.Join("data", "cl100k_base.tiktoken"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	vocab, err := loadTiktokenVocab(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Sanity checking that we've loaded it all correctly.
+	if len(vocab) != 100256 {
+		t.Errorf("got len %v, want 100256", len(vocab))
+	}
+	if vocab["a"]+1 != vocab["b"] {
+		t.Errorf("got %v %v, want consecutive vocab for 'a' and 'b'", vocab["a"], vocab["b"])
+	}
+
+	if _, ok := vocab["many"]; !ok {
+		t.Errorf("'many' not found in vocab")
+	}
+}
