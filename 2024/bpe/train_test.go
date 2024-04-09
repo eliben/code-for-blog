117cfa6af1fe3aed651c4e76f4b54b1339d260fb
Eli Bendersky <eliben@gmail.com>
2024-04-09 06:18:29 -0700

    more debugging spew and better test

--- 2024/bpe/train_test.go
+++ 2024/bpe/train_test.go
@@ -1,13 +1,22 @@
 package bpe
 
-import "testing"
+import (
+	"testing"
+)
 
 const gpt2splitPattern = `'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+`
 
 func TestTrainBasic(t *testing.T) {
 	debugTrain = true // TODO
 
-	txt := "low low low low low lowest lowest newer newer newer newer newer newer wider wider wider new new"
-	v := train(txt, 275, gpt2splitPattern)
-	_ = v
+	txt := "i'm blue dabadee dabadam 999999"
+	vocab := train(txt, 258, gpt2splitPattern)
+
+	if len(vocab) != 258 {
+		t.Errorf("got len(vocab)=%d, want 258", len(vocab))
+	}
+
+	if vocab["99"] != 256 || vocab["da"] != 257 {
+		t.Errorf(`vocab["99"]=%v, vocab["da"]=%v`, vocab["99"], vocab["da"])
+	}
 }
