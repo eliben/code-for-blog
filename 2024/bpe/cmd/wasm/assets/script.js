a6b76a235021bce6bf8ded8872bfbf89950fd418
Eli Bendersky <eliben@gmail.com>
2024-04-10 10:45:34 -0700

    tokens in js

--- 2024/bpe/cmd/wasm/assets/script.js
+++ 2024/bpe/cmd/wasm/assets/script.js
@@ -7,11 +7,22 @@ Flipx.addEventListener("change", onStateChange);
 const Flipy = document.querySelector("#flipy");
 Flipy.addEventListener("change", onStateChange);
 
-// Trigger a redraw to get started.
-onStateChange();
+function init() {
+    // Trigger a redraw to get started.
+    onStateChange();
+}
 
 //------------------
 
 function onStateChange() {
     console.log("state changed");
+    const text = TextBox.value;
+
+    // Measure the time it takes to invoke textToBPETokens
+    const start = performance.now();
+    let tokens = textToBPETokens(text);
+    const end = performance.now();
+    console.log("Time taken (ms): ", end - start);
+    console.log(tokens);
 }
+
--- 2024/bpe/cmd/wasm/main.go
+++ 2024/bpe/cmd/wasm/main.go
@@ -29,6 +29,7 @@ func main() {
 	})
 
 	js.Global().Set("itsAlive", jsItsAlive)
+	js.Global().Set("textToBPETokens", jsTextToBPETokens)
 
 	// For the Go code to be usable from JS, the main function has to run forever.
 	<-make(chan bool)
@@ -43,3 +44,23 @@ func itsAlive() string {
 	fmt.Printf("vocabulary loaded, len=%v\n", len(vocab))
 	return "go is alive"
 }
+
+// textToBPETokens takes text, tokenizes it with BPE using the loaded vocabulary
+// and returns the token IDs.
+func textToBPETokens(txt string) []int {
+	return bpe.Encode(txt, vocab, bpe.CL100KBaseSplitPattern)
+}
+
+var jsTextToBPETokens = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	if len(args) != 1 {
+		return "expected 1 argument: text to tokenize"
+	}
+	txt := args[0].String()
+	tokens := textToBPETokens(txt)
+
+	jsTokens := js.Global().Get("Array").New()
+	for _, s := range tokens {
+		jsTokens.Call("push", js.ValueOf(s))
+	}
+	return jsTokens
+})
