25de4a35b7215c2df98f8dfbd4d5284a6c8f43a7
Eli Bendersky <eliben@gmail.com>
2024-04-10 11:15:39 -0700

    show tokens and text fragments

--- 2024/bpe/cmd/wasm/main.go
+++ 2024/bpe/cmd/wasm/main.go
@@ -15,6 +15,7 @@ import (
 var vocabFileData string
 
 var vocab map[string]int
+var decoder *bpe.Decoder
 
 func main() {
 	var once sync.Once
@@ -25,11 +26,13 @@ func main() {
 		}
 		vocab = v
 
-		fmt.Printf("vocabulary loaded, len=%v\n", len(vocab))
+		decoder = bpe.NewDecoder(vocab)
+		fmt.Printf("vocabulary loaded, len=%v; decoder initialized\n", len(vocab))
 	})
 
 	js.Global().Set("itsAlive", jsItsAlive)
 	js.Global().Set("textToBPETokens", jsTextToBPETokens)
+	js.Global().Set("textToBPEFragments", jsTextToBPEFragments)
 
 	// For the Go code to be usable from JS, the main function has to run forever.
 	<-make(chan bool)
@@ -60,6 +63,27 @@ var jsTextToBPETokens = js.FuncOf(func(this js.Value, args []js.Value) interface
 
 	jsTokens := js.Global().Get("Array").New()
 	for _, s := range tokens {
+		jsTokens.Call("push", js.ValueOf(s))
+	}
+	return jsTokens
+})
+
+// textToBPEFragments takes text, tokenizes it with BPE using the loaded
+// vocabulary and returns the decoded token fragments.
+func textToBPEFragments(txt string) []string {
+	toks := bpe.Encode(txt, vocab, bpe.CL100KBaseSplitPattern)
+	return decoder.Decode(toks)
+}
+
+var jsTextToBPEFragments = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	if len(args) != 1 {
+		return "expected 1 argument: text to tokenize"
+	}
+	txt := args[0].String()
+	fragments := textToBPEFragments(txt)
+
+	jsTokens := js.Global().Get("Array").New()
+	for _, s := range fragments {
 		jsTokens.Call("push", js.ValueOf(s))
 	}
 	return jsTokens
