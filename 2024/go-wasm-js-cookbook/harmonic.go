5f67ab7c71bbfcbaae6adf4767be0b9e53f841be
Eli Bendersky <eliben@gmail.com>
2024-09-06 14:17:57 -0700

    Initial scaffolding

---
+++ 2024/go-wasm-js-cookbook/harmonic.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"math/big"
+	"syscall/js"
+	"time"
+)
+
+func main() {
+	js.Global().Set("calcHarmonic", jsCalcHarmonic)
+
+	select {}
+}
+
+// calcHarmonic calculates the harmonic series for approximately the given
+// number of seconds and returns the accumulated result in a string.
+func calcHarmonic(nsecs int) string {
+	d := time.Duration(nsecs) * time.Second
+	start := time.Now()
+	r1 := big.NewRat(1, 1)
+	for i := 2; ; i++ {
+		addend := big.NewRat(1, int64(i))
+		r1 = r1.Add(r1, addend)
+
+		if i%10 == 0 && time.Now().Sub(start) >= d {
+			break
+		}
+	}
+	return r1.FloatString(50)
+}
+
+var jsCalcHarmonic = js.FuncOf(func(this js.Value, args []js.Value) any {
+	if len(args) != 1 {
+		panic("want one argument")
+	}
+
+	s := calcHarmonic(args[0].Int())
+	return js.ValueOf(s)
+})
