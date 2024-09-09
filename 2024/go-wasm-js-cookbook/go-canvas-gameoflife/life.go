e8ae4e1e90da56cd1d377a008e5cbdb334e1a8d4
Eli Bendersky <eliben@gmail.com>
2024-09-09 06:00:57 -0700

    Finish game logic

--- 2024/go-wasm-js-cookbook/go-canvas-gameoflife/life.go
+++ 2024/go-wasm-js-cookbook/go-canvas-gameoflife/life.go
@@ -13,13 +13,18 @@ const cellSize = 20
 const numRows = 25
 const numCols = 25
 
+// Period (in milliseconds) between re-draws when running
+const runPeriod = 300
+
 type gameState struct {
 	canvas     js.Value
 	runButton  js.Value
 	stepButton js.Value
 
-	// Grid of cells
+	// Game of life grid; true means the cell is alive.
 	grid [numRows][numCols]bool
+
+	running bool
 }
 
 func main() {
@@ -30,7 +35,7 @@ func main() {
 		runButton:  doc.Call("getElementById", "runButton"),
 		stepButton: doc.Call("getElementById", "stepButton"),
 	}
-	state.initialize()
+	state.initializeGrid()
 
 	state.canvas.Call("addEventListener", "click", js.FuncOf(
 		func(this js.Value, args []js.Value) any {
@@ -46,7 +51,7 @@ func main() {
 		}))
 	state.stepButton.Call("addEventListener", "click", js.FuncOf(
 		func(this js.Value, args []js.Value) any {
-			state.onRunClicked()
+			state.onStepClicked()
 			return nil
 		}))
 
@@ -55,7 +60,7 @@ func main() {
 	select {}
 }
 
-func (state *gameState) initialize() {
+func (state *gameState) initializeGrid() {
 	// Draw a glider in the center of the grid
 	state.grid[12][11] = true
 	state.grid[12][12] = true
@@ -65,6 +70,7 @@ func (state *gameState) initialize() {
 }
 
 func (state *gameState) onCanvasClicked(x, y int) {
+	defer state.draw()
 	r := y / cellSize
 	c := x / cellSize
 
@@ -73,14 +79,36 @@ func (state *gameState) onCanvasClicked(x, y int) {
 }
 
 func (state *gameState) onStepClicked() {
+	defer state.draw()
+	state.advance()
 }
 
 func (state *gameState) onRunClicked() {
+	defer state.draw()
+	if state.running {
+		state.running = false
+		state.runButton.Set("innerText", "Run")
+		return
+	}
+
+	state.running = true
+	state.runButton.Set("innerText", "Stop")
+
+	js.Global().Call("setTimeout", js.FuncOf(func(this js.Value, args []js.Value) any {
+		state.runningStep()
+		return nil
+	}), runPeriod)
 }
 
-// updateStep updates single step of game of life rules.
-func (state *gameState) updateStep() {
-	// TODO: Implement game of life rules
+func (state *gameState) runningStep() {
+	if state.running {
+		state.advance()
+		state.draw()
+		js.Global().Call("setTimeout", js.FuncOf(func(this js.Value, args []js.Value) any {
+			state.runningStep()
+			return nil
+		}), runPeriod)
+	}
 }
 
 func (state *gameState) draw() {
@@ -97,7 +125,9 @@ func (state *gameState) draw() {
 			cctx.Call("fillRect", c*cellSize, r*cellSize, cellSize, cellSize)
 		}
 	}
+
 	// Horizontal lines
+	cctx.Set("strokeStyle", "#000000")
 	for r := range numRows {
 		cctx.Call("beginPath")
 		cctx.Call("moveTo", 0, (r+1)*cellSize-1)
@@ -112,4 +142,44 @@ func (state *gameState) draw() {
 		cctx.Call("lineTo", (c+1)*cellSize-1, numRows*cellSize)
 		cctx.Call("stroke")
 	}
+}
+
+// advance updates the game state to the next generation using game of life
+// rules.
+func (state *gameState) advance() {
+	countNeighbors := func(r, c int) int {
+		count := 0
+		for dr := -1; dr <= 1; dr++ {
+			for dc := -1; dc <= 1; dc++ {
+				if dr == 0 && dc == 0 {
+					continue
+				}
+
+				r2 := r + dr
+				c2 := c + dc
+				if r2 < 0 || r2 >= numRows || c2 < 0 || c2 >= numCols {
+					continue
+				}
+
+				if state.grid[r2][c2] {
+					count++
+				}
+			}
+		}
+		return count
+	}
+
+	newGrid := [numRows][numCols]bool{}
+	for r := range numRows {
+		for c := range numCols {
+			neighbors := countNeighbors(r, c)
+			if state.grid[r][c] {
+				newGrid[r][c] = neighbors == 2 || neighbors == 3
+			} else {
+				newGrid[r][c] = neighbors == 3
+			}
+		}
+	}
+
+	state.grid = newGrid
 }
