9c2bbb381326c4a58f07031bb6e8ab086fa9b01d
Eli Bendersky <eliben@gmail.com>
2024-11-07 13:42:48 -0800

    Add global gitignore and fix typo

--- 2024/go-py-ml/jax-gemma-server/run-gemma-server.sh
+++ 2024/go-py-ml/jax-gemma-server/run-gemma-server.sh
@@ -14,7 +14,7 @@ source $HOME/bin/venv/gemma-jax-gpu/bin/activate
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 export XLA_PYTHON_CLIENT_ALLOCATOR=platform
 
-# Point to the model checkpoint and tokenizer file downloaded from Huggingface.
+# Point to the model checkpoint and tokenizer file downloaded from Kaggle.
 export MODEL_CHECKPOINT=/home/eliben/Downloads/2b-it/
 export MODEL_TOKENIZER=/home/eliben/Downloads/tokenizer.model
 
