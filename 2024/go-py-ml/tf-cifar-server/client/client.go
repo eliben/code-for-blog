6e279881175a428fee29c3092459258c44b1e4dd
Eli Bendersky <eliben@gmail.com>
2024-11-08 06:45:33 -0800

    Refactor client measurement

--- 2024/go-py-ml/tf-cifar-server/client/client.go
+++ 2024/go-py-ml/tf-cifar-server/client/client.go
@@ -35,6 +35,26 @@ func readPacket(c net.Conn) (int, []byte) {
 	return int(buf[0]), buf[1:]
 }
 
+func runBenchmark(c net.Conn, numIters int) {
+	// Create a []byte with 3072 bytes.
+	body := make([]byte, 3072)
+	for i := range body {
+		body[i] = byte(i % 256)
+	}
+
+	t1 := time.Now()
+	for range numIters {
+		sendPacket(c, 0, body)
+		cmd, resp := readPacket(c)
+		if cmd != 0 || len(resp) != len(body) {
+			log.Fatal("bad response")
+		}
+	}
+	elapsed := time.Since(t1)
+	fmt.Printf("Num packets: %d, Elapsed time: %s\n", numIters, elapsed)
+	fmt.Printf("Average time per request: %d ns\n", elapsed.Nanoseconds()/int64(numIters))
+}
+
 func main() {
 	// Flags:
 	// -measure <n>: measure the time to send <n> packets
@@ -51,23 +71,7 @@ func main() {
 	defer c.Close()
 
 	if *measureFlag > 0 {
-		// Create a []byte with 3072 bytes.
-		body := make([]byte, 3072)
-		for i := range body {
-			body[i] = byte(i % 256)
-		}
-
-		t1 := time.Now()
-		for range *measureFlag {
-			sendPacket(c, 0, body)
-			cmd, resp := readPacket(c)
-			if cmd != 0 || len(resp) != len(body) {
-				log.Fatal("bad response")
-			}
-		}
-		elapsed := time.Since(t1)
-		fmt.Printf("Num packets: %d, Elapsed time: %s\n", *measureFlag, elapsed)
-		fmt.Printf("Average time per request: %d ns\n", elapsed.Nanoseconds()/int64(*measureFlag))
+		runBenchmark(c, *measureFlag)
 	} else if len(*classifyFlag) > 0 {
 
 	} else {
