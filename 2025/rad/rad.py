d85ee443149459f2764802121ef1ed618e727335
Eli Bendersky <eliben@gmail.com>
2025-01-11 07:11:24 -0800

    Comments and tweaks

--- 2025/rad/rad.py
+++ 2025/rad/rad.py
@@ -13,6 +13,15 @@ class Predecessor:
     multiplier: float
     var: "Var"
 
+# A Var represents a node in the computational graph. When expressions are
+# constructed out of Var values (using operator overloading and custom math
+# function wrappers), in addition to calculating the value, the computational
+# graph is also implicitly built in the background.
+# The graph is represented by a list of predecessors for each Var, where
+# each predecessor points to the other Vars that feed into this var. For each
+# predecessor we also hold a "multiplier", which is the derivative of the
+# expression this Var represents with respect to the predecessor Var.
+# When .grad() is called, it backpropagates the gradient through the graph.
 
 class Var:
     def __init__(self, v):
@@ -99,7 +108,6 @@ def sin(x):
     """sin(x)"""
     x = ensure_var(x)
     out = Var(math.sin(x.v))
-
     out.predecessors.append(Predecessor(math.cos(x.v), x))
     return out
 
@@ -110,17 +118,17 @@ if __name__ == "__main__":
     # participating Vars should not be reused.
     xx = Var(0.5)
     sigmoid = 1 / (1 + exp(-xx))
-    print(f"xx={xx.v}, sigmoid={sigmoid.v}")
+    print(f"xx = {xx.v:.2}, sigmoid = {sigmoid.v:.2}")
 
     sigmoid.grad(1.0)
-    print(f"dsigmoid/dxx = {xx.gv}")
+    print(f"dsigmoid/dxx = {xx.gv:.2}")
 
     # Example from the paper "Automatic Differentiation in Machine Learning:
     # a Survey" by Baydin et al.
-    x1 = Var(2)
-    x2 = Var(5)
+    x1 = Var(2.0)
+    x2 = Var(5.0)
     f = log(x1) + x1 * x2 - sin(x2)
-    print(f"x1={x1.v}, x2={x2.v}, f={f.v}")
+    print(f"x1 = {x1.v:.2}, x2 = {x2.v:.2}, f = {f.v:.2}")
 
     f.grad(1.0)
-    print(f"df/dx1={x1.gv}, df/dx2={x2.gv}")
+    print(f"df/dx1 = {x1.gv:.2}, df/dx2 = {x2.gv:.2}")
