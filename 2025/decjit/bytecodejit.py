98c6f1c4cc75fba673f913397c2695883ea376a6
Eli Bendersky <eliben@gmail.com>
2025-01-28 12:29:45 -0800

    Initial exprcode emission from bytecode

--- 2025/decjit/bytecodejit.py
+++ 2025/decjit/bytecodejit.py
@@ -2,8 +2,57 @@ import functools
 import dis
 
 
+from exprcode import (
+    VarExpr,
+    ConstantExpr,
+    BinOpExpr,
+    Op,
+    llvm_jit_evaluate,
+)
+
+
 class BytecodeJITError(Exception):
     pass
+
+
+def emit_exprcode(func):
+    bc = func.__code__
+    print(f"co_varnames = {bc.co_varnames}")
+    print(f"co_argcount = {bc.co_argcount}")
+    for inst in dis.get_instructions(func):
+        print(inst)
+
+    stack = []
+    for inst in dis.get_instructions(func):
+        match inst.opname:
+            case "LOAD_FAST":
+                idx = inst.arg
+                stack.append(VarExpr(bc.co_varnames[idx], idx))
+            case "LOAD_CONST":
+                stack.append(ConstantExpr(inst.argval))
+            case "BINARY_OP":
+                right = stack.pop()
+                left = stack.pop()
+                match inst.argrepr:
+                    case "+":
+                        stack.append(BinOpExpr(left, right, Op.ADD))
+                    case "-":
+                        stack.append(BinOpExpr(left, right, Op.SUB))
+                    case "*":
+                        stack.append(BinOpExpr(left, right, Op.MUL))
+                    case "/":
+                        stack.append(BinOpExpr(left, right, Op.DIV))
+                    case _:
+                        raise BytecodeJITError(f"Unsupported operator {inst.argval}")
+            case "RETURN_VALUE":
+                if len(stack) != 1:
+                    raise BytecodeJITError("Invalid stack state")
+                return stack.pop()
+            case "RESUME" | "CACHE":
+                # Skip nops
+                pass
+            case _:
+                raise BytecodeJITError(f"Unsupported opcode {inst.opname}")
 
 
 def bytecodejit(func):
@@ -12,11 +61,8 @@ def bytecodejit(func):
         if kwargs:
             raise BytecodeJITError("Keyword arguments are not supported")
 
-        bc = func.__code__
-        print(f"co_varnames = {bc.co_varnames}")
-        print(f"co_argcount = {bc.co_argcount}")
-        for inst in dis.get_instructions(func):
-            print(inst)
+        expr = emit_exprcode(func)
+        print(expr)
 
         # emitter = ExprCodeEmitter()
         # emitter.visit(tree)
--- 2025/decjit/sample-bytecodejit.py
+++ 2025/decjit/sample-bytecodejit.py
@@ -3,8 +3,8 @@ from bytecodejit import bytecodejit
 
 @bytecodejit
 def some_expr(a, b, c):
-    return a + b + 1
-    # return b / (a + 2) - c * (b - a)
+    # return a + b + 1
+    return b / (a + 2) - c * (b - a)
 
 
 some_expr(2, 16, 3)
