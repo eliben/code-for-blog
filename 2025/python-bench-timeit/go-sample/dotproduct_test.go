12ca00652bb258bec95f5f5752bdf734a5445614
Eli Bendersky <eliben@gmail.com>
2025-02-08 07:25:39 -0800

    Change to dot product, for simplicity

--- 2025/python-bench-timeit/go-sample/dotproduct_test.go
+++ 2025/python-bench-timeit/go-sample/dotproduct_test.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"math"
 	"math/rand/v2"
 	"testing"
 )
@@ -16,24 +15,31 @@ func makeRandSlice(sz int) []float32 {
 	return s
 }
 
-func cosSimGo(a, b []float32) float32 {
-	var dot, na, nb float32
-	for i, x := range a {
-		y := b[i]
-		dot += x * y
-		na += x * x
-		nb += y * y
+func dotProduct(a, b []float32) float32 {
+	var dot float32
+	for i := range a {
+		dot += a[i] * b[i]
 	}
-	return dot / float32(math.Sqrt(float64(na*nb)))
+	return dot
 }
 
 const benchArrSize = 1 * 1024 * 1024
 
-func BenchmarkCosSimGo(b *testing.B) {
+func BenchmarkDot(b *testing.B) {
 	aa := makeRandSlice(benchArrSize)
 	bb := makeRandSlice(benchArrSize)
 
 	for b.Loop() {
-		cosSimGo(aa, bb)
+		dotProduct(aa, bb)
+	}
+}
+
+func TestDotProduct(t *testing.T) {
+	a := []float32{1.0, 3.0, -2.0}
+	b := []float32{5.0, -2.0, -7.0}
+
+	want := float32(13.0)
+	if got := dotProduct(a, b); got != want {
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
