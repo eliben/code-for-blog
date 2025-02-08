17b3a98bb311f807f9939543101b6d25cebd4cca
Eli Bendersky <eliben@gmail.com>
2025-02-08 08:52:11 -0800

    Tests

---
+++ 2025/python-bench-timeit/test_dot.py
@@ -0,0 +1,28 @@
+# Unit tests for dot.py
+
+import unittest
+import dot
+
+
+class TestDot(unittest.TestCase):
+
+    def doTestDotFunc(self, dotfunc):
+        self.assertEqual(dotfunc([1, 2, 3], [4, 5, 6]), 32)
+        self.assertEqual(dotfunc([0, 0, 0], [0, 0, 0]), 0)
+        self.assertEqual(dotfunc([1], [34]), 34)
+        self.assertEqual(dotfunc([1, 2], [3, 5]), 13)
+        self.assertEqual(dotfunc([1, 2, 3, 5], [1, 2, 3, 6]), 44)
+        self.assertEqual(dotfunc([1, 2, 3], [3, 2, 1]), 10)
+
+    def test_dotProductLoop(self):
+        self.doTestDotFunc(dot.dotProductLoop)
+
+    def test_dotProductZip(self):
+        self.doTestDotFunc(dot.dotProductZip)
+
+    def test_dotProductStarmap(self):
+        self.doTestDotFunc(dot.dotProductStarmap)
+
+
+if __name__ == "__main__":
+    unittest.main()
