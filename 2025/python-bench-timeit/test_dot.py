0d8469a5cc23fedfb4e98f8d721f7e904f295be2
Eli Bendersky <eliben@gmail.com>
2025-02-10 06:19:05 -0800

    Reorder tests

--- 2025/python-bench-timeit/test_dot.py
+++ 2025/python-bench-timeit/test_dot.py
@@ -7,12 +7,13 @@ import dot
 class TestDot(unittest.TestCase):
 
     def doTestDotFunc(self, dotfunc):
-        self.assertEqual(dotfunc([1, 2, 3], [4, 5, 6]), 32)
+        self.assertEqual(dotfunc([], []), 0)
+        self.assertEqual(dotfunc([2], [34]), 68)
         self.assertEqual(dotfunc([0, 0, 0], [0, 0, 0]), 0)
-        self.assertEqual(dotfunc([1], [34]), 34)
         self.assertEqual(dotfunc([1, 2], [3, 5]), 13)
-        self.assertEqual(dotfunc([1, 2, 3, 5], [1, 2, 3, 6]), 44)
+        self.assertEqual(dotfunc([1, 2, 3], [4, 5, 6]), 32)
         self.assertEqual(dotfunc([1, 2, 3], [3, 2, 1]), 10)
+        self.assertEqual(dotfunc([1, 2, 3, 5], [1, 2, 3, 6]), 44)
 
     def test_dotProductLoop(self):
         self.doTestDotFunc(dot.dotProductLoop)
