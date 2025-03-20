2914598387ce45019f5ae1a8e9a2ede56e709415
Eli Bendersky <eliben@gmail.com>
2025-03-20 10:58:42 -0700

    Run thru ruff check/format

--- 2025/einsum-impl/impl.py
+++ 2025/einsum-impl/impl.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def translate_einsum(subscript):
     """Translates an explicit np.einsum subscript into Python code.
 
@@ -10,22 +11,23 @@ def translate_einsum(subscript):
 
         def calc(__a, __b):
             ... return out
-    
+
     And it will calculate the matmul of __a and __b.
     """
     # split by ->
-    ins_s, out = map(str.strip, subscript.split('->'))
-    inputs = list(map(str.strip, ins_s.split(',')))
+    ins_s, out = map(str.strip, subscript.split("->"))
+    inputs = list(map(str.strip, ins_s.split(",")))
 
     lines = []
     indent = 0
-    def emit_line(line=''):
-        prefix = ' ' * indent
+
+    def emit_line(line=""):
+        prefix = " " * indent
         lines.append(prefix + line)
-    
+
     # We have len(inputs) input arrays to the emitted function; their names
     # will be '__a', '__b', '__c', etc.
-    input_names = ['__'+chr(ord('a') + i) for i in range(len(inputs))]
+    input_names = ["__" + chr(ord("a") + i) for i in range(len(inputs))]
 
     emit_line(f"def calc({', '.join(input_names)}):")
     indent += 4
@@ -61,37 +63,37 @@ def translate_einsum(subscript):
         indent += 4
 
     # Create the indexing expressions for each input array and the output array.
-    out_access = f'out[{', '.join(label for label in out)}]'
+    out_access = f"out[{', '.join(label for label in out)}]"
     in_accesses = [
-        f'{input_name}[{', '.join(label for label in input)}]'
+        f"{input_name}[{', '.join(label for label in input)}]"
         for input, input_name in zip(inputs, input_names)
     ]
 
     # Determine if there are input dimensions that were not used in the output.
     # If so, we'll need to sum over them.
-    if unused_dims := {label for label in ''.join(inputs)} - set(out):
+    if unused_dims := {label for label in "".join(inputs)} - set(out):
         for label in unused_dims:
             emit_line(f"for {label} in range({label}_size):")
             indent += 4
 
-        emit_line(f'{out_access} += {' * '.join(in_accesses)}')
+        emit_line(f"{out_access} += {' * '.join(in_accesses)}")
     else:
         # If all input dimensions are used in the output, we can just assign
         # the product directly.
-        emit_line(f'{out_access} = {' * '.join(in_accesses)}')
+        emit_line(f"{out_access} = {' * '.join(in_accesses)}")
 
     indent = saved_indent
-    emit_line('return out')
-    return '\n'.join(lines)
+    emit_line("return out")
+    return "\n".join(lines)
 
 
 if __name__ == "__main__":
     A = np.arange(6).reshape(2, 3)
-    B = np.arange(12).reshape(3, 4)+1
+    B = np.arange(12).reshape(3, 4) + 1
 
-    func_code = translate_einsum('ij,jk->ik')
+    func_code = translate_einsum("ij,jk->ik")
     print(func_code)
 
-    namespace = {'np': np}
+    namespace = {"np": np}
     exec(func_code, namespace)
-    print(namespace['calc'](A, B))
+    print(namespace["calc"](A, B))
--- 2025/einsum-impl/test_impl.py
+++ 2025/einsum-impl/test_impl.py
@@ -3,61 +3,69 @@ from impl import translate_einsum
 
 
 def run_test(subscript, *args, show_func=False):
-    namespace = {'np': np}
+    namespace = {"np": np}
     if show_func:
-        print('\n'+translate_einsum(subscript))
+        print("\n" + translate_einsum(subscript))
     exec(translate_einsum(subscript), namespace)
-    got = namespace['calc'](*args)
+    got = namespace["calc"](*args)
     want = np.einsum(subscript, *args)
     assert np.allclose(got, want)
 
 
 def test_translate_einsum():
     A = np.arange(6).reshape(2, 3)
-    B = np.arange(12).reshape(3, 4)+1
+    B = np.arange(12).reshape(3, 4) + 1
 
     # Basic matmul
-    run_test('ij,jk->ik', A, B)
+    run_test("ij,jk->ik", A, B)
 
     # Try the same with some whitespace in the subscript
-    run_test('ij, jk -> ik', A, B)
-    run_test('  ij, jk -> ik', A, B)
-    run_test('  ij, jk  -> ik  ', A, B)
-    
+    run_test("ij, jk -> ik", A, B)
+    run_test("  ij, jk -> ik", A, B)
+    run_test("  ij, jk  -> ik  ", A, B)
+
     # Transpose output
-    run_test('ij,jk->ki', A, B)
+    run_test("ij,jk->ki", A, B)
 
     # Tranpose input
-    run_test('ij,kj->ik', A, A)
+    run_test("ij,kj->ik", A, A)
 
     # Not matmul because of different indices: this is a double loop sum
-    run_test('ij,pk->ik', A, B)
+    run_test("ij,pk->ik", A, B)
 
     # Outer product
     v1 = np.arange(8)
-    v2 = np.arange(5)+2
-    run_test('i,j->ij', v1, v2)
+    v2 = np.arange(5) + 2
+    run_test("i,j->ij", v1, v2)
 
     # Batched matmuls
-    Ab = np.arange(6*6).reshape(6,2,3)
-    Bb = np.arange(6*12).reshape(6,3,4)
+    Ab = np.arange(6 * 6).reshape(6, 2, 3)
+    Bb = np.arange(6 * 12).reshape(6, 3, 4)
 
-    run_test('bmd,bdn->bmn', Ab, Bb)
+    run_test("bmd,bdn->bmn", Ab, Bb)
 
     # Two batch dimensions
-    m = 4; d = 3; k = 6; h = 5; b = 10
+    m = 4
+    d = 3
+    k = 6
+    h = 5
+    b = 10
     Pk = np.random.randn(h, d, k)
     M = np.random.randn(b, m, d)
 
-    run_test('bmd,hdk->bhmk', M, Pk)
-    run_test('bmd,hdk->hbmk', M, Pk)
+    run_test("bmd,hdk->bhmk", M, Pk)
+    run_test("bmd,hdk->hbmk", M, Pk)
 
     # Contraction in more than one dimension
-    b = 10; n = 4; d = 3; v = 6; h = 5
-    O = np.random.randn(b, h, n, v)
+    b = 10
+    n = 4
+    d = 3
+    v = 6
+    h = 5
+    Ox = np.random.randn(b, h, n, v)
     Po = np.random.randn(h, d, v)
-    run_test('bhnv,hdv->bnd', O, Po)
+    run_test("bhnv,hdv->bnd", Ox, Po)
 
     # More than two input arrays
     C = np.arange(20).reshape(4, 5)
-    run_test('ij,jk,kl->il', A, B, C)
+    run_test("ij,jk,kl->il", A, B, C)
