7bc613a9981cef51a3f625c42efc35e89bd7daa0
Eli Bendersky <eliben@gmail.com>
2025-09-20 09:51:52 -0700

    Tikz dump of circle

--- 2025/consistent-hashing/demo.go
+++ 2025/consistent-hashing/demo.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"math/rand/v2"
+	"os"
+	"strings"
 )
 
 func demo1() {
@@ -35,7 +38,7 @@ func dumpDistribution() {
 	}
 
 	cnt := make(map[string]int)
-	for range 1000 {
+	for range 100000 {
 		str := generateRandomString(rnd, 16)
 		nn := ch.FindNodeFor(str)
 		cnt[nn] += 1
@@ -46,6 +49,60 @@ func dumpDistribution() {
 	}
 }
 
+var tikzTemplate = template.Must(template.New("t").Parse(`
+\documentclass[tikz]{standalone}
+\usepackage{tikz}
+\usetikzlibrary{arrows.meta}
+
+\definecolor{Oc}{RGB}{0, 0, 0}
+
+\begin{document}
+	\begin{tikzpicture}[>=Stealth, line cap=round, line join=round, scale=2.8]
+	\pgfextra{\path[use as bounding box] (-1.3,-1.3) rectangle (1.3,1.3);}
+    \def\R{1.0}
+	\draw[thick] (0,0) circle (\R);
+	
+	% Angles array
+	\def\angles#1{\ifcase#1 {{.Angles}}\fi}
+	
+	\foreach \i in { 0,...,{{.RangeEnd}} }{
+		 \pgfmathsetmacro{\ang}{\angles{\i}}
+		% draw dot
+		\fill[Oc] (\ang:\R) circle (1.1pt);
+	}
+	\end{tikzpicture}
+\end{document}
+`))
+
+func dumpNodesCircleTikz() {
+	ch := NewConsistentHasher(1024 * 1024)
+
+	var nodes []string
+	for i := range 64 {
+		n := fmt.Sprintf("n%003d", i)
+		nodes = append(nodes, n)
+		if err := ch.AddNode(n); err != nil {
+			panic(err)
+		}
+	}
+
+	var angles []string
+
+	// For each node index, compute the angle it represents. ringSize is 360
+	// degrees.
+	for _, s := range ch.slots {
+		fraction := float32(s) / float32(ch.ringSize)
+		angle := fraction * 360.0
+		angles = append(angles, fmt.Sprintf("%d", int(angle)))
+	}
+
+	tikzTemplate.Execute(os.Stdout, map[string]string{
+		"Angles":   strings.Join(angles, `\or `),
+		"RangeEnd": fmt.Sprintf("%d", len(angles)-1),
+	})
+}
+
 func main() {
-	dumpDistribution()
+	//dumpDistribution()
+	dumpNodesCircleTikz()
 }
